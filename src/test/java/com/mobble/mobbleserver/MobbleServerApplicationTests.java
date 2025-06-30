7a4dbbd27df430d1439fd51fc5a61df32f1917a6
Groo <woohyeon3382@naver.com>
2025-06-30 10:17:35 +0900

    project initial commit

---
+++ src/test/java/com/mobble/mobbleserver/MobbleServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mobble.mobbleserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MobbleServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
