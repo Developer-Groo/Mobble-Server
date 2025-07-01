770bcfbfdc6f004de1b1d46456ef754fcaf51c72
Groo <woohyeon3382@naver.com>
2025-07-01 19:09:09 +0900

    refactor: CommentRequestDto 생성 메서드 분리
    - Comment 와 ReplyComment 생성 메서드 각각 분리

--- src/main/java/com/mobble/mobbleserver/domain/comment/dto/request/CommentRequestDto.java
+++ src/main/java/com/mobble/mobbleserver/domain/comment/dto/request/CommentRequestDto.java
@@ -2,13 +2,13 @@ package com.mobble.mobbleserver.domain.comment.dto.request;
 
 import com.mobble.mobbleserver.domain.comment.entity.Comment;
 
-public record CommentRequestDto(Comment parent, String content) {
+public record CommentRequestDto(String content) {
 
-    public Comment toEntity() {
-        if (parent == null) {
-            return Comment.createRootComment(this.content);
-        }
-        return Comment.createReplyComment(this.parent, this.content);
+    public Comment toEntity() { // Todo: article, memeber 객체 필요
+        return Comment.createRootComment(this.content);
+    }
+
+    public Comment toEntity(Comment parent) { // Todo: article, memeber 객체 필요
+        return Comment.createReplyComment(parent, this.content);
     }
 }
-// Todo: 필요한 필드 구현 완료 되면 작업
