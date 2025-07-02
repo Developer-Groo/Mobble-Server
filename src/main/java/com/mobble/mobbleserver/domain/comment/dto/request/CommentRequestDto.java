a05431ec114b0586a27f30349ba8bbe22aba8f55
Groo <woohyeon3382@naver.com>
2025-07-02 20:24:26 +0900

    refactor: Member, Article 파라미터 추가 및 주석 제거

--- src/main/java/com/mobble/mobbleserver/domain/comment/dto/request/CommentRequestDto.java
+++ src/main/java/com/mobble/mobbleserver/domain/comment/dto/request/CommentRequestDto.java
@@ -1,14 +1,16 @@
 package com.mobble.mobbleserver.domain.comment.dto.request;
 
+import com.mobble.mobbleserver.domain.article.entity.Article;
 import com.mobble.mobbleserver.domain.comment.entity.Comment;
+import com.mobble.mobbleserver.domain.member.entity.Member;
 
 public record CommentRequestDto(String content) {
 
-    public Comment toEntity() { // Todo: article, memeber 객체 필요
-        return Comment.createRootComment(this.content);
+    public Comment toEntity(Member member, Article article) {
+        return Comment.createRootComment(member, article, this.content);
     }
 
-    public Comment toEntity(Comment parent) { // Todo: article, memeber 객체 필요
-        return Comment.createReplyComment(parent, this.content);
+    public Comment toEntity(Member member, Article article, Comment parent) {
+        return Comment.createReplyComment(member, article, parent, this.content);
     }
 }
