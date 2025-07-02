903061179df6729a6d493562659cbde9b2eb58ae
taejunUM <utj0411@naver.com>
2025-07-02 19:57:33 +0900

    feat: member repository 구현

---
+++ src/main/java/com/mobble/mobbleserver/member/repository/MemberRepository.java
@@ -0,0 +1,13 @@
+package com.mobble.mobbleserver.member.repository;
+
+import com.mobble.mobbleserver.account.oauth2.provider.SocialProvider;
+import com.mobble.mobbleserver.member.entity.Member;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface MemberRepository extends JpaRepository<Member, Long> {
+
+    Optional<Member> findBySocialTypeAndSocialId(SocialProvider socialType, String socialId);
+
+}
