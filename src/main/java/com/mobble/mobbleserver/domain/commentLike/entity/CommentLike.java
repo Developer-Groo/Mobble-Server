ce7135e07e21411972e3c617376564a71195a6b9
taejunUM <utj0411@naver.com>
2025-07-03 14:12:57 +0900

    feat: commentLike entity&repository 구현

---
+++ src/main/java/com/mobble/mobbleserver/domain/commentLike/entity/CommentLike.java
@@ -0,0 +1,34 @@
+package com.mobble.mobbleserver.domain.commentLike.entity;
+
+import com.mobble.mobbleserver.domain.comment.entity.Comment;
+import com.mobble.mobbleserver.domain.member.entity.Member;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class CommentLike {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "comment_id")
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "comment_id")
+    private Comment commentId;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "member_id")
+    private Member memberId;
+
+    @Builder
+    public CommentLike(Comment commentId, Member memberId) {
+        this.commentId = commentId;
+        this.memberId = memberId;
+    }
+}
