ae8af6cad596605c5a785d00fd1906ebd556f43b
taejunUM <utj0411@naver.com>
2025-07-03 20:45:27 +0900

    feat: like&unlike 토글 서비스로직 구현
    - 멤버 검증 로직은 ClubMember 구현후 리팩토링 (TODO작성)

--- src/main/java/com/mobble/mobbleserver/like/articleLike/service/ArticleLikeService.java
+++ src/main/java/com/mobble/mobbleserver/like/articleLike/service/ArticleLikeService.java
@@ -14,4 +14,34 @@ import org.springframework.transaction.annotation.Transactional;
 @RequiredArgsConstructor
 @Transactional(readOnly = true)
 public class ArticleLikeService {
+    private final ArticleRepository articleRepository;
+    private final ArticleLikeRepository articleLikeRepository;
+//    private final ClubMemberRepository clubMemberRepository;
+
+    @Transactional
+    public ArticleLikeToggleResponseDto toggleLike(Long articleId, Member member) {
+        // TODO 예외처리 강화
+        Article article = articleRepository.findById(articleId)
+                .orElseThrow(() -> new IllegalArgumentException("존재하지 않는 게시글입니다."));
+
+//         // TODO ClubMember 구현 및 security 구현 후 리팩토링
+//         boolean isClubMember = clubMemberRepository.existsByClubAndMember(article.getClub(), member);
+//         if (!isClubMember) {
+//             throw new IllegalStateException("클럽에 가입된 사용자만 좋아요를 누를 수 있습니다.");
+//         }
+
+        return articleLikeRepository.findByArticleAndMember(article, member)
+                // like > unlike 변환
+                .map(existingLike -> {
+                    articleLikeRepository.delete(existingLike);
+                    return ArticleLikeToggleResponseDto.toDto(false, articleId, member.getId());
+                })
+                // unlike > like 변환
+                .orElseGet(() -> {
+                    ArticleLike articleLike = ArticleLike.createArticleLike(article, member);
+                    articleLikeRepository.save(articleLike);
+                    return ArticleLikeToggleResponseDto.toDto(true, articleId, member.getId());
+                });
+
+    }
 }
