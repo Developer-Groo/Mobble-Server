2b591c2dd46b38384199570d95fedf94e271c54c
taejunUM <utj0411@naver.com>
2025-07-03 12:38:01 +0900

    feat: clubLike entity&repository 구현

---
+++ src/main/java/com/mobble/mobbleserver/domain/clubLikes/entity/ClubLike.java
@@ -0,0 +1,27 @@
+package com.mobble.mobbleserver.domain.clubLikes.entity;
+
+import com.mobble.mobbleserver.domain.member.entity.Member;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class ClubLike {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "club_like_id")
+    private Long id;
+
+//Todo 추후 cascade, orphanRemoval = true 설정 필요
+//    @ManyToOne(fetch = FetchType.LAZY)
+//    @JoinColumn(name = "club_id", nullable = false)
+//    private Club clubId;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "member_id", nullable = false)
+    private Member memberId;
+}
