8fa4793791b32f7c839154f9c2f316666adcdf1a
taejunUM <utj0411@naver.com>
2025-07-03 22:46:06 +0900

    feat: 좋아요 멤버 반환 dto 구현

---
+++ src/main/java/com/mobble/mobbleserver/like/articleLike/dto/response/ArticleLikeMemberResponseDto.java
@@ -0,0 +1,17 @@
+package com.mobble.mobbleserver.like.articleLike.dto.response;
+
+import com.mobble.mobbleserver.domain.member.entity.Member;
+
+public record ArticleLikeMemberResponseDto (
+        Long memberId,
+        String name,
+        String profileImage
+){
+    public static ArticleLikeMemberResponseDto toDto(Member member) {
+        return new ArticleLikeMemberResponseDto(
+                member.getId(),
+                member.getName(),
+                member.getProfileImage()
+        );
+    }
+}
