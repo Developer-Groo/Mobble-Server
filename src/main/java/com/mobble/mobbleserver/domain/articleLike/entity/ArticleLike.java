73f7bfdffefd45a2f47eccf1c43dc99dd73bf3fd
taejunUM <utj0411@naver.com>
2025-07-03 14:08:31 +0900

    feat: articleLike entity&repository 구현

---
+++ src/main/java/com/mobble/mobbleserver/domain/articleLike/entity/ArticleLike.java
@@ -0,0 +1,34 @@
+package com.mobble.mobbleserver.domain.articleLike.entity;
+
+import com.mobble.mobbleserver.domain.article.entity.Article;
+import com.mobble.mobbleserver.domain.member.entity.Member;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class ArticleLike {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "article_like_id")
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "article_id")
+    private Article articleId;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "member_id")
+    private Member memberId;
+
+    @Builder
+    public ArticleLike(Article articleId, Member memberId) {
+        this.articleId = articleId;
+        this.memberId = memberId;
+    }
+}
