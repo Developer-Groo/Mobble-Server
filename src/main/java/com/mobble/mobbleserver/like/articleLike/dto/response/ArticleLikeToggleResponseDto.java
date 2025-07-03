307e9b41fbdba31dceadd1ec8a149b6ae216c2dc
taejunUM <utj0411@naver.com>
2025-07-03 20:43:48 +0900

    feat: like&unlike 토글 반환 dto 구현

--- src/main/java/com/mobble/mobbleserver/like/articleLike/dto/response/ArticleLikeToggleResponseDto.java
+++ src/main/java/com/mobble/mobbleserver/like/articleLike/dto/response/ArticleLikeToggleResponseDto.java
@@ -3,5 +3,17 @@ package com.mobble.mobbleserver.like.articleLike.dto.response;
 import com.mobble.mobbleserver.like.LikeType;
 
 public record ArticleLikeToggleResponseDto(
+        LikeType targetType,
+        Long articleId,
+        Long memberId,
+        String status // "LIKED" or "UNLIKED"
 ) {
+    public static ArticleLikeToggleResponseDto toDto(boolean isLiked, Long articleId, Long memberId) {
+        return new ArticleLikeToggleResponseDto(
+                LikeType.ARTICLE,
+                articleId,
+                memberId,
+                isLiked ? "LIKED" : "UNLIKED"
+        );
+    }
 }
