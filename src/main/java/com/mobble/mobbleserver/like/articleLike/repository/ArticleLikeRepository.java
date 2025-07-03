7ec15bd550e18e2f1fbdb3614c8b3fb221198276
taejunUM <utj0411@naver.com>
2025-07-03 22:49:47 +0900

    feat: ArticleLike Repository 기능 추가

--- src/main/java/com/mobble/mobbleserver/like/articleLike/repository/ArticleLikeRepository.java
+++ src/main/java/com/mobble/mobbleserver/like/articleLike/repository/ArticleLikeRepository.java
@@ -1,13 +1,18 @@
 package com.mobble.mobbleserver.like.articleLike.repository;
 
 import com.mobble.mobbleserver.domain.article.entity.Article;
-import com.mobble.mobbleserver.like.articleLike.entity.ArticleLike;
 import com.mobble.mobbleserver.domain.member.entity.Member;
+import com.mobble.mobbleserver.like.articleLike.entity.ArticleLike;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface ArticleLikeRepository extends JpaRepository<ArticleLike, Long> {
 
     Optional<ArticleLike> findByArticleAndMember(Article article, Member member);
+
+    int countByArticle(Article article);
+
+    List<ArticleLike> findAllByArticle(Article article);
 }
