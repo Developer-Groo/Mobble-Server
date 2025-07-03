b2b13cd9bf2828c767ab0b36c34146e8b5c3f20a
taejunUM <utj0411@naver.com>
2025-07-03 20:52:00 +0900

    feat: 좋아요 갯수 반환 dto 구현

---
+++ src/main/java/com/mobble/mobbleserver/like/articleLike/dto/response/ArticleLikeCountResponseDto.java
@@ -0,0 +1,10 @@
+package com.mobble.mobbleserver.like.articleLike.dto.response;
+
+public record ArticleLikeCountResponseDto(
+        Long articleId,
+        int likeCount
+) {
+    public static ArticleLikeCountResponseDto toDto(Long articleId, int count) {
+        return new ArticleLikeCountResponseDto(articleId, count);
+    }
+}
