c6eb316b455a72d6517ff05353e7f8e59bf9224f
taejunUM <utj0411@naver.com>
2025-07-03 15:20:29 +0900

    refactor: Meeting import, 정적 팩토리 메서드 추가

--- src/main/java/com/mobble/mobbleserver/domain/meetingMember/entity/MeetingMember.java
+++ src/main/java/com/mobble/mobbleserver/domain/meetingMember/entity/MeetingMember.java
@@ -1,8 +1,10 @@
 package com.mobble.mobbleserver.domain.meetingMember.entity;
 
+import com.mobble.mobbleserver.domain.meeting.entity.Meeting;
 import com.mobble.mobbleserver.domain.member.entity.Member;
 import jakarta.persistence.*;
 import lombok.AccessLevel;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
@@ -16,18 +18,24 @@ public class MeetingMember {
     @Column(name = "meeting_member_id")
     private Long id;
 
-    //TODO 머지 후 import
-//    @ManyToOne(fetch = FetchType.LAZY)
-//    @JoinColumn(name = "meeting_id")
-//    private Meeting meetingId;
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "meeting_id")
+    private Meeting meetingId;
 
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "member_id")
     private Member memberId;
 
-//    @Builder
-//    public MeetingMember(Meeting meetingId, Member memberId) {
-//        this.meetingId = meetingId;
-//        this.memberId = memberId;
-//    }
+    @Builder
+    private MeetingMember(Meeting meetingId, Member memberId) {
+        this.meetingId = meetingId;
+        this.memberId = memberId;
+    }
+
+    public static MeetingMember createMeetingMember(Meeting meetingId, Member memberId) {
+        return MeetingMember.builder()
+                .meetingId(meetingId)
+                .memberId(memberId)
+                .build();
+    }
 }
