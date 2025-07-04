5adb0da4089f7ec5d47ca6faa3028977ebc5504f
taejunUM <utj0411@naver.com>
2025-07-04 10:32:52 +0900

    refactor: articleLike 컨트롤러 api 제거

--- src/main/java/com/mobble/mobbleserver/domain/like/articleLike/controller/ArticleLikeController.java
+++ src/main/java/com/mobble/mobbleserver/domain/like/articleLike/controller/ArticleLikeController.java
@@ -1,15 +1,14 @@
 package com.mobble.mobbleserver.domain.like.articleLike.controller;
 
-import com.mobble.mobbleserver.domain.like.articleLike.dto.response.ArticleLikeCountResponseDto;
-import com.mobble.mobbleserver.domain.like.articleLike.dto.response.ArticleLikeMemberResponseDto;
 import com.mobble.mobbleserver.domain.like.articleLike.dto.response.ArticleLikeToggleResponseDto;
 import com.mobble.mobbleserver.domain.like.articleLike.service.ArticleLikeService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
-
-import java.util.List;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
@@ -26,21 +25,5 @@ public class ArticleLikeController {
         Long memberId = 1L;
         return ResponseEntity.status(HttpStatus.CREATED)
                 .body(articleLikeService.toggleLike(articleId, memberId));
-    }
-
-    @GetMapping("/count")
-    public ResponseEntity<ArticleLikeCountResponseDto> getLikeCount(
-            @PathVariable("article-id") Long articleId
-    ) {
-        return ResponseEntity.status(HttpStatus.OK)
-                .body(articleLikeService.getArticleLikeCount(articleId));
-    }
-
-    @GetMapping("/members")
-    public ResponseEntity<List<ArticleLikeMemberResponseDto>> getArticleLikedMembers(
-            @PathVariable("article-id") Long articleId
-    ) {
-        return ResponseEntity.status(HttpStatus.OK)
-                .body(articleLikeService.getArticleLikedMembers(articleId));
     }
 }
