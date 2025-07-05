43ccc84d4d0465089c0a28ae14134dc96430b749
Groo <woohyeon3382@naver.com>
2025-07-05 16:31:10 +0900

    feat: ErrorResponseDto 에 시간 필드 추가
    - ZonedDateTime 활용

--- src/main/java/com/mobble/mobbleserver/global/exception/handler/dto/ErrorResponseDto.java
+++ src/main/java/com/mobble/mobbleserver/global/exception/handler/dto/ErrorResponseDto.java
@@ -3,17 +3,39 @@ package com.mobble.mobbleserver.global.exception.handler.dto;
 import com.mobble.mobbleserver.global.exception.common.ErrorCode;
 import org.springframework.http.HttpStatus;
 
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.Map;
 
-public record ErrorResponseDto(String message, HttpStatus code, Map<String, String> errors) {
+public record ErrorResponseDto(
+        String timestamp,
+        String message,
+        HttpStatus code,
+        Map<String, String> errors) {
 
     // Validation or Binding Error
     public static ErrorResponseDto toDto(ErrorCode errorCode, Map<String, String> errors) {
-        return new ErrorResponseDto(errorCode.message(), errorCode.httpStatus(), errors);
+        return new ErrorResponseDto(
+                createDateTime(),
+                errorCode.message(),
+                errorCode.httpStatus(),
+                errors
+        );
     }
 
     // Global or Domain Error
     public static ErrorResponseDto toDto(ErrorCode errorCode) {
-        return new ErrorResponseDto(errorCode.message(), errorCode.httpStatus(), null);
+        return new ErrorResponseDto(
+                createDateTime(),
+                errorCode.message(),
+                errorCode.httpStatus(),
+                null
+        );
+    }
+
+    private static String createDateTime() {
+        return ZonedDateTime.now(ZoneId.of("Asia/Seoul"))
+                .format(DateTimeFormatter.ISO_OFFSET_DATE_TIME);
     }
 }
