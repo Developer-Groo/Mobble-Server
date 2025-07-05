ccf851e3db9e24cd8a182f73272bffa189dac4ef
Groo <woohyeon3382@naver.com>
2025-07-05 16:11:27 +0900

    feat: findByIdAndMemberId 메서드 반환 타입 Optional 로 변경

--- src/main/java/com/mobble/mobbleserver/domain/comment/repository/CommentRepository.java
+++ src/main/java/com/mobble/mobbleserver/domain/comment/repository/CommentRepository.java
@@ -4,7 +4,10 @@ import com.mobble.mobbleserver.domain.comment.entity.Comment;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface CommentRepository extends JpaRepository<Comment, Long>, CommentQueryDslRepository {
 
+    Optional<Comment> findByIdAndMemberId(Long commentId, Long memberId);
 }
