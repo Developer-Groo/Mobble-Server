718124c65427556fd56eca9fe614b5da758ebce2
taejunUM <utj0411@naver.com>
2025-07-07 18:10:58 +0900

    feat: 댓글 좋아요 repository 구현
    - 좋아요 유무 검증
    - 좋아요 갯수 검색

--- src/main/java/com/mobble/mobbleserver/domain/like/commentLike/repository/CommentLikeRepository.java
+++ src/main/java/com/mobble/mobbleserver/domain/like/commentLike/repository/CommentLikeRepository.java
@@ -3,6 +3,11 @@ package com.mobble.mobbleserver.domain.like.commentLike.repository;
 import com.mobble.mobbleserver.domain.like.commentLike.entity.CommentLike;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface CommentLikeRepository extends JpaRepository<CommentLike, Long> {
 
+    Optional<CommentLike> findLikedByCommentIdAndMemberId(Long commentId, Long memberId);
+
+    int countCommentLikesByCommentId(Long commentId);
 }
