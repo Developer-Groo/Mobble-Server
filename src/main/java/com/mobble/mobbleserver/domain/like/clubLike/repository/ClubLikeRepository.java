a0c829aeb4fbaff4e296914022667f1d42a6c065
taejunUM <utj0411@naver.com>
2025-07-08 11:22:25 +0900

    feat: clubLike repository 구현

--- src/main/java/com/mobble/mobbleserver/domain/like/clubLike/repository/ClubLikeRepository.java
+++ src/main/java/com/mobble/mobbleserver/domain/like/clubLike/repository/ClubLikeRepository.java
@@ -3,6 +3,13 @@ package com.mobble.mobbleserver.domain.like.clubLike.repository;
 import com.mobble.mobbleserver.domain.like.clubLike.entity.ClubLike;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface ClubLikeRepository extends JpaRepository<ClubLike, Long> {
 
+    Optional<ClubLike> findLikedByClubIdAndMemberId(Long clubId, Long memberId);
+
+    int countClubLikesByClubId(Long clubId);
+
+    boolean existsByClubIdAndMemberId(Long clubId, Long memberId);
 }
