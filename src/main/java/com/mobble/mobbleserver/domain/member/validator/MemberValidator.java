d0c4f51177ae209e61a05221bb67331b65822cd0
taejunUM <utj0411@naver.com>
2025-07-08 20:01:34 +0900

    feat: email 중복 검증 메서드 구현

---
+++ src/main/java/com/mobble/mobbleserver/domain/member/validator/MemberValidator.java
@@ -0,0 +1,20 @@
+package com.mobble.mobbleserver.domain.member.validator;
+
+import com.mobble.mobbleserver.domain.member.repository.MemberRepository;
+import com.mobble.mobbleserver.global.exception.common.DomainException;
+import com.mobble.mobbleserver.global.exception.errorCode.member.MemberErrorCode;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class MemberValidator {
+
+    private final MemberRepository memberRepository;
+
+    public void validateEmail(String email) {
+        if (memberRepository.existsByEmail(email)) {
+            throw new DomainException(MemberErrorCode.JOINED_MEMBER);
+        }
+    }
+}
