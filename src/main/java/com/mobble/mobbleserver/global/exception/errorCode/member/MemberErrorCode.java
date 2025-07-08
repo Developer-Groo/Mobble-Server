ff103092dbb42800c2cc3c8feddbc380612e73ce
taejunUM <utj0411@naver.com>
2025-07-09 01:00:00 +0900

    feat: ErrorCode 추가

--- src/main/java/com/mobble/mobbleserver/global/exception/errorCode/member/MemberErrorCode.java
+++ src/main/java/com/mobble/mobbleserver/global/exception/errorCode/member/MemberErrorCode.java
@@ -7,6 +7,7 @@ import org.springframework.http.HttpStatus;
 @RequiredArgsConstructor
 public enum MemberErrorCode implements ErrorCode {
     JOINED_EMAIL("이미 가입된 email 입니다.", HttpStatus.BAD_REQUEST),
+    NOT_FOUND_MEMBER("멤버를 찾을 수 없습니다.", HttpStatus.NOT_FOUND);
 
 
     private final String message;
