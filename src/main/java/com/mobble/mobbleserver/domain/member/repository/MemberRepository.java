3729b8127afaf21f7d5ead93cda8e17c1f1f0c27
taejunUM <utj0411@naver.com>
2025-07-09 18:06:00 +0900

    feat: 레포지토리 hardDelete nativeQuery 구현

--- src/main/java/com/mobble/mobbleserver/domain/member/repository/MemberRepository.java
+++ src/main/java/com/mobble/mobbleserver/domain/member/repository/MemberRepository.java
@@ -2,11 +2,22 @@ package com.mobble.mobbleserver.domain.member.repository;
 
 import com.mobble.mobbleserver.domain.member.entity.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.time.LocalDateTime;
+import java.util.List;
 
 @Repository
 public interface MemberRepository extends JpaRepository<Member, Long> {
 
     boolean existsByEmail(String email);
 
+    @Query(value =
+            "SELECT * FROM member " +
+            "WHERE is_deleted = true " +
+            "AND deleted_at < :withdrewDate",
+            nativeQuery = true)
+    List<Member> findWithdrewMembers(@Param("withdrewDate") LocalDateTime withdrewDate);
 }
