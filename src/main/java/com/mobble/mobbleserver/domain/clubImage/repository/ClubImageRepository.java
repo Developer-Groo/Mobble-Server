34fc78fdde040826b45b0f5f607ae5872ac9e603
Hojin02 <140497846+Hojin02@users.noreply.github.com>
2025-07-09 20:44:01 +0900

    Club Image Repository 구현

---
+++ src/main/java/com/mobble/mobbleserver/domain/clubImage/repository/ClubImageRepository.java
@@ -0,0 +1,7 @@
+package com.mobble.mobbleserver.domain.clubImage.repository;
+
+import com.mobble.mobbleserver.domain.clubImage.entity.ClubImage;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ClubImageRepository extends JpaRepository<ClubImage, Long> {
+}
