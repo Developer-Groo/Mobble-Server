15d9644c9293544e479ae6085a4af631469a4e54
taejunUM <utj0411@naver.com>
2025-07-10 06:33:23 +0900

    feat: memberErrorcode 추가

---
+++ src/main/java/com/mobble/mobbleserver/global/exception/errorCode/member/MemberErrorCode.java
@@ -0,0 +1,24 @@
+package com.mobble.mobbleserver.global.exception.errorCode.member;
+
+import com.mobble.mobbleserver.global.exception.common.ErrorCode;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+
+@RequiredArgsConstructor
+public enum MemberErrorCode implements ErrorCode {
+    NOT_FOUND_MEMBER("멤버를 찾을 수 없습니다.", HttpStatus.NOT_FOUND);
+
+    private final String message;
+    private final HttpStatus httpStatus;
+
+
+    @Override
+    public String message() {
+        return message;
+    }
+
+    @Override
+    public HttpStatus httpStatus() {
+        return httpStatus;
+    }
+}
