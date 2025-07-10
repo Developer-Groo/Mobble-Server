7220c901a6bb9c770c6bfc44e3060391d9aeef59
taejunUM <utj0411@naver.com>
2025-07-10 14:57:23 +0900

    refactor: 레포지토리 native query 적용

--- src/main/java/com/mobble/mobbleserver/domain/member/repository/MemberRepository.java
+++ src/main/java/com/mobble/mobbleserver/domain/member/repository/MemberRepository.java
@@ -2,13 +2,34 @@ package com.mobble.mobbleserver.domain.member.repository;
 
 import com.mobble.mobbleserver.domain.member.entity.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.time.LocalDateTime;
 import java.util.List;
+import java.util.Optional;
 
 public interface MemberRepository extends JpaRepository<Member, Long> {
 
-    boolean existsByEmail(String email);
+    /**
+     * isDeleted = true 포함 멤버 유무 조회
+     * @param email
+     * @return
+     */
+    @Query(value = "SELECT * FROM member m " +
+            "WHERE m.email = :email",
+            nativeQuery = true)
+    Optional<Member> findMemberByEmail(@Param("email") String email);
 
-    List<Member> findByIsDeletedTrueAndDeletedAtBefore(LocalDateTime withdrewDate);
+    /**
+     * softDelete 멤버 조회
+     * @param withdrewDate
+     * @return
+     */
+    @Query(value =
+            "SELECT * FROM member " +
+                    "WHERE is_deleted = true " +
+                    "AND deleted_at < :withdrewDate",
+            nativeQuery = true)
+    List<Member> findByIsDeletedTrueAndDeletedAtBefore(@Param("withdrewDate") LocalDateTime withdrewDate);
 }
