736e83259f6cd0f024e6789cc31e93bde7111c0a
taejunUM <utj0411@naver.com>
2025-07-10 12:50:00 +0900

    feat: scheduler 작동시  log 출력 구현

--- src/main/java/com/mobble/mobbleserver/domain/member/Scheduler/MemberScheduler.java
+++ src/main/java/com/mobble/mobbleserver/domain/member/Scheduler/MemberScheduler.java
@@ -3,6 +3,7 @@ package com.mobble.mobbleserver.domain.member.Scheduler;
 import com.mobble.mobbleserver.domain.member.entity.Member;
 import com.mobble.mobbleserver.domain.member.repository.MemberRepository;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Transactional;
@@ -10,6 +11,7 @@ import org.springframework.transaction.annotation.Transactional;
 import java.time.LocalDateTime;
 import java.util.List;
 
+@Slf4j
 @Component
 @RequiredArgsConstructor
 public class MemberScheduler {
@@ -19,10 +21,19 @@ public class MemberScheduler {
     @Scheduled(cron = "0 0 0 * * *")
     @Transactional
     public void deleteWithdrewMembers() {
+        log.info("Delete the withdrew members.");
 
         LocalDateTime withdrewDate = LocalDateTime.now().minusDays(7);
+        log.info("withdrew members delete time: {}", withdrewDate);
+
         List<Member> withdrewMembers = memberRepository.findWithdrewMembers(withdrewDate);
 
+        if (withdrewMembers.isEmpty()) {
+            log.info("Not found withdrew members to delete");
+        }
+        log.info("Deleting {} withdrew members.", withdrewMembers.size());
+
         memberRepository.deleteAll(withdrewMembers);
+        log.info("Finished deleting withdrew members.");
     }
 }
