1c195f7780b2d3d4e0242d4ac3c23fb4de7ff905
taejunUM <utj0411@naver.com>
2025-07-10 18:49:34 +0900

    refactor: query method 네이밍 변경

--- src/main/java/com/mobble/mobbleserver/domain/member/repository/MemberRepository.java
+++ src/main/java/com/mobble/mobbleserver/domain/member/repository/MemberRepository.java
@@ -12,7 +12,10 @@ public interface MemberRepository extends JpaRepository<Member, Long> {
     // 활성 회원(isDeleted = false)만 조회하는 메소드
     Optional<Member> findByIdAndIsDeletedFalse(Long memberId);
 
+    boolean existsByEmail(String email);
+
     // soft-delete(isDeleted = true) 회원을 포함한 모든 회원 조회
-    Optional<Member> findByEmail(String email);
     List<Member> findAllByIsDeletedTrueAndDeletedAtBefore(LocalDateTime withdrewDate);
+
+    boolean existsByEmailAndIsDeletedTrue(String email);
 }
