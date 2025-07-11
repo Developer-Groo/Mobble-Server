687d8c11515779c86990e932591e2edcd415e76c
Groo <woohyeon3382@naver.com>
2025-07-11 18:27:46 +0900

    feat: createWithMemberAndClub 메서드 구현

--- src/test/java/com/mobble/mobbleserver/support/fixture/article/ArticleTestFixture.java
+++ src/test/java/com/mobble/mobbleserver/support/fixture/article/ArticleTestFixture.java
@@ -2,10 +2,28 @@ package com.mobble.mobbleserver.support.fixture.article;
 
 import com.mobble.mobbleserver.domain.article.entity.Article;
 import com.mobble.mobbleserver.domain.article.entity.ArticleType;
+import com.mobble.mobbleserver.domain.club.entity.Club;
+import com.mobble.mobbleserver.domain.member.entity.Member;
+import com.mobble.mobbleserver.support.fixture.club.ClubTestFixture;
+import com.mobble.mobbleserver.support.fixture.member.MemberTestFixture;
 
 public class ArticleTestFixture {
 
     public static Article createDefaultArticle() {
-        return Article.createArticle(ArticleType.FREE, "title", "content");
+        return Article.createArticle(
+                ClubTestFixture.createDefaultClub(),
+                MemberTestFixture.createDefaultMember(),
+                ArticleType.FREE, "title",
+                "content"
+        );
+    }
+
+    public static Article createWithMemberAndClub(Member member, Club club) {
+        return Article.createArticle(
+                club,
+                member,
+                ArticleType.FREE, "title",
+                "content"
+        );
     }
 }
