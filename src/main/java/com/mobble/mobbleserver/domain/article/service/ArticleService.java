194e5f3b5024855e093edd0c4f8bfa6a330eaf61
Hojin02 <140497846+Hojin02@users.noreply.github.com>
2025-07-11 16:47:17 +0900

    feat: Article 생성 서비스 로직 구현
    일반유저가 공지를 작성 못하도록 구현

--- src/main/java/com/mobble/mobbleserver/domain/article/service/ArticleService.java
+++ src/main/java/com/mobble/mobbleserver/domain/article/service/ArticleService.java
@@ -1,9 +1,22 @@
 package com.mobble.mobbleserver.domain.article.service;
 
+import com.mobble.mobbleserver.domain.article.dto.request.ArticleRequestDto;
+import com.mobble.mobbleserver.domain.article.dto.response.ArticleResponseDto;
+import com.mobble.mobbleserver.domain.article.entity.Article;
+import com.mobble.mobbleserver.domain.article.entity.ArticleType;
 import com.mobble.mobbleserver.domain.article.repository.ArticleRepository;
+import com.mobble.mobbleserver.domain.club.entity.Club;
+import com.mobble.mobbleserver.domain.club.repository.ClubRepository;
+import com.mobble.mobbleserver.domain.clubMember.entity.ClubMember;
+import com.mobble.mobbleserver.domain.clubMember.entity.ClubMemberRole;
+import com.mobble.mobbleserver.domain.clubMember.repository.ClubMemberRepository;
+import com.mobble.mobbleserver.domain.member.entity.Member;
+import com.mobble.mobbleserver.domain.member.repository.MemberRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
@@ -11,4 +24,41 @@ import org.springframework.transaction.annotation.Transactional;
 public class ArticleService {
 
     private final ArticleRepository articleRepository;
+    private final MemberRepository memberRepository;
+    private final ClubRepository clubRepository;
+    private final ClubMemberRepository clubMemberRepository;
+
+
+    public ArticleResponseDto createArticle(Long memberId, Long clubId, ArticleRequestDto dto) {
+        Member member = findMemberOrThrow(memberId);
+        Club club = findClubOrThrow(clubId);
+        Article article = dto.toEntity(club,member);
+        ClubMember clubMember = findClubMemberOrThrow(clubId,memberId);
+
+        if (dto.articleType() == ArticleType.NOTICE && clubMember.getClubMemberRole() == ClubMemberRole.MEMBER) {
+            throw new IllegalArgumentException(""); // Todo: Custom 예외 적용 및 validator 접근
+        }
+
+        // 해당 멤버의 권한 확인
+        // 관리자 제외 일반 사용자는 공지글 생성 불가
+        return ArticleResponseDto.toDto(
+                articleRepository.save(article),
+                false,
+                true,
+                0,
+                List.of());
+    }
+
+    private Member findMemberOrThrow(Long memberId) {
+        return memberRepository.findById(memberId)
+                .orElseThrow(() -> new IllegalArgumentException("")); // Todo: Custom 예외 적용 및 validator 접근
+    }
+    private Club findClubOrThrow(Long clubId) {
+        return clubRepository.findById(clubId)
+                .orElseThrow(() -> new IllegalArgumentException("")); // Todo: Custom 예외 적용 및 validator 접근
+    }
+    private ClubMember findClubMemberOrThrow(Long clubId, Long memberId) {
+        return clubMemberRepository.findByClubIdAndMemberId(clubId, memberId)
+                .orElseThrow(() -> new IllegalArgumentException("")); // Todo: Custom 예외 적용 및 validator 접근
+    }
 }
