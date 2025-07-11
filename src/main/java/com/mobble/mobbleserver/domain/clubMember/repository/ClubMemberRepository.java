21763a195352fdcfdeb64c1977d8e50687fb53a6
Groo <woohyeon3382@naver.com>
2025-07-11 22:57:48 +0900

    feat: ClubMemberRepository 메서드 추가
    - findClubMemberByClubIdAndMemberId

--- src/main/java/com/mobble/mobbleserver/domain/clubMember/repository/ClubMemberRepository.java
+++ src/main/java/com/mobble/mobbleserver/domain/clubMember/repository/ClubMemberRepository.java
@@ -3,5 +3,9 @@ package com.mobble.mobbleserver.domain.clubMember.repository;
 import com.mobble.mobbleserver.domain.clubMember.entity.ClubMember;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface ClubMemberRepository extends JpaRepository<ClubMember, Long> {
+
+    Optional<ClubMember> findClubMemberByClubIdAndMemberId(Long clubId, Long memberId);
 }
