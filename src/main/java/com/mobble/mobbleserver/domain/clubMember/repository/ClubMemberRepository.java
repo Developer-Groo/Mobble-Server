d59fe6e2c812630578fb015cb8af6f8ab693e949
Hojin02 <140497846+Hojin02@users.noreply.github.com>
2025-07-11 16:29:26 +0900

    feat: ClubMember를 찾는 메소드 추가

--- src/main/java/com/mobble/mobbleserver/domain/clubMember/repository/ClubMemberRepository.java
+++ src/main/java/com/mobble/mobbleserver/domain/clubMember/repository/ClubMemberRepository.java
@@ -3,5 +3,8 @@ package com.mobble.mobbleserver.domain.clubMember.repository;
 import com.mobble.mobbleserver.domain.clubMember.entity.ClubMember;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface ClubMemberRepository extends JpaRepository<ClubMember, Long> {
+    Optional<ClubMember> findByClubIdAndMemberId(Long clubId, Long memberId);
 }
