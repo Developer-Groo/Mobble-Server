36ee1616349802444ff9abd5b8e77da76166cabb
Hojin02 <140497846+Hojin02@users.noreply.github.com>
2025-07-11 16:25:30 +0900

    feat: Article Controller 글작성 메서드  구현

--- src/main/java/com/mobble/mobbleserver/domain/article/controller/ArticleController.java
+++ src/main/java/com/mobble/mobbleserver/domain/article/controller/ArticleController.java
@@ -1,9 +1,14 @@
 package com.mobble.mobbleserver.domain.article.controller;
 
+import com.mobble.mobbleserver.domain.article.dto.request.ArticleRequestDto;
+import com.mobble.mobbleserver.domain.article.dto.response.ArticleResponseDto;
 import com.mobble.mobbleserver.domain.article.service.ArticleService;
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.Positive;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
@@ -11,4 +16,15 @@ import org.springframework.web.bind.annotation.RestController;
 public class ArticleController {
 
     private final ArticleService articleService;
+
+    @PostMapping("/{club-id}")
+    public ResponseEntity<ArticleResponseDto> createArticle(
+            @PathVariable("club-id") @Positive Long clubId,
+            @RequestBody @Valid ArticleRequestDto dto
+    ) {
+        Long memberId = 1L; // Todo: 임시 member id
+
+        return ResponseEntity.status(HttpStatus.CREATED)
+                .body(articleService.createArticle(memberId, clubId, dto));
+    }
 }
