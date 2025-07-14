ea2f54554008528ad87e24f6a14ae041230e2467
Hojin02 <140497846+Hojin02@users.noreply.github.com>
2025-07-14 11:01:40 +0900

    feat: 한 클럽의 전체 게시글 조회 Dto 구현
    필요한 필드만 있음

---
+++ src/main/java/com/mobble/mobbleserver/domain/article/dto/response/ArticleSummaryResponseDto.java
@@ -0,0 +1,20 @@
+package com.mobble.mobbleserver.domain.article.dto.response;
+
+import com.mobble.mobbleserver.domain.article.entity.ArticleType;
+import java.time.LocalDateTime;
+
+public record ArticleSummaryResponseDto(
+        Long articleId,
+        String title,
+        String content,
+        ArticleType articleType,
+        Long clubId,
+        String memberName,
+
+        // todo: 글작성 회원 프로필 사진 추가
+        Long likeCount,
+        Long commentCount,
+        LocalDateTime createdAt,
+        LocalDateTime updatedAt
+) {
+}
