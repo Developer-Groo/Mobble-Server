e73c8d1b22ae083f5fef4fb734a0f8e35f9232bb
Groo <woohyeon3382@naver.com>
2025-07-15 00:15:31 +0900

    feat: 좋아요 조회를 위한 findLikeInfoByCommentIdsAndMemberId 메서드 정의

--- src/main/java/com/mobble/mobbleserver/domain/comment/repository/CommentQueryDslRepository.java
+++ src/main/java/com/mobble/mobbleserver/domain/comment/repository/CommentQueryDslRepository.java
@@ -1,10 +1,14 @@
 package com.mobble.mobbleserver.domain.comment.repository;
 
+import com.mobble.mobbleserver.domain.comment.repository.dto.CommentLikeInfo;
 import com.mobble.mobbleserver.domain.comment.entity.Comment;
 
 import java.util.List;
+import java.util.Map;
 
 public interface CommentQueryDslRepository {
 
     List<Comment> findCommentsWithRepliesByArticleId(Long articleId);
+
+    Map<Long, CommentLikeInfo> findLikeInfoByCommentIdsAndMemberId(List<Long> commentIds, Long memberId);
 }
