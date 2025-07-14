8c1f651be60b6fee948536c11baff1ead4d7a4b2
Hojin02 <140497846+Hojin02@users.noreply.github.com>
2025-07-14 15:18:29 +0900

    feat: Article단건 조회 API 컨트롤러 구현

--- src/main/java/com/mobble/mobbleserver/domain/article/controller/ArticleController.java
+++ src/main/java/com/mobble/mobbleserver/domain/article/controller/ArticleController.java
@@ -35,8 +35,18 @@ public class ArticleController {
     public ResponseEntity<List<ArticleSummaryResponseDto>> findArticlesByClubId(
             @PathVariable("club-id") @Positive Long clubId,
             @RequestParam(value = "articleType", required = false) ArticleType articleType
-    ){
+    ) {
         return ResponseEntity.status(HttpStatus.OK)
-                .body(articleService.findArticlesByClubId(clubId,articleType));
+                .body(articleService.findArticlesByClubId(clubId, articleType));
+    }
+
+    @GetMapping("/articles/{article-id}")
+    public ResponseEntity<ArticleResponseDto> findArticle(
+            @PathVariable("article-id") @Positive Long articleId
+    ) {
+        Long memberId = 1L; // Todo: 임시 member id
+
+        return ResponseEntity.status(HttpStatus.OK)
+                .body(articleService.findArticleById(articleId, memberId));
     }
 }
