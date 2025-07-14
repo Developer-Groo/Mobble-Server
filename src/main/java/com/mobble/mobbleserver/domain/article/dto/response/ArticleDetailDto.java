867a1c12ec354899e403223b8f4dfa971579419d
Hojin02 <140497846+Hojin02@users.noreply.github.com>
2025-07-14 17:20:52 +0900

    feat: 게시글 상세조회 QueryDsl을 위한 Dto 설계

---
+++ src/main/java/com/mobble/mobbleserver/domain/article/dto/response/ArticleDetailDto.java
@@ -0,0 +1,19 @@
+package com.mobble.mobbleserver.domain.article.dto.response;
+
+import com.mobble.mobbleserver.domain.article.entity.ArticleType;
+
+import java.time.LocalDateTime;
+
+public record ArticleDetailDto(
+        Long articleId,
+        String title,
+        String content,
+        ArticleType articleType,
+        Long clubId,
+        Long memberId,
+        String memberName,
+        Long likeCount,
+        Long commentCount,
+        LocalDateTime createdAt,
+        LocalDateTime updatedAt
+) {}
