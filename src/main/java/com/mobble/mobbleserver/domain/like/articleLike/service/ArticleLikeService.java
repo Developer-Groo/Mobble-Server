7494dafa26e3e8a2ca2002d711f9110c379eabc4
taejunUM <utj0411@naver.com>
2025-07-14 16:52:43 +0900

    refactor: toggleLike 메서드 반환 dto 변경

--- src/main/java/com/mobble/mobbleserver/domain/like/articleLike/service/ArticleLikeService.java
+++ src/main/java/com/mobble/mobbleserver/domain/like/articleLike/service/ArticleLikeService.java
@@ -2,8 +2,8 @@ package com.mobble.mobbleserver.domain.like.articleLike.service;
 
 import com.mobble.mobbleserver.domain.article.entity.Article;
 import com.mobble.mobbleserver.domain.article.repository.ArticleRepository;
-import com.mobble.mobbleserver.domain.like.articleLike.dto.response.ArticleLikeInfoResponseDto;
 import com.mobble.mobbleserver.domain.like.articleLike.dto.response.ArticleLikeMemberListResponseDto;
+import com.mobble.mobbleserver.domain.like.articleLike.dto.response.ArticleToggleLikeResponseDto;
 import com.mobble.mobbleserver.domain.like.articleLike.entity.ArticleLike;
 import com.mobble.mobbleserver.domain.like.articleLike.repository.ArticleLikeRepository;
 import com.mobble.mobbleserver.domain.member.entity.Member;
@@ -26,7 +26,7 @@ public class ArticleLikeService {
 //    private final ClubMemberRepository clubMemberRepository;
 
     @Transactional
-    public ArticleLikeInfoResponseDto toggleLike(Long articleId, Long memberId) {
+    public ArticleToggleLikeResponseDto toggleLike(Long articleId, Long memberId) {
         Member member = findMemberOrThrow(memberId);
         Article article = findArticleOrThrow(articleId);
 //        ClubMember clubMember = findClubMemberOrThrow(clubMemberId);
@@ -39,11 +39,9 @@ public class ArticleLikeService {
             ArticleLike articleLike = ArticleLike.createArticleLike(article, member);
             articleLikeRepository.save(articleLike);
         }
-
         boolean isLiked = checkLiked.isEmpty();
-        List<ArticleLike> articleLikes = articleLikeRepository.findAllByArticleId(article.getId());
 
-        return ArticleLikeInfoResponseDto.toDto(article.getId(), isLiked, articleLikes);
+        return ArticleToggleLikeResponseDto.toDto(article.getId(), isLiked);
     }
 
     public ArticleLikeMemberListResponseDto getArticleLikedMembers(Long articleId) {
