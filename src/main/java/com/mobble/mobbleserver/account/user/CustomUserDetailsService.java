fbe410747454e0baaeb8f72ca63fb22a4641a76d
taejunUM <utj0411@naver.com>
2025-07-15 20:32:06 +0900

    feat: CustomUserDetailsService 구현

---
+++ src/main/java/com/mobble/mobbleserver/account/user/CustomUserDetailsService.java
@@ -0,0 +1,25 @@
+package com.mobble.mobbleserver.account.user;
+
+import com.mobble.mobbleserver.domain.member.entity.Member;
+import com.mobble.mobbleserver.domain.member.validator.MemberValidator;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@RequiredArgsConstructor
+@Transactional(readOnly = true)
+public class CustomUserDetailsService implements UserDetailsService {
+
+    private final MemberValidator memberValidator;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        Long memberId = Long.parseLong(username);
+        Member member = memberValidator.findMemberByMemberIdOrThrow(memberId);
+        return new CustomUserDetails(member);
+    }
+}
