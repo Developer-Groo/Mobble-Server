59de1e42ea5fe0ec4e19f8f1dc7ec41125a5702c
Groo <woohyeon3382@naver.com>
2025-07-16 16:28:42 +0900

    refactor: ClubChatRoomRepository 참조 에티티를 ClubChatRoom 으로 수정

--- src/main/java/com/mobble/mobbleserver/domain/chat/clubChatRoom/repository/ClubChatRoomRepository.java
+++ src/main/java/com/mobble/mobbleserver/domain/chat/clubChatRoom/repository/ClubChatRoomRepository.java
@@ -1,7 +1,7 @@
 package com.mobble.mobbleserver.domain.chat.clubChatRoom.repository;
 
-import com.mobble.mobbleserver.domain.chat.chatRoom.entity.ChatRoom;
+import com.mobble.mobbleserver.domain.chat.clubChatRoom.entity.ClubChatRoom;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface ClubChatRoomRepository extends JpaRepository<ChatRoom, Long> {
+public interface ClubChatRoomRepository extends JpaRepository<ClubChatRoom, Long> {
 }
