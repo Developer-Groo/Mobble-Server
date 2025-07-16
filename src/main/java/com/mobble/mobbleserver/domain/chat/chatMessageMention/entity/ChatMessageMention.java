fe1b53abb16fb6dbbf459cc862013169db548cd2
Groo <woohyeon3382@naver.com>
2025-07-16 17:13:26 +0900

    feat: ChatMessageMention 엔티티 구현

---
+++ src/main/java/com/mobble/mobbleserver/domain/chat/chatMessageMention/entity/ChatMessageMention.java
@@ -0,0 +1,41 @@
+package com.mobble.mobbleserver.domain.chat.chatMessageMention.entity;
+
+import com.mobble.mobbleserver.domain.chat.chatMessage.entity.ChatMessage;
+import com.mobble.mobbleserver.domain.member.entity.Member;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@Entity
+@RequiredArgsConstructor(access = AccessLevel.PROTECTED)
+public class ChatMessageMention {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "chat_message_mention_id")
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "chat_message_id", nullable = false)
+    private ChatMessage chatMessage;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "mentioned_member_id", nullable = false)
+    private Member mentionedMember;
+
+    @Builder(access = AccessLevel.PRIVATE)
+    private ChatMessageMention(ChatMessage chatMessage, Member mentionedMember) {
+        this.chatMessage = chatMessage;
+        this.mentionedMember = mentionedMember;
+    }
+
+    public static ChatMessageMention createChatMessageMention(ChatMessage chatMessage, Member mentionedMember) {
+        return ChatMessageMention.builder()
+                .chatMessage(chatMessage)
+                .mentionedMember(mentionedMember)
+                .build();
+    }
+}
