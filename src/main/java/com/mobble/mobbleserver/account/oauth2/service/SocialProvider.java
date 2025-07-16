3302d8045479ece871b89b159bd6f0682307907c
taejunUM <utj0411@naver.com>
2025-07-16 20:45:36 +0900

    feat: SocialProvider 구현

---
+++ src/main/java/com/mobble/mobbleserver/account/oauth2/service/SocialProvider.java
@@ -0,0 +1,40 @@
+package com.mobble.mobbleserver.account.oauth2.service;
+
+import com.mobble.mobbleserver.account.oauth2.dto.response.GoogleUserInfoResponse;
+import com.mobble.mobbleserver.account.oauth2.dto.response.KakaoUserInfoResponse;
+import com.mobble.mobbleserver.account.oauth2.dto.response.NaverUserInfoResponse;
+import com.mobble.mobbleserver.account.oauth2.dto.response.OAuth2UserInfo;
+
+import java.util.Map;
+
+public enum SocialProvider {
+    KAKAO {
+        @Override
+        public OAuth2UserInfo getOAuth2UserInfo(Map<String, Object> attributes) {
+            return new KakaoUserInfoResponse(attributes);
+        }
+    },
+
+    NAVER {
+        @Override
+        public OAuth2UserInfo getOAuth2UserInfo(Map<String, Object> attributes) {
+            return new NaverUserInfoResponse(attributes);
+        }
+    },
+
+    GOOGLE {
+        @Override
+        public OAuth2UserInfo getOAuth2UserInfo(Map<String, Object> attributes) {
+            return new GoogleUserInfoResponse(attributes);
+        }
+    };
+
+    /**
+     * 각 프로바이더별로 구현 할 추상 메서드
+     */
+    public abstract OAuth2UserInfo getOAuth2UserInfo(Map<String, Object> attributes);
+
+    public static SocialProvider fromString(String providerName) {
+        return SocialProvider.valueOf(providerName.toUpperCase());
+    }
+}
