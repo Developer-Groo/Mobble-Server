fd22289554be8e3cb2fa6af401056193ac1ad60a
taejunUM <utj0411@naver.com>
2025-07-16 20:44:22 +0900

    feat: OAuth2UserInfo 구현체 생성
    - Kakao, Naver, Google

---
+++ src/main/java/com/mobble/mobbleserver/account/oauth2/dto/response/GoogleUserInfoResponse.java
@@ -0,0 +1,36 @@
+package com.mobble.mobbleserver.account.oauth2.dto.response;
+
+import com.mobble.mobbleserver.account.oauth2.service.SocialProvider;
+
+import java.util.Map;
+
+public class GoogleUserInfoResponse implements OAuth2UserInfo {
+
+    private final Map<String, Object> attributes;
+
+    private final SocialProvider socialProvider = SocialProvider.GOOGLE;
+
+    public GoogleUserInfoResponse(Map<String, Object> attributes) {
+        this.attributes = attributes;
+    }
+
+    @Override
+    public SocialProvider getProvider() {
+        return socialProvider;
+    }
+
+    @Override
+    public String getProviderId() {
+        return attributes.get("sub").toString();
+    }
+
+    @Override
+    public String getName() {
+        return attributes.get("name").toString();
+    }
+
+    @Override
+    public String getEmail() {
+        return attributes.get("email").toString();
+    }
+}
