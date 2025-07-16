3a9331ba2cc4f8d143d3c70c93950821f3fb0b6e
taejunUM <utj0411@naver.com>
2025-07-16 20:45:59 +0900

    refactor: CustomUserDetails 리팩토링

--- src/main/java/com/mobble/mobbleserver/account/user/CustomUserDetails.java
+++ src/main/java/com/mobble/mobbleserver/account/user/CustomUserDetails.java
@@ -2,46 +2,51 @@ package com.mobble.mobbleserver.account.user;
 
 import com.mobble.mobbleserver.domain.member.entity.Member;
 import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.oauth2.core.user.OAuth2User;
 
 import java.util.Collection;
-import java.util.Collections;
+import java.util.Map;
 
-public record CustomUserDetails(Member member) implements UserDetails {
+public record CustomUserDetails(Member member, boolean isNewUser, Map<String, Object> attributes) implements UserDetails, OAuth2User {
+
+
+    @Override
+    public String getName() {
+        if (isNewUser) {
+            return attributes.get("name").toString();
+        }
+        return member.getName();
+    }
+
+    @Override
+    public Map<String, Object> getAttributes() {
+        return attributes;
+    }
 
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
-        return Collections.singletonList(new SimpleGrantedAuthority("ROLE_USER"));
+        return null;
     }
 
     @Override
     public String getPassword() {
-        return member.getPassword();
+        return null;
     }
 
     @Override
     public String getUsername() {
-        return member.getId().toString();
-    }
-
-    @Override
-    public boolean isAccountNonExpired() {
-        return true;
-    }
-
-    @Override
-    public boolean isAccountNonLocked() {
-        return true;
-    }
-
-    @Override
-    public boolean isCredentialsNonExpired() {
-        return true;
+        if (isNewUser) {
+            return attributes.get("email").toString();
+        }
+        return member.getEmail();
     }
 
     @Override
     public boolean isEnabled() {
-        return true;
+        if (isNewUser) {
+            return true;
+        }
+        return !member.isDeleted();
     }
 }
