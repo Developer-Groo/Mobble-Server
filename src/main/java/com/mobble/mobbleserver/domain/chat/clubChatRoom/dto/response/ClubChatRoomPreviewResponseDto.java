6ab9dac8faa4ddd3b0cbea5699eb77f67315e323
Groo <woohyeon3382@naver.com>
2025-07-17 21:59:11 +0900

    feat: ClubChatRoomPreviewResponseDto 구현

---
+++ src/main/java/com/mobble/mobbleserver/domain/chat/clubChatRoom/dto/response/ClubChatRoomPreviewResponseDto.java
@@ -0,0 +1,13 @@
+package com.mobble.mobbleserver.domain.chat.clubChatRoom.dto.response;
+
+import java.time.LocalDateTime;
+
+public record ClubChatRoomPreviewResponseDto(
+        Long chatRoomId,
+        Long clubId,
+        String clubName,
+        String lastMessage,
+        LocalDateTime lastMessageTime,
+        int unreadCount
+) {
+}
