5cb43e63bb3e67428f4aa9c2c0c7049e17cdbd7c
Groo <woohyeon3382@naver.com>
2025-07-17 20:56:20 +0900

    feat: sendDirectMessage 메서드 구현

---
+++ src/main/java/com/mobble/mobbleserver/domain/chat/directChatRoom/service/DirectChatRoomService.java
@@ -0,0 +1,35 @@
+package com.mobble.mobbleserver.domain.chat.directChatRoom.service;
+
+import com.mobble.mobbleserver.domain.chat.directChatRoom.dto.request.DirectChatRoomRequestDto;
+import com.mobble.mobbleserver.domain.chat.directChatRoom.dto.response.DirectChatRoomResponseDto;
+import com.mobble.mobbleserver.domain.member.entity.Member;
+import com.mobble.mobbleserver.domain.member.validator.MemberValidator;
+import lombok.RequiredArgsConstructor;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+
+@Service
+@RequiredArgsConstructor
+public class DirectChatRoomService {
+
+    private final SimpMessagingTemplate messagingTemplate;
+    private final MemberValidator memberValidator;
+
+    public void sendDirectMessage(DirectChatRoomRequestDto dto, Long memberId) {
+        Member member = memberValidator.findMemberByMemberIdOrThrow(memberId);
+        Member targetMember = memberValidator.findMemberByMemberIdOrThrow(dto.receiverId());
+
+        DirectChatRoomResponseDto response = DirectChatRoomResponseDto.toDto(
+                dto.chatRoomId(),
+                dto.content(),
+                dto.type(),
+                member.getId(),
+                member.getName(),
+                LocalDateTime.now()
+        );
+
+        messagingTemplate.convertAndSendToUser(targetMember.getEmail(), "/queue/direct/chatroom/" + dto.chatRoomId(), response);
+    }
+}
