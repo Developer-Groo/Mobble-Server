d89bbdd7e77f84ecf101cd020f4726df2e2ad088
Groo <woohyeon3382@naver.com>
2025-07-18 19:38:28 +0900

    feat: createClubChatRoom 메서드 시그니처 구현

--- src/main/java/com/mobble/mobbleserver/domain/chat/clubChatRoom/service/ClubChatRoomService.java
+++ src/main/java/com/mobble/mobbleserver/domain/chat/clubChatRoom/service/ClubChatRoomService.java
@@ -3,7 +3,7 @@ package com.mobble.mobbleserver.domain.chat.clubChatRoom.service;
 import com.mobble.mobbleserver.domain.chat.chatMessage.dto.response.ChatMessageResponseDto;
 import com.mobble.mobbleserver.domain.chat.clubChatRoom.dto.response.ClubChatRoomPreviewResponseDto;
 import com.mobble.mobbleserver.domain.chat.clubChatRoom.dto.request.ClubChatRoomRequestDto;
-import com.mobble.mobbleserver.domain.chat.clubChatRoom.dto.response.ClubChatRoomResponseDto;
+import com.mobble.mobbleserver.domain.chat.clubChatRoom.dto.response.ClubChatMessageResponseDto;
 import com.mobble.mobbleserver.domain.member.entity.Member;
 import com.mobble.mobbleserver.domain.member.validator.MemberValidator;
 import lombok.RequiredArgsConstructor;
@@ -26,7 +26,7 @@ public class ClubChatRoomService {
     public void sendGroupMessage(ClubChatRoomRequestDto dto, Long memberId) {
         Member member = memberValidator.findMemberByMemberIdOrThrow(memberId);
 
-        ClubChatRoomResponseDto response = ClubChatRoomResponseDto.toDto(
+        ClubChatMessageResponseDto response = ClubChatMessageResponseDto.toDto(
                 dto.chatRoomId(),
                 dto.content(),
                 dto.type(),
@@ -36,6 +36,19 @@ public class ClubChatRoomService {
         );
 
         messagingTemplate.convertAndSend("/topic/group/chatroom/" + dto.chatRoomId(), response);
+    }
+
+    public ClubChatRoomPreviewResponseDto createClubChatRoom(Long clubId, Long memberId) {
+        // Todo: club 과 member 는 해당 메서드를 호출하는 createClub 에서 이미 객체를 가지고 있음(고민 필요)
+        
+        // Todo: clubId 로 Club 엔티티 조회
+        // Todo: Chatroom 엔티티 생성 후 저장
+        // Todo: Member 엔티티 조회(연관 관계 주입)
+        // Todo: ChatRoomParticipant 엔티티 생성 후 저장
+        // Todo: ClubChatRoom 엔티티 생성(연관 관계 주입)
+        // Todo: ClubChatRoom 저장
+
+        return null;
     }
 
     public List<ClubChatRoomPreviewResponseDto> getClubChatRooms(Long memberId) {
