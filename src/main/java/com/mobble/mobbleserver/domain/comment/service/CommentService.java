300a7cf633116d5a7a971f6ee5ef2749b9dc127b
Hojin02 <140497846+Hojin02@users.noreply.github.com>
2025-07-21 21:13:28 +0900

    feat: Article에 대한 전체 댓글 수 조회하는 로직 추가

--- src/main/java/com/mobble/mobbleserver/domain/comment/service/CommentService.java
+++ src/main/java/com/mobble/mobbleserver/domain/comment/service/CommentService.java
@@ -100,4 +100,13 @@ public class CommentService {
 
         return commentRepository.findLikeInfoByCommentIdsAndMemberId(commentIds, memberId);
     }
+
+    public Map<Long, Integer> getArticleCommentCount(List<Article> articles) {
+        List<Long> articleIds = articles.stream()
+                .map(Article::getId)
+                .distinct()
+                .toList();
+
+        return commentRepository.countCommentsByArticleIds(articleIds);
+    }
 }
