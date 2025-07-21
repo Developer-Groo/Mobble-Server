25d72d342e50d6f7f04b039c01796f40802f923c
taejunUM <utj0411@naver.com>
2025-07-21 14:24:47 +0900

    feat: AuthService 구현
    - 회원가입 서비스

---
+++ src/main/java/com/mobble/mobbleserver/account/auth/service/AuthService.java
@@ -0,0 +1,27 @@
+package com.mobble.mobbleserver.account.auth.service;
+
+import com.mobble.mobbleserver.account.auth.dto.request.SignUpRequestDto;
+import com.mobble.mobbleserver.domain.member.entity.Member;
+import com.mobble.mobbleserver.domain.member.repository.MemberRepository;
+import com.mobble.mobbleserver.domain.member.validator.MemberValidator;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@RequiredArgsConstructor
+@Transactional(readOnly = true)
+public class AuthService {
+
+    private final MemberRepository memberRepository;
+    private final MemberValidator memberValidator;
+
+    @Transactional
+    public Member registerNewUser(SignUpRequestDto dto) {
+        memberValidator.exitsEmailOrThrow(dto.email());
+        memberValidator.existsIsDeletedEmailOrThrow(dto.email());
+        Member newMember = dto.toEntity();
+
+        return memberRepository.save(newMember);
+    }
+}
