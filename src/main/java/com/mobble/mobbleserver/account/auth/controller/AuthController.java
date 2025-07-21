ff9a74a3eced914c965fe04e45fdd3b13b14e5b8
taejunUM <utj0411@naver.com>
2025-07-21 14:23:20 +0900

    feat: AuthController 구현
    - 회원가입&로그인 컨트롤러

---
+++ src/main/java/com/mobble/mobbleserver/account/auth/controller/AuthController.java
@@ -0,0 +1,28 @@
+package com.mobble.mobbleserver.account.auth.controller;
+
+import com.mobble.mobbleserver.account.auth.dto.request.SignUpRequestDto;
+import com.mobble.mobbleserver.account.auth.service.AuthService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/signup")
+public class AuthController {
+
+    private final AuthService authService;
+
+    @PostMapping
+    public ResponseEntity<String> loginAndSignup(
+            @ModelAttribute SignUpRequestDto dto
+    ) {
+        authService.registerNewUser(dto);
+        return ResponseEntity.status(HttpStatus.OK)
+                .body("회원가입이 완료되었습니다.");
+    }
+}
