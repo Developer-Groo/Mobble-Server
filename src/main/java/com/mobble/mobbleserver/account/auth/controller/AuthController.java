5fa34cb907fb12282aec094e5158d3845f2c197e
taejunUM <utj0411@naver.com>
2025-07-21 15:13:47 +0900

    refactor: Valid 어노테이션 추가

--- src/main/java/com/mobble/mobbleserver/account/auth/controller/AuthController.java
+++ src/main/java/com/mobble/mobbleserver/account/auth/controller/AuthController.java
@@ -2,14 +2,17 @@ package com.mobble.mobbleserver.account.auth.controller;
 
 import com.mobble.mobbleserver.account.auth.dto.request.SignUpRequestDto;
 import com.mobble.mobbleserver.account.auth.service.AuthService;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Validated
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/signup")
@@ -19,7 +22,7 @@ public class AuthController {
 
     @PostMapping
     public ResponseEntity<String> signUp(
-            @ModelAttribute SignUpRequestDto dto
+            @ModelAttribute @Valid SignUpRequestDto dto
     ) {
         authService.registerNewUser(dto);
         return ResponseEntity.status(HttpStatus.CREATED)
