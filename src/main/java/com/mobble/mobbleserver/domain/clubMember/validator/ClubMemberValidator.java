b6714d748a2a71aa42f9e138deafd32068ec4eac
Groo <woohyeon3382@naver.com>
2025-07-22 18:15:45 +0900

    feat: ClubMemberValidator 메서드 추가
    findAllClubMemberByMemberId 메서드

--- src/main/java/com/mobble/mobbleserver/domain/clubMember/validator/ClubMemberValidator.java
+++ src/main/java/com/mobble/mobbleserver/domain/clubMember/validator/ClubMemberValidator.java
@@ -5,6 +5,8 @@ import com.mobble.mobbleserver.domain.clubMember.repository.ClubMemberRepository
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 
+import java.util.List;
+
 @Component
 @RequiredArgsConstructor
 public class ClubMemberValidator {
@@ -14,5 +16,9 @@ public class ClubMemberValidator {
     public ClubMember findClubMemberByClubIdAndMemberIdOrThrow(Long clubId, Long memberId) {
         return clubMemberRepository.findClubMemberByClubIdAndMemberId(clubId, memberId)
                 .orElseThrow(() -> new IllegalArgumentException(""));
+    }
+
+    public List<ClubMember> findAllClubMemberByMemberId(Long memberId) {
+        return clubMemberRepository.findAllClubMemberByMemberId(memberId);
     }
 }
