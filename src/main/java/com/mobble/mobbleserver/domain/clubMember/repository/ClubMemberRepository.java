a38e399cb2c7a47bb184f25532e8f0983e385f3f
Groo <woohyeon3382@naver.com>
2025-07-22 18:15:18 +0900

    feat: ClubMemberRepositoryImpl 구현

--- src/main/java/com/mobble/mobbleserver/domain/clubMember/repository/ClubMemberRepository.java
+++ src/main/java/com/mobble/mobbleserver/domain/clubMember/repository/ClubMemberRepository.java
@@ -5,7 +5,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
 
-public interface ClubMemberRepository extends JpaRepository<ClubMember, Long> {
+public interface ClubMemberRepository extends JpaRepository<ClubMember, Long>, ClubMemberQueryRepository {
 
     Optional<ClubMember> findClubMemberByClubIdAndMemberId(Long clubId, Long memberId);
 }
