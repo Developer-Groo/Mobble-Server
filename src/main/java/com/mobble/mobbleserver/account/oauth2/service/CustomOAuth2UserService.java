90e4ed46369e977cf2a920aefe3e60465e43c81b
taejunUM <utj0411@naver.com>
2025-07-24 17:19:52 +0900

    style: 공백 제거

--- src/main/java/com/mobble/mobbleserver/account/oauth2/service/CustomOAuth2UserService.java
+++ src/main/java/com/mobble/mobbleserver/account/oauth2/service/CustomOAuth2UserService.java
@@ -28,8 +28,7 @@ public class CustomOAuth2UserService extends DefaultOAuth2UserService {
     @Transactional
     public OAuth2User loadUser(OAuth2UserRequest userRequest) throws OAuth2AuthenticationException {
         OAuth2User oAuth2User = delegate.loadUser(userRequest);
-
-
+        
         String registrationId = userRequest
                 .getClientRegistration()
                 .getRegistrationId();
