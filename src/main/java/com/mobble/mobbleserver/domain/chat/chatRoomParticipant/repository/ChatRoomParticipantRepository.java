244d751fe75829a2ab01e98c7133dc6d76e51af4
Groo <woohyeon3382@naver.com>
2025-07-25 01:39:35 +0900

    feat: ChatRoomParticipantQueryRepository 정의

--- src/main/java/com/mobble/mobbleserver/domain/chat/chatRoomParticipant/repository/ChatRoomParticipantRepository.java
+++ src/main/java/com/mobble/mobbleserver/domain/chat/chatRoomParticipant/repository/ChatRoomParticipantRepository.java
@@ -3,5 +3,5 @@ package com.mobble.mobbleserver.domain.chat.chatRoomParticipant.repository;
 import com.mobble.mobbleserver.domain.chat.chatRoomParticipant.entity.ChatRoomParticipant;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface ChatRoomParticipantRepository extends JpaRepository<ChatRoomParticipant, Long> {
+public interface ChatRoomParticipantRepository extends JpaRepository<ChatRoomParticipant, Long>, ChatRoomParticipantQueryRepository {
 }
