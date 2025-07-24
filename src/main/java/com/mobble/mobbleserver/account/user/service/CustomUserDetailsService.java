f3be1a1113cca41f58e556e620c537535f2fbb65
taejunUM <utj0411@naver.com>
2025-07-24 18:05:00 +0900

    refactor: 예외처리 수정
    - DomainException으로 감싸서 처리

--- src/main/java/com/mobble/mobbleserver/account/user/service/CustomUserDetailsService.java
+++ src/main/java/com/mobble/mobbleserver/account/user/service/CustomUserDetailsService.java
@@ -4,6 +4,7 @@ import com.mobble.mobbleserver.account.user.CustomUserDetails;
 import com.mobble.mobbleserver.domain.member.entity.Member;
 import com.mobble.mobbleserver.domain.member.validator.MemberValidator;
 import com.mobble.mobbleserver.global.exception.common.DomainException;
+import com.mobble.mobbleserver.global.exception.errorCode.account.AccountValidationErrorCode;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -26,11 +27,9 @@ public class CustomUserDetailsService implements UserDetailsService {
             Long memberId = Long.parseLong(username);
             Member member = memberValidator.findMemberByMemberIdOrThrow(memberId);
 
-            return new CustomUserDetails(member, false, Collections.emptyMap());
+            return CustomUserDetails.existingMember(member, Collections.emptyMap());
         } catch (NumberFormatException e) {
-            throw new UsernameNotFoundException("잘못된 사용자 ID 형식입니다: " + username);
-        } catch (DomainException e) {
-            throw new UsernameNotFoundException(e.getMessage());
+            throw new DomainException(AccountValidationErrorCode.WRONG_MEMBER_ID_TYPE);
         }
     }
 }
