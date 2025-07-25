a91b1424ec84265da6f604b7390b5079300ca16b
Groo <woohyeon3382@naver.com>
2025-07-25 20:21:24 +0900

    feat: findByChatRoomIdAndMemberId 메서드 시그니처 정의

--- src/main/java/com/mobble/mobbleserver/domain/chat/chatRoomParticipant/repository/ChatRoomParticipantRepository.java
+++ src/main/java/com/mobble/mobbleserver/domain/chat/chatRoomParticipant/repository/ChatRoomParticipantRepository.java
@@ -3,5 +3,9 @@ package com.mobble.mobbleserver.domain.chat.chatRoomParticipant.repository;
 import com.mobble.mobbleserver.domain.chat.chatRoomParticipant.entity.ChatRoomParticipant;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface ChatRoomParticipantRepository extends JpaRepository<ChatRoomParticipant, Long>, ChatRoomParticipantQueryRepository {
+
+    Optional<ChatRoomParticipant> findByChatRoomIdAndMemberId(Long chatRoomId, Long memberId);
 }
