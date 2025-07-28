6c015958eb6a5816499111605b5dd3ddb664def3
Hojin02 <140497846+Hojin02@users.noreply.github.com>
2025-07-28 14:48:19 +0900

    style: 공백수정

--- src/main/java/com/mobble/mobbleserver/domain/article/repository/ArticleRepository.java
+++ src/main/java/com/mobble/mobbleserver/domain/article/repository/ArticleRepository.java
@@ -7,5 +7,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
 
 public interface ArticleRepository extends JpaRepository<Article, Long>,  ArticleQueryDslRepository {
+    
     List<Article> findArticlesByClubId(Long clubId);
 }
