0023b4bb7f351f0aa0cab3a3ba170fa91277341a
taejunUM <utj0411@naver.com>
2025-07-29 17:55:24 +0900

    refactor: 소셜 로그인 서비스 로직 수정

--- src/main/java/com/mobble/mobbleserver/account/auth/service/AuthService.java
+++ src/main/java/com/mobble/mobbleserver/account/auth/service/AuthService.java
@@ -1,28 +1,41 @@
 package com.mobble.mobbleserver.account.auth.service;
 
-import com.mobble.mobbleserver.account.auth.dto.request.SignUpRequestDto;
-import com.mobble.mobbleserver.account.oauth2.service.SocialProvider;
+import com.mobble.mobbleserver.account.auth.dto.request.SocialLoginRequestDto;
+import com.mobble.mobbleserver.account.auth.dto.response.SocialLoginResponseDto;
+import com.mobble.mobbleserver.account.jwt.TokenProvider;
+import com.mobble.mobbleserver.account.oauth2.verifier.SocialUserInfo;
+import com.mobble.mobbleserver.account.oauth2.verifier.TokenVerifier;
+import com.mobble.mobbleserver.account.oauth2.verifier.TokenVerifierFactory;
 import com.mobble.mobbleserver.domain.member.entity.Member;
-import com.mobble.mobbleserver.domain.member.repository.MemberRepository;
 import com.mobble.mobbleserver.domain.member.validator.MemberValidator;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
 @Transactional(readOnly = true)
 public class AuthService {
 
-    private final MemberRepository memberRepository;
+    private final TokenVerifierFactory verifierFactory;
     private final MemberValidator memberValidator;
+    private final TokenProvider tokenProvider;
 
-    @Transactional
-    public Member registerNewMember(SignUpRequestDto dto) {
-        SocialProvider socialProvider = SocialProvider.fromString(dto.socialProvider());
-        memberValidator.validateSignUpEmailOrThrow(socialProvider, dto.socialId());
-        Member newMember = dto.toEntity();
+    public SocialLoginResponseDto socialLoginAndSignUp(SocialLoginRequestDto dto) {
+        TokenVerifier verifier = verifierFactory.getVerifier(dto.socialProvider());
+        SocialUserInfo userInfo = verifier.verify(dto.accessToken());
 
-        return memberRepository.save(newMember);
+        Optional<Member> optionalMember = memberValidator.findIsDeletedFalseMemberByProviderAndSocialId(userInfo.socialProvider(), userInfo.socialId());
+
+        if (optionalMember.isPresent()) {
+            Member member = optionalMember.get();
+            String accessToken = tokenProvider.createAccessToken(member.getId());
+
+            return SocialLoginResponseDto.toDto(accessToken);
+        }
+        //Todo 회원가입 로직
+        return null;
     }
 }
