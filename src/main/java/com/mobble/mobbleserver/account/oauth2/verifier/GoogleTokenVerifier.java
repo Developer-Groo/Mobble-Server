9534e200c13568770b2091910ab5bb2715b7421b
taejunUM <utj0411@naver.com>
2025-07-29 18:36:31 +0900

    feat: google 소셜 토큰 검증 구현체
    - google 소셜 accessToken 검증 & 사용자 정보 요청

---
+++ src/main/java/com/mobble/mobbleserver/account/oauth2/verifier/GoogleTokenVerifier.java
@@ -0,0 +1,40 @@
+package com.mobble.mobbleserver.account.oauth2.verifier;
+
+import com.mobble.mobbleserver.account.oauth2.dto.response.GoogleUserInfoResponse;
+import com.mobble.mobbleserver.global.exception.common.DomainException;
+import com.mobble.mobbleserver.global.exception.errorCode.oAuth2.OAuth2ErrorCode;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpHeaders;
+import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestClient;
+
+import java.util.Map;
+
+@Component
+@RequiredArgsConstructor
+public class GoogleTokenVerifier implements SocialVerifier {
+
+    private final RestClient googleRestClient;
+
+    @Override
+    public SocialUserInfo verify(String accessToken) {
+        Map<String, Object> response = googleRestClient.get()
+                .uri("/oauth2/v2/userinfo")
+                .header(HttpHeaders.AUTHORIZATION, "Bearer " + accessToken)
+                .retrieve()
+                .body(Map.class);
+
+        if (response == null || response.get("id") == null) {
+            throw new DomainException(OAuth2ErrorCode.NO_USER_INFO);
+        }
+
+        GoogleUserInfoResponse userInfo = new GoogleUserInfoResponse(response);
+
+        return new SocialUserInfo(
+                userInfo.getName(),
+                userInfo.getEmail(),
+                userInfo.getProvider(),
+                userInfo.getProviderId()
+        );
+    }
+}
