f5a0c4a1d2868cbb8c2340f3deba2a700f8b9497
taejunUM <utj0411@naver.com>
2025-07-29 17:55:03 +0900

    refactor: 로그인 성공 dto 수정

--- src/main/java/com/mobble/mobbleserver/account/auth/dto/response/SocialLoginResponseDto.java
+++ src/main/java/com/mobble/mobbleserver/account/auth/dto/response/SocialLoginResponseDto.java
@@ -3,4 +3,8 @@ package com.mobble.mobbleserver.account.auth.dto.response;
 public record SocialLoginResponseDto(
         String accessToken
 ) {
+
+    public static SocialLoginResponseDto toDto(String accessToken) {
+        return new SocialLoginResponseDto(accessToken);
+    }
 }
