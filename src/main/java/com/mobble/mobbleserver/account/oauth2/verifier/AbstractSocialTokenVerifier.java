796ba687b84313cd69935d44564853c9f9808bf5
taejunUM <utj0411@naver.com>
2025-08-03 06:02:17 +0900

    feat: TokenVerifier 추상클래스 도입

---
+++ src/main/java/com/mobble/mobbleserver/account/oauth2/verifier/AbstractSocialTokenVerifier.java
@@ -0,0 +1,38 @@
+package com.mobble.mobbleserver.account.oauth2.verifier;
+
+import com.mobble.mobbleserver.global.exception.common.DomainException;
+import com.mobble.mobbleserver.global.exception.errorCode.oAuth2.OAuth2ErrorCode;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.web.client.RestClient;
+
+import java.util.Map;
+
+@RequiredArgsConstructor
+public abstract class AbstractSocialTokenVerifier implements SocialVerifier {
+
+    private final RestClient restClient;
+
+    protected abstract String getUserInfoUri();
+
+    protected abstract SocialUserInfo parseUserInfo(Map<String, Object> attributes);
+
+    @Override
+    public SocialUserInfo verify(String accessToken) {
+        Map<String, Object> response;
+        try {
+            response = restClient.get()
+                    .uri(getUserInfoUri())
+                    .header(HttpHeaders.AUTHORIZATION, "Bearer " + accessToken)
+                    .retrieve()
+                    .onStatus(HttpStatusCode::isError, (req, res) -> {
+                        throw new DomainException(OAuth2ErrorCode.INVALID_ACCESS_TOKEN);
+                    })
+                    .body(Map.class);
+        } catch (Exception e) {
+            throw new DomainException(OAuth2ErrorCode.FAILED_TO_REQUEST_USER_INFO);
+        }
+        return parseUserInfo(response);
+    }
+}
