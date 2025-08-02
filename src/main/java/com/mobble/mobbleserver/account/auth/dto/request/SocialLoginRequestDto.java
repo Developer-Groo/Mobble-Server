6d777bf6bec86fc8eee56efdc793c418dbd5f78e
taejunUM <utj0411@naver.com>
2025-08-03 06:43:01 +0900

    style: 줄바꿈 수정

--- src/main/java/com/mobble/mobbleserver/account/auth/dto/request/SocialLoginRequestDto.java
+++ src/main/java/com/mobble/mobbleserver/account/auth/dto/request/SocialLoginRequestDto.java
@@ -2,8 +2,5 @@ package com.mobble.mobbleserver.account.auth.dto.request;
 
 import com.mobble.mobbleserver.account.auth.oauth.service.SocialProvider;
 
-public record SocialLoginRequestDto(
-        String accessToken,
-        SocialProvider socialProvider
-) {
+public record SocialLoginRequestDto(String accessToken, SocialProvider socialProvider) {
 }
