3c696f950e4aa86b6f051c3ef90f3ba44931d0fb
taejunUM <utj0411@naver.com>
2025-08-03 07:05:25 +0900

    refactor: socialProvider 리팩토링
    - 미사용 메서드 정리
    - fromString 메서드 @JsonCreator 추가

--- src/main/java/com/mobble/mobbleserver/account/auth/oauth/service/SocialProvider.java
+++ src/main/java/com/mobble/mobbleserver/account/auth/oauth/service/SocialProvider.java
@@ -1,39 +1,13 @@
 package com.mobble.mobbleserver.account.auth.oauth.service;
 
-import com.mobble.mobbleserver.account.auth.oauth.dto.response.GoogleUserInfoResponse;
-import com.mobble.mobbleserver.account.auth.oauth.dto.response.KakaoUserInfoResponse;
-import com.mobble.mobbleserver.account.auth.oauth.dto.response.NaverUserInfoResponse;
-import com.mobble.mobbleserver.account.auth.oauth.dto.response.OAuth2UserInfo;
-
-import java.util.Map;
+import com.fasterxml.jackson.annotation.JsonCreator;
 
 public enum SocialProvider {
-    KAKAO {
-        @Override
-        public OAuth2UserInfo getOAuth2UserInfo(Map<String, Object> attributes) {
-            return new KakaoUserInfoResponse(attributes);
-        }
-    },
+    KAKAO,
+    NAVER,
+    GOOGLE;
 
-    NAVER {
-        @Override
-        public OAuth2UserInfo getOAuth2UserInfo(Map<String, Object> attributes) {
-            return new NaverUserInfoResponse(attributes);
-        }
-    },
-
-    GOOGLE {
-        @Override
-        public OAuth2UserInfo getOAuth2UserInfo(Map<String, Object> attributes) {
-            return new GoogleUserInfoResponse(attributes);
-        }
-    };
-
-    /**
-     * 각 프로바이더별로 구현 할 추상 메서드
-     */
-    public abstract OAuth2UserInfo getOAuth2UserInfo(Map<String, Object> attributes);
-
+    @JsonCreator
     public static SocialProvider fromString(String socialProvider) {
         return SocialProvider.valueOf(socialProvider.toUpperCase());
     }
