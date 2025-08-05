bb877fe70690e10240492508f4814c985981c9e5
Hojin02 <140497846+Hojin02@users.noreply.github.com>
2025-08-05 12:09:51 +0900

    feat: Club Controller 클럽 생성 api구현

---
+++ src/main/java/com/mobble/mobbleserver/domain/club/controller/ClubController.java
@@ -0,0 +1,31 @@
+package com.mobble.mobbleserver.domain.club.controller;
+
+import com.mobble.mobbleserver.domain.club.dto.request.ClubRequestDto;
+import com.mobble.mobbleserver.domain.club.dto.response.ClubResponseDto;
+import com.mobble.mobbleserver.domain.club.service.ClubService;
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.Positive;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
+
+@Validated
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/clubs")
+public class ClubController {
+
+    private final ClubService clubService;
+
+    @PostMapping
+    public ResponseEntity<String> createClub(
+            @RequestBody @Valid ClubRequestDto dto
+    ) {
+        Long memberId = 1L; // Todo: 임시 member id
+        clubService.createClub(memberId, dto);
+
+        return ResponseEntity.status(HttpStatus.CREATED)
+                .body("CLUB:CREATED_SUCCESSFULLY");
+    }
