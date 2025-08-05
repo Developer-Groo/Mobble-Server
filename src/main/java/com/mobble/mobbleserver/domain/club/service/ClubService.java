6aad12c869169921a6b1e46074aba7b468c3d734
Hojin02 <140497846+Hojin02@users.noreply.github.com>
2025-08-05 12:13:50 +0900

    feat: Club 생성 서비스 로직 구

---
+++ src/main/java/com/mobble/mobbleserver/domain/club/service/ClubService.java
@@ -0,0 +1,20 @@
+    private final ClubRepository clubRepository;
+    private final ClubCategoryRepository clubCategoryRepository;
+    private final ClubMemberRepository clubMemberRepository;
+    private final ClubAgeGroupRepository clubAgeGroupRepository;
+    private final ClubValidator clubValidator;
+    private final MemberValidator memberValidator;
+    @Transactional
+    public void createClub(Long memberId, ClubRequestDto dto) {
+        ClubCategory category = findCategoryOrThrow(dto.category());
+        Member member = memberValidator.findMemberByMemberIdOrThrow(memberId);
+
+        Club club = dto.toEntity(category);
+        clubRepository.save(club);
+
+        ClubMember clubMember = ClubMember.createClubMember(member, club, ClubMemberRole.LEADER, true);
+        clubMemberRepository.save(clubMember);
+
+        List<ClubAgeGroup> ageGroups = createClubAgeGroups(club, dto.ageGroup());
+        clubAgeGroupRepository.saveAll(ageGroups);
+    }
