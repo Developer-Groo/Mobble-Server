bc16bd69cf7e21160519c52795cea395d56301a7
Hojin02 <140497846+Hojin02@users.noreply.github.com>
2025-08-05 12:18:05 +0900

    feat: Club Response Dto 구

---
+++ src/main/java/com/mobble/mobbleserver/domain/club/dto/response/ClubResponseDto.java
@@ -0,0 +1,46 @@
+package com.mobble.mobbleserver.domain.club.dto.response;
+
+import com.mobble.mobbleserver.domain.club.entity.Club;
+import com.mobble.mobbleserver.domain.club.repository.dto.ClubLikeInfoDto;
+import com.mobble.mobbleserver.domain.clubAgeGroup.entity.ClubAgeGroupType;
+import com.mobble.mobbleserver.domain.member.entity.Member;
+
+import java.util.List;
+
+public record ClubResponseDto(
+        Long id,
+        String leader,
+        String name,
+        String category,
+        String ground,
+        String address,
+        int headcount,
+        List<ClubAgeGroupType> ageGroup,
+//        String profileImage,
+//        List<String> infoImage,
+        int likeCount,
+        boolean liked,
+        boolean isAutoJoin
+) {
+
+    public static ClubResponseDto toDto(
+            Club club,
+            Member member,
+            List<ClubAgeGroupType> ageGroup,
+            ClubLikeInfoDto likeInfo
+    ){
+        return new ClubResponseDto(
+                club.getId(),
+                member.getName(),
+                club.getName(),
+                club.getClubCategory().getName(),
+                club.getGround(),
+                club.getAddress(),
+                club.getHeadCount(),
+                ageGroup,
+                likeInfo.likeCount(),
+                likeInfo.isLiked(),
+                club.isAutoJoin()
+        );
+    }
+}
