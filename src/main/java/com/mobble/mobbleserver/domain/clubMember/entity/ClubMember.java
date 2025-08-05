2eab3e6d62be20c8b3d9b9ff1d6aeca3536524cb
Hojin02 <140497846+Hojin02@users.noreply.github.com>
2025-08-05 16:50:07 +0900

    refactor: import 수정

--- src/main/java/com/mobble/mobbleserver/domain/clubMember/entity/ClubMember.java
+++ src/main/java/com/mobble/mobbleserver/domain/clubMember/entity/ClubMember.java
@@ -1,7 +1,7 @@
 package com.mobble.mobbleserver.domain.clubMember.entity;
 
 import com.mobble.mobbleserver.common.baseEntity.CreatedAtEntity;
-import com.mobble.mobbleserver.domain.club.entity.Club;
+import com.mobble.mobbleserver.domain.club.club.entity.Club;
 import com.mobble.mobbleserver.domain.member.entity.Member;
 import jakarta.persistence.*;
 import lombok.AccessLevel;
