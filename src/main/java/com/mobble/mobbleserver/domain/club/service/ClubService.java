b6b9c22055283bf2b7e1139b59b982b2fa7fa939
Hojin02 <140497846+Hojin02@users.noreply.github.com>
2025-08-05 12:42:49 +0900

    feat: 클럽 단건 조회 서비스 로직 구현

--- src/main/java/com/mobble/mobbleserver/domain/club/service/ClubService.java
+++ src/main/java/com/mobble/mobbleserver/domain/club/service/ClubService.java
@@ -18,9 +18,32 @@
         List<ClubAgeGroup> ageGroups = createClubAgeGroups(club, dto.ageGroup());
         clubAgeGroupRepository.saveAll(ageGroups);
     }
+
+    public ClubResponseDto findClubById(Long clubId, Long memberId) {
+        Club club = clubValidator.findClubByClubIdOrThrow(clubId);
+        ClubMember leader = clubMemberRepository
+                .findByClubIdAndClubMemberRole(clubId, ClubMemberRole.LEADER).get();
+        String leaderName = leader.getMember().getName();
+
+        Member member = memberValidator.findMemberByMemberIdOrThrow(memberId);
+
+        // todo: 로그인 한 사용자가 가입안됬을때 공개여부 처리 질문
+
+        return buildClubResponse(club, member, leaderName);
+    }
     private ClubCategory findCategoryOrThrow(String categoryName) {
         return clubCategoryRepository.findByName(categoryName)
                 .orElseThrow(() -> new DomainException(ClubErrorCode.CATEGORY_NOT_FOUND));
+    }
+
+    private ClubResponseDto buildClubResponse(Club club, Member member, String leaderName) {
+        List<ClubAgeGroupType> ageGroupList = clubAgeGroupRepository.findByClub(club).stream()
+                .map(ClubAgeGroup::getAgeGroupType)
+                .toList();
+
+        ClubLikeInfoDto likeInfo = clubRepository.findLikeInfoByClubIdAndMemberId(club.getId(), member.getId());
+
+        return ClubResponseDto.toDto(club, leaderName, ageGroupList, likeInfo);
     }
     private List<ClubAgeGroup> createClubAgeGroups(Club club, List<ClubAgeGroupType> ageGroupTypes) {
         return ageGroupTypes.stream()
