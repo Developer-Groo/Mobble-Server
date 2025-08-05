bf8c248c1cab6814c4003f7967ab0de18940a303
Hojin02 <140497846+Hojin02@users.noreply.github.com>
2025-08-05 16:08:33 +0900

    refactor: 생성자 변경에 따른 test코드 수정

--- src/test/java/com/mobble/mobbleserver/support/fixture/club/ClubTestFixture.java
+++ src/test/java/com/mobble/mobbleserver/support/fixture/club/ClubTestFixture.java
@@ -1,13 +1,18 @@
 package com.mobble.mobbleserver.support.fixture.club;
 
 import com.mobble.mobbleserver.domain.club.entity.Club;
+import com.mobble.mobbleserver.domain.clubCategory.entity.ClubCategory;
 
 public class ClubTestFixture {
 
     public static Club createDefaultClub() {
+        ClubCategory category = ClubCategory.createClubCategory("SOCCER");
+
         return Club.createClub(
+                category,
                 "name",
                 "ground",
+                "address",
                 1,
                 true
         );
