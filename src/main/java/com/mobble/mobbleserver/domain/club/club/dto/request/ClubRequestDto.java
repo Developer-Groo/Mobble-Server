1b53bb5efd92bc5e89127b484990f53fe0eca8ea
Hojin02 <140497846+Hojin02@users.noreply.github.com>
2025-08-06 09:12:51 +0900

    style: 주석제거

--- src/main/java/com/mobble/mobbleserver/domain/club/club/dto/request/ClubRequestDto.java
+++ src/main/java/com/mobble/mobbleserver/domain/club/club/dto/request/ClubRequestDto.java
@@ -10,7 +10,6 @@ import jakarta.validation.constraints.Size;
 import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.NotNull;
 
-
 import java.util.List;
 
 public record ClubRequestDto(
