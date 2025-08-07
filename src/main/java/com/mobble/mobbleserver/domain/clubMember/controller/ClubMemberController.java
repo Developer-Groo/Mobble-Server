b67d47242a19073bcef2dde051aeedb722636b14
Hojin02 <140497846+Hojin02@users.noreply.github.com>
2025-08-07 18:12:31 +0900

    feat: Club Member Controller 가입 신청 api 설계

---
+++ src/main/java/com/mobble/mobbleserver/domain/clubMember/controller/ClubMemberController.java
@@ -0,0 +1,33 @@
+package com.mobble.mobbleserver.domain.clubMember.controller;
+
+import com.mobble.mobbleserver.domain.clubMember.dto.request.UpdateClubMemberRoleDto;
+import com.mobble.mobbleserver.domain.clubMember.dto.request.UpdateClubMemberStatusDto;
+import com.mobble.mobbleserver.domain.clubMember.dto.response.ClubMemberResponseDto;
+import com.mobble.mobbleserver.domain.clubMember.dto.response.ClubMemberUpsertResponseDto;
+import com.mobble.mobbleserver.domain.clubMember.service.ClubMemberService;
+import jakarta.validation.constraints.Positive;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@Validated
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/clubs")
+public class ClubMemberController {
+
+    private final ClubMemberService clubMemberService;
+
+    @PostMapping("/join/{club-id}")
+    public ResponseEntity<ClubMemberUpsertResponseDto> joinClub(
+            @PathVariable("club-id") @Positive Long clubId
+    ) {
+        Long memberId = 2L; // Todo: 임시 member id
+
+        return ResponseEntity.status(HttpStatus.CREATED)
+                .body(clubMemberService.joinClub(memberId, clubId));
+    }
