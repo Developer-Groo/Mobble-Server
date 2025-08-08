1e34fd859f373bed77adea8af9ac391201732640
taejunUM <utj0411@naver.com>
2025-08-08 17:07:50 +0900

    feat: 좋아요 통합 컨트롤러 구현

---
+++ src/main/java/com/mobble/mobbleserver/domain/like/controller/LikeController.java
@@ -0,0 +1,47 @@
+package com.mobble.mobbleserver.domain.like.controller;
+
+import com.mobble.mobbleserver.domain.like.entity.LikeType;
+import com.mobble.mobbleserver.domain.like.articleLike.service.ArticleLikeService;
+import com.mobble.mobbleserver.domain.like.clubLike.service.ClubLikeService;
+import com.mobble.mobbleserver.domain.like.commentLike.service.CommentLikeService;
+import com.mobble.mobbleserver.domain.like.dto.response.LikeToggleResponseDto;
+import com.mobble.mobbleserver.domain.like.service.LikeDispatcherService;
+import com.mobble.mobbleserver.global.exception.common.DomainException;
+import com.mobble.mobbleserver.global.exception.errorCode.like.LikeErrorCode;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/likes")
+public class LikeController {
+
+    private final LikeDispatcherService likeDispatcherService;
+    private final ArticleLikeService articleLikeService;
+    private final CommentLikeService commentLikeService;
+    private final ClubLikeService clubLikeService;
+
+    @PostMapping
+    public ResponseEntity<LikeToggleResponseDto> toggleLike(
+            @RequestParam LikeType likeType,
+            @RequestParam Long targetId
+    ) {
+        Long memberId = 1L;
+        return ResponseEntity.status(HttpStatus.CREATED)
+                .body(likeDispatcherService.toggleLike(likeType, targetId, memberId));
+    }
+
+    @GetMapping("/members")
+    public ResponseEntity<?> getMemberList(
+            @RequestParam LikeType likeType,
+            @RequestParam Long targetId
+    ) {
+        return switch (likeType) {
+            case ARTICLE -> ResponseEntity.status(HttpStatus.OK)
+                    .body(articleLikeService.getArticleLikedMembers(targetId));
+            case CLUB, COMMENT -> throw new DomainException(LikeErrorCode.NOT_SUPPORTED_TYPE);
+        };
+    }
+}
