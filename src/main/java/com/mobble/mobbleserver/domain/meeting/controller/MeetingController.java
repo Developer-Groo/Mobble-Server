39a928aefdec78f2aec875b11b2923beb95e7461
taejunUM <utj0411@naver.com>
2025-08-14 04:30:16 +0900

    feat: 컨트롤러 미팅 수정 api 구현

--- src/main/java/com/mobble/mobbleserver/domain/meeting/controller/MeetingController.java
+++ src/main/java/com/mobble/mobbleserver/domain/meeting/controller/MeetingController.java
@@ -1,6 +1,7 @@
 package com.mobble.mobbleserver.domain.meeting.controller;
 
 import com.mobble.mobbleserver.domain.meeting.dto.request.MeetingRequestDto;
+import com.mobble.mobbleserver.domain.meeting.dto.request.MeetingUpdateRequestDto;
 import com.mobble.mobbleserver.domain.meeting.dto.response.MeetingResponseDto;
 import com.mobble.mobbleserver.domain.meeting.service.MeetingService;
 import jakarta.validation.constraints.Positive;
@@ -48,5 +49,15 @@ public class MeetingController {
 
         return ResponseEntity.status(HttpStatus.OK)
                 .body(meetingService.findMeetingById(meetingId, memberId));
+    }
+
+    @PatchMapping("/api/meetings/{meeting-id}")
+    public ResponseEntity<MeetingResponseDto> updateMeeting(
+            @PathVariable("meeting-id") @Positive Long meetingId,
+            @RequestBody MeetingUpdateRequestDto dto
+    ) {
+        Long memberId = 1L;
+        return ResponseEntity.status(HttpStatus.OK)
+                .body(meetingService.updateMeeting(memberId, meetingId, dto));
     }
 }
