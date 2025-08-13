259e373f631a1f2dad3509d5e4740b41f977fb0f
taejunUM <utj0411@naver.com>
2025-08-13 21:19:56 +0900

    feat: MeetingResponseDto 구현

--- src/main/java/com/mobble/mobbleserver/domain/meeting/dto/response/MeetingResponseDto.java
+++ src/main/java/com/mobble/mobbleserver/domain/meeting/dto/response/MeetingResponseDto.java
@@ -1,4 +1,32 @@
 package com.mobble.mobbleserver.domain.meeting.dto.response;
 
-public record MeetingResponseDto() {
+import com.mobble.mobbleserver.domain.meeting.entity.Meeting;
+import com.mobble.mobbleserver.domain.meeting.entity.MeetingType;
+
+import java.time.LocalDateTime;
+
+public record MeetingResponseDto(
+        Long meetingId,
+        Long clubId,
+        String title,
+        LocalDateTime dateTime,
+        String location,
+        String cost,
+        Integer memberLimit,
+        MeetingType type
+//        String dDay //Todo d-day 추가
+) {
+    public static MeetingResponseDto toDto(Meeting meeting) {
+        return new MeetingResponseDto(
+                meeting.getId(),
+                meeting.getClubMember().getClub().getId(),
+                meeting.getTitle(),
+                meeting.getDatetime(),
+                meeting.getLocation(),
+                meeting.getCost(),
+                meeting.getMemberLimit(),
+                meeting.getType()
+//                dDay
+        );
+    }
 }
