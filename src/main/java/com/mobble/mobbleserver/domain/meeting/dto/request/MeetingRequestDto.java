0235bebded11f291e435c0edc60c1e20d7e49ecf
taejunUM <utj0411@naver.com>
2025-08-13 21:19:38 +0900

    feat: MeetingRequestDto 구현

--- src/main/java/com/mobble/mobbleserver/domain/meeting/dto/request/MeetingRequestDto.java
+++ src/main/java/com/mobble/mobbleserver/domain/meeting/dto/request/MeetingRequestDto.java
@@ -1,4 +1,30 @@
 package com.mobble.mobbleserver.domain.meeting.dto.request;
 
-public record MeetingRequestDto() {
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.mobble.mobbleserver.domain.clubMember.entity.ClubMember;
+import com.mobble.mobbleserver.domain.meeting.entity.Meeting;
+import com.mobble.mobbleserver.domain.meeting.entity.MeetingType;
+
+import java.time.LocalDateTime;
+
+public record MeetingRequestDto(
+        String title,
+        @JsonFormat(pattern = "yyyy-MM-dd'T'HH:mm")
+        LocalDateTime dateTime,
+        String location,
+        String cost,
+        Integer memberLimit,
+        MeetingType type
+) {
+    public Meeting toEntity(ClubMember host) {
+        return Meeting.createMeeting(
+                host,
+                this.title,
+                this.dateTime,
+                this.location,
+                this.cost,
+                this.memberLimit,
+                this.type
+        );
+    }
 }
