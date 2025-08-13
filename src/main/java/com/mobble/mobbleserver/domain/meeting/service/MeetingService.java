4596e7f19291069fdfa99cb259a09d977818abaf
taejunUM <utj0411@naver.com>
2025-08-13 21:19:22 +0900

    feat: 서비스 미팅 생성 구현

--- src/main/java/com/mobble/mobbleserver/domain/meeting/service/MeetingService.java
+++ src/main/java/com/mobble/mobbleserver/domain/meeting/service/MeetingService.java
@@ -1,5 +1,13 @@
 package com.mobble.mobbleserver.domain.meeting.service;
 
+import com.mobble.mobbleserver.domain.clubMember.entity.ClubMember;
+import com.mobble.mobbleserver.domain.clubMember.entity.ClubMemberRole;
+import com.mobble.mobbleserver.domain.clubMember.validator.ClubMemberValidator;
+import com.mobble.mobbleserver.domain.meeting.dto.request.MeetingRequestDto;
+import com.mobble.mobbleserver.domain.meeting.dto.response.MeetingResponseDto;
+import com.mobble.mobbleserver.domain.meeting.entity.Meeting;
+import com.mobble.mobbleserver.domain.meeting.repository.MeetingRepository;
+import com.mobble.mobbleserver.domain.member.validator.MemberValidator;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -8,4 +16,24 @@ import org.springframework.transaction.annotation.Transactional;
 @RequiredArgsConstructor
 @Transactional(readOnly = true)
 public class MeetingService {
+
+    private final MeetingRepository meetingRepository;
+    private final MemberValidator memberValidator;
+    private final ClubMemberValidator clubMemberValidator;
+
+    @Transactional
+    public MeetingResponseDto createMeeting(Long memberId, Long clubId, MeetingRequestDto dto) {
+        ClubMember host = clubMemberValidator.findClubMemberByClubIdAndMemberIdOrThrow(clubId, memberId);
+
+        ClubMemberRole clubMemberRole = host.getClubMemberRole();
+
+        if (clubMemberRole == ClubMemberRole.MEMBER) {
+            throw new IllegalArgumentException(""); //Todo 커스텀 예외 적용
+        }
+
+        Meeting meeting = dto.toEntity(host);
+        //Todo d-day 표시 추가
+
+        return MeetingResponseDto.toDto(meetingRepository.save(meeting));
+    }
 }
