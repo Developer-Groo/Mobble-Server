e804d19d5e000e4824672470de1ac5249e1ef7de
taejunUM <utj0411@naver.com>
2025-08-13 21:19:01 +0900

    feat: 컨트롤러 미팅 생성 api 구현

--- src/main/java/com/mobble/mobbleserver/domain/meeting/controller/MeetingController.java
+++ src/main/java/com/mobble/mobbleserver/domain/meeting/controller/MeetingController.java
@@ -1,11 +1,33 @@
 package com.mobble.mobbleserver.domain.meeting.controller;
 
+import com.mobble.mobbleserver.domain.meeting.dto.request.MeetingRequestDto;
+import com.mobble.mobbleserver.domain.meeting.dto.response.MeetingResponseDto;
+import com.mobble.mobbleserver.domain.meeting.service.MeetingService;
+import jakarta.validation.constraints.Positive;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+@Validated
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/api/meeting")
 public class MeetingController {
+
+    private final MeetingService meetingService;
+
+    @PostMapping("/api/clubs/{club-id}/meetings")
+    public ResponseEntity<MeetingResponseDto> createMeeting(
+            @PathVariable("club-id") @Positive Long clubId,
+            @RequestBody MeetingRequestDto dto
+    ) {
+        Long memberId = 1L;
+
+        return ResponseEntity.status(HttpStatus.CREATED)
+                .body(meetingService.createMeeting(memberId, clubId, dto));
+    }
 }
