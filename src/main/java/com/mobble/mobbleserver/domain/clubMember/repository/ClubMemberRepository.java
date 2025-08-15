bbb555f0a39c718c1354c3815ba8bd4628cbe5b8
taejunUM <utj0411@naver.com>
2025-08-16 04:02:22 +0900

    feat: ClubMemberRole List로 가져오는 메서드 추가

--- src/main/java/com/mobble/mobbleserver/domain/clubMember/repository/ClubMemberRepository.java
+++ src/main/java/com/mobble/mobbleserver/domain/clubMember/repository/ClubMemberRepository.java
@@ -5,7 +5,9 @@ import com.mobble.mobbleserver.domain.clubMember.entity.ClubMemberRole;
 import com.mobble.mobbleserver.domain.clubMember.entity.JoinStatus;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 
+import java.util.Collection;
 import java.util.List;
 import java.util.Optional;
 
@@ -21,4 +23,13 @@ public interface ClubMemberRepository extends JpaRepository<ClubMember, Long> {
     long countByClubIdAndJoinStatus(Long clubId, JoinStatus joinStatus);
 
     List<ClubMember> findByClubId(Long clubId);
+
+    @Query("""
+      select distinct cm.clubMemberRole
+      from ClubMember cm
+      where cm.member.id = :memberId
+        and cm.clubMemberRole in :roles
+        and cm.member.isDeleted = false
+    """)
+    List<ClubMemberRole> findDistinctRolesByMemberIdAndRoleIn(Long memberId, Collection<ClubMemberRole> roles);
 }
