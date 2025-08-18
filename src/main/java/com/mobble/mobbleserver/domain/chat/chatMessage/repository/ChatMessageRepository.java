73a3ab76979eadacddb9341450aa0d3631ef4a58
Groo <woohyeon3382@naver.com>
2025-08-18 19:04:30 +0900

    feat: deleteByChatRoomId 메서드 정의

--- src/main/java/com/mobble/mobbleserver/domain/chat/chatMessage/repository/ChatMessageRepository.java
+++ src/main/java/com/mobble/mobbleserver/domain/chat/chatMessage/repository/ChatMessageRepository.java
@@ -4,4 +4,6 @@ import com.mobble.mobbleserver.domain.chat.chatMessage.entity.ChatMessage;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ChatMessageRepository extends JpaRepository<ChatMessage, Long>, ChatMessageQueryRepository {
+
+    void deleteByChatRoomId(Long chatRoomId);
 }
