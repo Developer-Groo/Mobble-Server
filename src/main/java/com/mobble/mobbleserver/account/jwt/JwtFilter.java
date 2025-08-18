da54b1ce874765739f745c4b8b21898148ecb87c
taejunUM <utj0411@naver.com>
2025-08-18 20:12:35 +0900

    refactor: JwtFilter 리팩토링
    - JWT tokenVersion, DB tokenVersion 비교 검증
    - JWT tokenVersion이 이전 버전일 때 인증 실패 > 재로그인 흐름

--- src/main/java/com/mobble/mobbleserver/account/jwt/JwtFilter.java
+++ src/main/java/com/mobble/mobbleserver/account/jwt/JwtFilter.java
@@ -2,6 +2,7 @@ package com.mobble.mobbleserver.account.jwt;
 
 import com.mobble.mobbleserver.account.auth.principal.AuthMember;
 import com.mobble.mobbleserver.domain.clubMember.entity.ClubMemberRole;
+import com.mobble.mobbleserver.domain.member.validator.MemberValidator;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
@@ -21,6 +22,7 @@ import java.util.List;
 public class JwtFilter extends OncePerRequestFilter {
 
     private final TokenProvider tokenProvider;
+    private final MemberValidator memberValidator;
 
     private static final String AUTHORIZATION = "Authorization";
     private static final String BEARER = "Bearer ";
@@ -31,7 +33,18 @@ public class JwtFilter extends OncePerRequestFilter {
         String token = resolveToken(request);
 
         if (token != null && tokenProvider.validateToken(token)) {
-            Long memberId = tokenProvider.getMemberIdByJwtToken(token);
+            Long memberId = tokenProvider.getMemberIdByJwtToken(token); // memberId 추출
+
+            int tokenVersionFromToken = tokenProvider.getTokenVersionByJwtToken(token); // JWT Token Version
+            int currentVersion = memberValidator.findMemberByMemberIdOrThrow(memberId).getTokenVersion(); // DB Token Version
+
+            // 이전 버전의 토큰이므로 인증 실패 (401)
+            // 클라이언트 측에서는 이 응답을 받으면 JWT를 삭제하고 로그인 페이지로 리다이렉트하는 등의 처리 필요
+            if (tokenVersionFromToken < currentVersion) {
+                response.setStatus(HttpServletResponse.SC_UNAUTHORIZED); // 이전 버전 토큰인 경우 차단 (401 UnAuthorized)
+                return;
+            }
+
             List<ClubMemberRole> roles = tokenProvider.getRolesByJwtToken(token);
             Collection<? extends GrantedAuthority> authorities = roles.stream()
                     .map(clubMemberRole -> new SimpleGrantedAuthority(clubMemberRole.name()))
