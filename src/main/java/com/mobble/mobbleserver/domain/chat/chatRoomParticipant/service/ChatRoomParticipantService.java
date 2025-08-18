9c9812729fa5d725681db25f90dab852b4c8473a
Groo <woohyeon3382@naver.com>
2025-08-18 18:53:25 +0900

    refactor: updateLastReadMessage 메서드 member 단독 검증 제거

--- src/main/java/com/mobble/mobbleserver/domain/chat/chatRoomParticipant/service/ChatRoomParticipantService.java
+++ src/main/java/com/mobble/mobbleserver/domain/chat/chatRoomParticipant/service/ChatRoomParticipantService.java
@@ -4,8 +4,6 @@ import com.mobble.mobbleserver.domain.chat.chatMessage.entity.ChatMessage;
 import com.mobble.mobbleserver.domain.chat.chatMessage.validator.ChatMessageValidator;
 import com.mobble.mobbleserver.domain.chat.chatRoomParticipant.entity.ChatRoomParticipant;
 import com.mobble.mobbleserver.domain.chat.chatRoomParticipant.validator.ChatRoomParticipantValidator;
-import com.mobble.mobbleserver.domain.member.entity.Member;
-import com.mobble.mobbleserver.domain.member.validator.MemberValidator;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -17,15 +15,15 @@ public class ChatRoomParticipantService {
 
     private final ChatRoomParticipantValidator chatRoomParticipantValidator;
     private final ChatMessageValidator chatMessageValidator;
-    private final MemberValidator memberValidator;
 
     @Transactional
     public void updateLastReadMessage(Long memberId, Long chatRoomId, Long lastMessageId) {
-        Member member = memberValidator.findMemberByMemberIdOrThrow(memberId);
+        ChatRoomParticipant participant = chatRoomParticipantValidator.findParticipantByChatRoomIdAndMemberIdOrThrow(chatRoomId, memberId);
+        ChatMessage lastReadMessage = chatMessageValidator.findChatMessageByChatMessageIdOrThrow(lastMessageId);
 
-        ChatRoomParticipant participant = chatRoomParticipantValidator.findParticipantByChatRoomIdAndMemberIdOrThrow(chatRoomId, member.getId());
+        participant.updateLastReadMessage(lastReadMessage);
+    }
 
-        ChatMessage lastReadMessage = chatMessageValidator.findChatMessageByChatMessageId(lastMessageId);
 
         participant.updateLastReadMessage(lastReadMessage);
     }
