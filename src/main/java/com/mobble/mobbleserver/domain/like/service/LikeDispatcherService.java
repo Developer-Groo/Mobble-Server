0cbe119773fc86b95ecff076f872cd7cf4d9069d
taejunUM <utj0411@naver.com>
2025-08-22 03:23:26 +0900

    refactor: LikeDispatcherService 리팩토링

--- src/main/java/com/mobble/mobbleserver/domain/like/service/LikeDispatcherService.java
+++ src/main/java/com/mobble/mobbleserver/domain/like/service/LikeDispatcherService.java
@@ -2,9 +2,12 @@ package com.mobble.mobbleserver.domain.like.service;
 
 import com.mobble.mobbleserver.domain.like.dto.response.LikeToggleResponseDto;
 import com.mobble.mobbleserver.domain.like.entity.LikeType;
+import com.mobble.mobbleserver.domain.member.entity.Member;
+import com.mobble.mobbleserver.domain.member.validator.MemberValidator;
 import com.mobble.mobbleserver.global.exception.common.DomainException;
 import com.mobble.mobbleserver.global.exception.errorCode.like.LikeErrorCode;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.EnumMap;
 import java.util.List;
@@ -13,25 +16,31 @@ import java.util.function.Function;
 import java.util.stream.Collectors;
 
 @Service
+@Transactional(readOnly = true)
 public class LikeDispatcherService {
 
-    private final Map<LikeType, LikeStrategy> strategyMap;
+    private final MemberValidator memberValidator;
+    private final Map<LikeType, AbstractLikeService<?, ?>> strategyMap;
 
-    public LikeDispatcherService(List<LikeStrategy> strategies) {
-        this.strategyMap = strategies.stream()
+    public LikeDispatcherService(MemberValidator memberValidator, List<AbstractLikeService<?, ?>> services) {
+        this.memberValidator = memberValidator;
+        this.strategyMap = services.stream()
                 .collect(Collectors.toMap(
-                        LikeStrategy::getType,
+                        AbstractLikeService::getType,
                         Function.identity(),
                         (a, b) -> a,
                         () -> new EnumMap<>(LikeType.class)
                 ));
     }
 
+    @Transactional
     public LikeToggleResponseDto toggleLike(LikeType likeType, Long targetId, Long memberId) {
-        LikeStrategy strategy = strategyMap.get(likeType);
-        if (strategy == null) {
-            throw new DomainException(LikeErrorCode.NOT_SUPPORTED_TYPE);
-        }
-        return strategy.toggleLike(targetId, memberId);
+        Member member = memberValidator.findMemberByMemberIdOrThrow(memberId);
+
+        AbstractLikeService<?, ?> service = strategyMap.get(likeType);
+
+        if (service == null) throw new DomainException(LikeErrorCode.NOT_SUPPORTED_TYPE);
+
+        return service.toggleLike(targetId, member);
     }
 }
