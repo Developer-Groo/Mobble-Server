ef6f92a91f7516d4476ff620f752f47705743adf
taejunUM <utj0411@naver.com>
2025-08-22 14:59:14 +0900

    feat: 좋아요 멤버 조회 구현체 구현
    - ArticleLikedMemberList 조회

---
+++ src/main/java/com/mobble/mobbleserver/domain/like/articleLike/service/ArticleLikeQueryServiceImpl.java
@@ -0,0 +1,36 @@
+package com.mobble.mobbleserver.domain.like.articleLike.service;
+
+import com.mobble.mobbleserver.domain.article.entity.Article;
+import com.mobble.mobbleserver.domain.article.validator.ArticleValidator;
+import com.mobble.mobbleserver.domain.like.articleLike.dto.response.ArticleLikeMemberListResponseDto;
+import com.mobble.mobbleserver.domain.like.articleLike.entity.ArticleLike;
+import com.mobble.mobbleserver.domain.like.articleLike.repository.ArticleLikeRepository;
+import com.mobble.mobbleserver.domain.like.baseLike.entity.LikeType;
+import com.mobble.mobbleserver.domain.like.baseLike.service.LikeQueryService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+@Transactional(readOnly = true)
+public class ArticleLikeQueryServiceImpl implements LikeQueryService {
+
+    private final ArticleValidator articleValidator;
+    private final ArticleLikeRepository articleLikeRepository;
+
+    @Override
+    public LikeType getType() {
+        return LikeType.ARTICLE;
+    }
+
+    @Override
+    public ArticleLikeMemberListResponseDto getLikedMemberList(Long articleId) {
+        Article article = articleValidator.findArticleByArticleIdOrThrow(articleId);
+        List<ArticleLike> articleLikes = articleLikeRepository.findAllByArticleId(article.getId());
+
+        return ArticleLikeMemberListResponseDto.toDto(article.getId(), articleLikes);
+    }
+}
