33d91ea08e2d1edaa7a1af63c74830b859842eec
Groo <woohyeon3382@naver.com>
2025-08-23 20:18:08 +0900

    refactor: 파라미터 네이밍 변경

--- src/main/java/com/mobble/mobbleserver/domain/chat/directChatRoom/dto/response/DirectChatRoomPreviewResponseDto.java
+++ src/main/java/com/mobble/mobbleserver/domain/chat/directChatRoom/dto/response/DirectChatRoomPreviewResponseDto.java
@@ -1,13 +1,27 @@
 package com.mobble.mobbleserver.domain.chat.directChatRoom.dto.response;
 
+import com.mobble.mobbleserver.domain.chat.chatMessage.entity.ChatMessage;
+import com.mobble.mobbleserver.domain.chat.directChatRoom.entity.DirectChatRoom;
+
 import java.time.LocalDateTime;
 
 public record DirectChatRoomPreviewResponseDto(
         Long chatRoomId,
-        String opponentName,
-        String opponentProfileImageUrl,
+        String receiverName,
+        String receiverProfileImageUrl,
         String lastMessage,
         LocalDateTime lastMessageTime,
         int unreadCount
 ) {
+
+    public static DirectChatRoomPreviewResponseDto toDto(DirectChatRoom directChatRoom, ChatMessage lastMessage, int unreadCount) {
+        return new DirectChatRoomPreviewResponseDto(
+                directChatRoom.getChatRoom().getId(),
+                directChatRoom.getMemberB().getName(),
+                directChatRoom.getMemberB().getProfileImage(),
+                lastMessage.getContent(),
+                lastMessage.getCreatedAt(),
+                unreadCount
+        );
+    }
 }
