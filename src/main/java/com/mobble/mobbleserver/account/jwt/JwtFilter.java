9dc5aa10b9dadffafdf3b1704396d405f2e15d79
taejunUM <utj0411@naver.com>
2025-08-25 18:11:30 +0900

    refactor: JwtFilter tokenVersion 검증 제거

--- src/main/java/com/mobble/mobbleserver/account/jwt/JwtFilter.java
+++ src/main/java/com/mobble/mobbleserver/account/jwt/JwtFilter.java
@@ -42,18 +42,8 @@ public class JwtFilter extends OncePerRequestFilter {
             }
 
             Long memberId = Long.valueOf(subject); // memberId 추출
-
-            int tokenVersionFromToken = tokenProvider.getTokenVersionByJwtToken(token); // JWT Token Version
-            int currentVersion = memberValidator.findMemberByMemberIdOrThrow(memberId).getTokenVersion(); // DB Token Version
-
-            // 이전 버전의 토큰이므로 인증 실패 (401)
-            // 클라이언트 측에서는 이 응답을 받으면 JWT를 삭제하고 로그인 페이지로 리다이렉트하는 등의 처리 필요
-            if (tokenVersionFromToken < currentVersion) {
-                response.setStatus(HttpServletResponse.SC_UNAUTHORIZED); // 이전 버전 토큰인 경우 차단 (401 UnAuthorized)
-                return;
-            }
-
             List<ClubMemberRole> roles = tokenProvider.getRolesByJwtToken(token);
+            
             Collection<? extends GrantedAuthority> authorities = roles.stream()
                     .map(clubMemberRole -> new SimpleGrantedAuthority(clubMemberRole.name()))
                     .toList();
