580d763fe7cc736e4351107c6c043c5171da1235
Groo <woohyeon3382@naver.com>
2025-08-25 23:12:21 +0900

    feat: DirectChatRoomController 메서드 구현

--- src/main/java/com/mobble/mobbleserver/domain/chat/directChatRoom/controller/DirectChatRoomController.java
+++ src/main/java/com/mobble/mobbleserver/domain/chat/directChatRoom/controller/DirectChatRoomController.java
@@ -1,18 +1,23 @@
 package com.mobble.mobbleserver.domain.chat.directChatRoom.controller;
 
+import com.mobble.mobbleserver.domain.chat.chatMessage.dto.request.ChatMessageRequestDto;
+import com.mobble.mobbleserver.domain.chat.chatMessage.dto.response.ChatMessageResponseDto;
 import com.mobble.mobbleserver.domain.chat.directChatRoom.dto.request.DirectChatMessageRequestDto;
 import com.mobble.mobbleserver.domain.chat.directChatRoom.dto.request.DirectChatRoomCreateRequestDto;
 import com.mobble.mobbleserver.domain.chat.directChatRoom.dto.response.DirectChatRoomPreviewResponseDto;
 import com.mobble.mobbleserver.domain.chat.directChatRoom.service.DirectChatRoomService;
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.Positive;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.messaging.handler.annotation.MessageMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
+@Validated
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api")
@@ -37,5 +42,34 @@ public class DirectChatRoomController {
 
         return ResponseEntity.status(HttpStatus.CREATED)
                 .body(directChatRoomService.createDirectChatRoom(dto, memberId));
+    }
+
+    @GetMapping("/chat-rooms/direct")
+    public ResponseEntity<List<DirectChatRoomPreviewResponseDto>> getDirectChatRooms() {
+        Long memberId = 1L;
+
+        return ResponseEntity.status(HttpStatus.OK)
+                .body(directChatRoomService.getDirectChatRooms(memberId));
+    }
+
+    @GetMapping("/chat-rooms/messages")
+    public ResponseEntity<List<ChatMessageResponseDto>> getDirectChatRoomMessages(
+            @RequestBody @Valid ChatMessageRequestDto dto
+    ) {
+        Long memberId = 1L;
+
+        return ResponseEntity.status(HttpStatus.OK)
+                .body(directChatRoomService.getDirectChatRoomMessages(memberId, dto));
+    }
+
+    @DeleteMapping("/chat-rooms/{chat-room-id}/direct/leave")
+    public ResponseEntity<Void> leaveDirectChatRoom(
+            @PathVariable("chat-room-id") @Positive Long chatRoomId
+    ) {
+        Long memberId = 1L;
+        directChatRoomService.leaveDirectChatRoom(chatRoomId, memberId);
+
+        return ResponseEntity.status(HttpStatus.NO_CONTENT)
+                .build();
     }
 }
