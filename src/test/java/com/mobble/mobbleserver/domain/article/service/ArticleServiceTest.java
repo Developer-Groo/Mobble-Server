1a3c0fc2f79719deead0572e4a17f7ccf241b7f0
Hojin02 <140497846+Hojin02@users.noreply.github.com>
2025-08-25 18:21:50 +0900

    test: Article Service Test 클래스 생성

---
+++ src/test/java/com/mobble/mobbleserver/domain/article/service/ArticleServiceTest.java
@@ -0,0 +1,70 @@
+package com.mobble.mobbleserver.domain.article.service;
+
+import com.mobble.mobbleserver.domain.article.dto.request.ArticleRequestDto;
+import com.mobble.mobbleserver.domain.article.dto.response.ArticleResponseDto;
+import com.mobble.mobbleserver.domain.article.dto.response.ArticleSummaryResponseDto;
+import com.mobble.mobbleserver.domain.article.entity.Article;
+import com.mobble.mobbleserver.domain.article.entity.ArticleType;
+import com.mobble.mobbleserver.domain.article.repository.ArticleRepository;
+import com.mobble.mobbleserver.domain.article.repository.dto.ArticleLikeInfoDto;
+import com.mobble.mobbleserver.domain.article.validator.ArticleValidator;
+import com.mobble.mobbleserver.domain.club.club.entity.Club;
+import com.mobble.mobbleserver.domain.club.club.validator.ClubValidator;
+import com.mobble.mobbleserver.domain.clubMember.entity.ClubMember;
+import com.mobble.mobbleserver.domain.clubMember.entity.ClubMemberRole;
+import com.mobble.mobbleserver.domain.clubMember.validator.ClubMemberValidator;
+import com.mobble.mobbleserver.domain.comment.dto.response.RootCommentResponseDto;
+import com.mobble.mobbleserver.domain.comment.entity.Comment;
+import com.mobble.mobbleserver.domain.comment.repository.CommentRepository;
+import com.mobble.mobbleserver.domain.comment.service.CommentService;
+import com.mobble.mobbleserver.domain.like.articleLike.repository.ArticleLikeRepository;
+import com.mobble.mobbleserver.domain.like.commentLike.repository.CommentLikeRepository;
+import com.mobble.mobbleserver.domain.member.entity.Member;
+import com.mobble.mobbleserver.domain.member.validator.MemberValidator;
+import com.mobble.mobbleserver.global.exception.common.DomainException;
+import com.mobble.mobbleserver.global.exception.errorCode.article.ArticleErrorCode;
+import com.mobble.mobbleserver.global.exception.errorCode.club.ClubErrorCode;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.List;
+import java.util.Map;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.mockito.ArgumentMatchers.*;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.BDDMockito.willThrow;
+import static org.mockito.Mockito.*;
+
+@ExtendWith(MockitoExtension.class)
+class ArticleServiceTest {
+
+    @Mock
+    private CommentService commentService;
+
+    @Mock
+    private ArticleRepository articleRepository;
+    @Mock
+    private CommentRepository commentRepository;
+    @Mock
+    private CommentLikeRepository commentLikeRepository;
+    @Mock
+    private ArticleLikeRepository articleLikeRepository;
+
+    @Mock
+    private ArticleValidator articleValidator;
+    @Mock
+    private ClubValidator clubValidator;
+    @Mock
+    private ClubMemberValidator clubMemberValidator;
+    @Mock
+    private MemberValidator memberValidator;
+
+    @InjectMocks
+    private ArticleService articleService;
