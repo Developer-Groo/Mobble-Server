2a986dda1696d234b69a91ccafe60bb2acc6ed44
Hojin02 <140497846+Hojin02@users.noreply.github.com>
2025-08-25 17:01:46 +0900

    style: 줄바꿈

--- src/test/java/com/mobble/mobbleserver/support/fixture/clubCategory/ClubCategoryTestFixture.java
+++ src/test/java/com/mobble/mobbleserver/support/fixture/clubCategory/ClubCategoryTestFixture.java
@@ -3,6 +3,7 @@ package com.mobble.mobbleserver.support.fixture.clubCategory;
 import com.mobble.mobbleserver.domain.clubCategory.entity.ClubCategory;
 
 public class ClubCategoryTestFixture {
+
     public static ClubCategory createDefaultCategory() {
         return ClubCategory.createClubCategory("SOCCER");
     }
