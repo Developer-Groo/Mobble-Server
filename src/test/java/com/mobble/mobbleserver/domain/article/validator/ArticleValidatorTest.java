039244eaf2039581c9f49138122a195b8bf8d71b
Hojin02 <140497846+Hojin02@users.noreply.github.com>
2025-08-26 18:03:08 +0900

    test: ArticleValidatorTest 클래스 생성

---
+++ src/test/java/com/mobble/mobbleserver/domain/article/validator/ArticleValidatorTest.java
@@ -0,0 +1,28 @@
+package com.mobble.mobbleserver.domain.article.validator;
+
+import com.mobble.mobbleserver.domain.article.entity.Article;
+import com.mobble.mobbleserver.domain.article.repository.ArticleRepository;
+import com.mobble.mobbleserver.global.exception.common.DomainException;
+import com.mobble.mobbleserver.global.exception.errorCode.article.ArticleErrorCode;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.Optional;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.mock;
+
+@ExtendWith(MockitoExtension.class)
+class ArticleValidatorTest {
+
+    @Mock
+    private ArticleRepository articleRepository;
+
+    @InjectMocks
+    private ArticleValidator articleValidator;
