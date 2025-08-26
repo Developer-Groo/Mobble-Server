0e7845fd3ab7b643e261a9206b2dc0e78acb9faa
taejunUM <utj0411@naver.com>
2025-08-26 15:29:42 +0900

    refactro: MeetingMember 레포지토리 쿼리메소드 추가

--- src/main/java/com/mobble/mobbleserver/domain/meetingMember/repository/MeetingMemberRepository.java
+++ src/main/java/com/mobble/mobbleserver/domain/meetingMember/repository/MeetingMemberRepository.java
@@ -3,5 +3,9 @@ package com.mobble.mobbleserver.domain.meetingMember.repository;
 import com.mobble.mobbleserver.domain.meetingMember.entity.MeetingMember;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface MeetingMemberRepository extends JpaRepository<MeetingMember, Long> {
+
+    Optional<MeetingMember> findMeetingMemberByMeetingIdAndMemberId(Long meetingId, Long memberId);
 }
