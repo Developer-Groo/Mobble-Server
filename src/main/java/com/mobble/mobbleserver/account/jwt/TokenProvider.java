72f7bd989de17cd82a0f1253a06b0ad2a3093053
taejunUM <utj0411@naver.com>
2025-08-26 20:09:17 +0900

    refactor: 토큰 네이밍 변경

--- src/main/java/com/mobble/mobbleserver/account/jwt/TokenProvider.java
+++ src/main/java/com/mobble/mobbleserver/account/jwt/TokenProvider.java
@@ -83,8 +83,8 @@ public class TokenProvider {
      * ======================= */
 
     // Access Token -> memberId
-    public Long getMemberIdByJwtToken(String accessJwtToken) {
-        String subject = parse(accessJwtToken).getSubject();
+    public Long getMemberIdByJwtToken(String jwtToken) {
+        String subject = parse(jwtToken).getSubject();
 
         if (!subject.matches("\\d+")) {
             throw new DomainException(OAuthErrorCode.INVALID_TOKEN_SUBJECT);
@@ -94,8 +94,8 @@ public class TokenProvider {
     }
 
     // Access Token -> roles (없으면 빈 리스트)
-    public List<ClubMemberRole> getRolesByJwtToken(String accessJwtToken) {
-        List<?> roles = parse(accessJwtToken).get("roles", List.class);
+    public List<ClubMemberRole> getRolesByJwtToken(String jwtToken) {
+        List<?> roles = parse(jwtToken).get("roles", List.class);
         if (roles == null) return List.of();
         return roles.stream()
                 .map(String::valueOf)          // Object -> String
@@ -119,9 +119,9 @@ public class TokenProvider {
      *  Validate
      * ======================= */
 
-    public boolean validateToken(String accessJwtToken) {
+    public boolean validateToken(String jwtToken) {
         try {
-            parse(accessJwtToken);
+            parse(jwtToken);
             return true;
         } catch (Exception e) {
             // MalformedJwtException, ExpiredJwtException, UnsupportedJwtException, IllegalArgumentException
