14aaac19e92957c9dd54894c6bbfd62803f4ec81
taejunUM <utj0411@naver.com>
2025-08-26 16:37:18 +0900

    feat: 참석 멤버 조회 api 구현

--- src/main/java/com/mobble/mobbleserver/domain/meetingMember/controller/MeetingMemberController.java
+++ src/main/java/com/mobble/mobbleserver/domain/meetingMember/controller/MeetingMemberController.java
@@ -1,16 +1,14 @@
 package com.mobble.mobbleserver.domain.meetingMember.controller;
 
 import com.mobble.mobbleserver.domain.meetingMember.dto.response.MeetingAttendanceResponseDto;
+import com.mobble.mobbleserver.domain.meetingMember.dto.response.MeetingMemberListResponseDto;
 import com.mobble.mobbleserver.domain.meetingMember.service.MeetingMemberService;
 import jakarta.validation.constraints.Positive;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @Validated
 @RestController
@@ -27,5 +25,13 @@ public class MeetingMemberController {
         Long memberId = 1L;
         return ResponseEntity.status(HttpStatus.OK)
                 .body(meetingMemberService.attendMeeting(meetingId, memberId));
+    }
+
+    @GetMapping
+    public ResponseEntity<MeetingMemberListResponseDto> getMeetingMembers(
+            @PathVariable("meeting-id") @Positive Long meetingId
+    ) {
+        return ResponseEntity.status(HttpStatus.OK)
+                .body(meetingMemberService.getMeetingMembers(meetingId));
     }
 }
