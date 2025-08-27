ce93d9b3c42cc1ac26eefdace51d6434bebdd5da
Hojin02 <140497846+Hojin02@users.noreply.github.com>
2025-08-27 15:59:08 +0900

    test: Article Id 로 좋아요 수와 사용자의 좋아요 여부 조회 test 코드 설계

--- src/test/java/com/mobble/mobbleserver/domain/article/repository/ArticleRepositoryImplTest.java
+++ src/test/java/com/mobble/mobbleserver/domain/article/repository/ArticleRepositoryImplTest.java
@@ -71,3 +71,65 @@ class ArticleRepositoryImplTest {
         assertThat(onlyNotice).hasSize(1);
         assertThat(onlyNotice.get(0).getArticleType()).isEqualTo(ArticleType.NOTICE);
     }
+
+    @Test
+    @DisplayName("게시글 ID들로 좋아요 수 & 사용자의 좋아요 여부 조회 성공")
+    void success_when_find_like_info_by_article_ids_and_member_id() {
+        // given
+        Member me = MemberTestFixture.createDefaultMember();
+        Member other = MemberTestFixture.createDefaultMember();
+
+        ClubCategory category = ClubCategory.createClubCategory("SOCCER");
+        Club club = ClubTestFixture.createDefaultClub(category);
+
+        em.persist(category);
+        em.persist(me);
+        em.persist(other);
+        em.persist(club);
+
+        Article a1 = ArticleTestFixture.createWithMemberAndClub(me, club);
+        a1.updateArticle(ArticleType.FREE, "a1", "c1");
+        Article a2 = ArticleTestFixture.createWithMemberAndClub(me, club);
+        a2.updateArticle(ArticleType.FREE, "a2", "c2");
+
+        em.persist(a1);
+        em.persist(a2);
+
+        ArticleLike l1 = ArticleLike.createArticleLike(a1, me);
+        ArticleLike l2 = ArticleLike.createArticleLike(a1, other);
+        ArticleLike l3 = ArticleLike.createArticleLike(a2, other);
+
+        em.persist(l1);
+        em.persist(l2);
+        em.persist(l3);
+
+        em.flush();
+        em.clear();
+
+        // when
+        List<Long> ids = List.of(a1.getId(), a2.getId());
+        Map<Long, ArticleLikeInfoDto> infoForMe =
+                articleRepository.findLikeInfoByArticleIdsAndMemberId(ids, me.getId());
+        Map<Long, ArticleLikeInfoDto> infoForNullUser =
+                articleRepository.findLikeInfoByArticleIdsAndMemberId(ids, null);
+
+        // then
+        assertThat(infoForMe).hasSize(2);
+        assertThat(infoForNullUser).hasSize(2);
+
+        ArticleLikeInfoDto a1InfoForMe = infoForMe.get(a1.getId());
+        assertThat(a1InfoForMe.likeCount()).isEqualTo(2);
+        assertThat(a1InfoForMe.isLiked()).isTrue();
+
+        ArticleLikeInfoDto a2InfoForMe = infoForMe.get(a2.getId());
+        assertThat(a2InfoForMe.likeCount()).isEqualTo(1);
+        assertThat(a2InfoForMe.isLiked()).isFalse();
+
+        ArticleLikeInfoDto a1InfoNull = infoForNullUser.get(a1.getId());
+        assertThat(a1InfoNull.likeCount()).isEqualTo(2);
+        assertThat(a1InfoNull.isLiked()).isFalse();
+
+        ArticleLikeInfoDto a2InfoNull = infoForNullUser.get(a2.getId());
+        assertThat(a2InfoNull.likeCount()).isEqualTo(1);
+        assertThat(a2InfoNull.isLiked()).isFalse();
+    }
