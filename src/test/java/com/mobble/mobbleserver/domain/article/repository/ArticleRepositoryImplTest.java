a5edc5a332b1fb3ab644def2ede14e3a810ab02f
Hojin02 <140497846+Hojin02@users.noreply.github.com>
2025-08-27 15:57:41 +0900

    test: ArticleRepositoryImplTest 클래스 생성

---
+++ src/test/java/com/mobble/mobbleserver/domain/article/repository/ArticleRepositoryImplTest.java
@@ -0,0 +1,34 @@
+package com.mobble.mobbleserver.domain.article.repository;
+
+import com.mobble.mobbleserver.config.QueryDslConfig;
+import com.mobble.mobbleserver.domain.article.entity.Article;
+import com.mobble.mobbleserver.domain.article.entity.ArticleType;
+import com.mobble.mobbleserver.domain.article.repository.dto.ArticleLikeInfoDto;
+import com.mobble.mobbleserver.domain.club.core.entity.Club;
+import com.mobble.mobbleserver.domain.clubCategory.entity.ClubCategory;
+import com.mobble.mobbleserver.domain.like.articleLike.entity.ArticleLike;
+import com.mobble.mobbleserver.domain.member.entity.Member;
+import com.mobble.mobbleserver.support.fixture.article.ArticleTestFixture;
+import com.mobble.mobbleserver.support.fixture.club.ClubTestFixture;
+import com.mobble.mobbleserver.support.fixture.member.MemberTestFixture;
+import jakarta.persistence.EntityManager;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.context.annotation.Import;
+
+import java.util.List;
+import java.util.Map;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@DataJpaTest
+@Import(QueryDslConfig.class)
+class ArticleRepositoryImplTest {
+
+    @Autowired
+    private ArticleRepository articleRepository;
+
+    @Autowired
+    private EntityManager em;
