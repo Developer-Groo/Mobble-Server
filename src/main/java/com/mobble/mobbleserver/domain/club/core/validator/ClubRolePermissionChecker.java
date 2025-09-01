efbba8b797bdd0f5499ff1c9e5075d3d325367f1
taejunUM <utj0411@naver.com>
2025-09-01 11:10:08 +0900

    feat: @PreAuthorize SpEL 권한 체크용 컴포넌트 구현

---
+++ src/main/java/com/mobble/mobbleserver/domain/club/core/validator/ClubRolePermissionChecker.java
@@ -0,0 +1,33 @@
+package com.mobble.mobbleserver.domain.club.core.validator;
+
+import com.mobble.mobbleserver.account.auth.principal.AuthMemberExtractor;
+import com.mobble.mobbleserver.domain.clubMember.entity.ClubMemberRole;
+import com.mobble.mobbleserver.domain.clubMember.validator.ClubMemberValidator;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+@Slf4j
+public class ClubRolePermissionChecker {
+
+    private final AuthMemberExtractor authMemberExtractor;
+    private final ClubMemberValidator clubMemberValidator;
+
+    public boolean isLeader(Long clubId) {
+        Long memberId = authMemberExtractor.getCurrentMemberId();
+
+        ClubMemberRole role = clubMemberValidator.findClubMemberByClubIdAndMemberIdOrThrow(clubId, memberId).getClubMemberRole();
+
+        return role == ClubMemberRole.LEADER;
+    }
+
+    public boolean isLeaderOrManager(Long clubId) {
+        Long memberId = authMemberExtractor.getCurrentMemberId();
+
+        ClubMemberRole role = clubMemberValidator.findClubMemberByClubIdAndMemberIdOrThrow(clubId, memberId).getClubMemberRole();
+
+        return role == ClubMemberRole.LEADER || role == ClubMemberRole.MANAGER;
+    }
+}
