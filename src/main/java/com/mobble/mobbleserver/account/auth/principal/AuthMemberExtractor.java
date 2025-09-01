d7a6db9117feae78acd27bd242c23d26908a2ef0
taejunUM <utj0411@naver.com>
2025-09-01 11:09:01 +0900

    feat: SecurityContext에서 memberId 추출하는 컴포넌트 구현

---
+++ src/main/java/com/mobble/mobbleserver/account/auth/principal/AuthMemberExtractor.java
@@ -0,0 +1,20 @@
+package com.mobble.mobbleserver.account.auth.principal;
+
+import com.mobble.mobbleserver.global.exception.common.DomainException;
+import com.mobble.mobbleserver.global.exception.errorCode.security.SecurityErrorCode;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Component;
+
+@Component
+public class AuthMemberExtractor {
+
+    public Long getCurrentMemberId() {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+
+        if (authentication == null || !(authentication.getPrincipal() instanceof AuthMember authMember))
+            throw new DomainException(SecurityErrorCode.ACCESS_DENIED);
+
+        return authMember.memberId();
+    }
+}
