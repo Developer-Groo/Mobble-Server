546f3853a03a70dfe5d596662b82582a739f2c0b
taejunUM <utj0411@naver.com>
2025-09-02 15:22:23 +0900

    refactor: 세미콜론 추가

--- src/main/java/com/mobble/mobbleserver/global/exception/errorCode/meeting/MeetingErrorCode.java
+++ src/main/java/com/mobble/mobbleserver/global/exception/errorCode/meeting/MeetingErrorCode.java
@@ -6,7 +6,7 @@ import org.springframework.http.HttpStatus;
 
 @RequiredArgsConstructor
 public enum MeetingErrorCode implements ErrorCode {
-    NOT_FOUND_MEETING("모임을 찾을 수 없습니다.", HttpStatus.NOT_FOUND)
+    NOT_FOUND_MEETING("모임을 찾을 수 없습니다.", HttpStatus.NOT_FOUND);
 
     private final String message;
     private final HttpStatus httpStatus;
