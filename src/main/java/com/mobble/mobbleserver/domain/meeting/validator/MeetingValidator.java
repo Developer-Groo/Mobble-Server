6856da7674786dc96c624075d0c22316a8b485d1
taejunUM <utj0411@naver.com>
2025-09-02 15:21:38 +0900

    feat: MeetingValidator 구현

---
+++ src/main/java/com/mobble/mobbleserver/domain/meeting/validator/MeetingValidator.java
@@ -0,0 +1,20 @@
+package com.mobble.mobbleserver.domain.meeting.validator;
+
+import com.mobble.mobbleserver.domain.meeting.entity.Meeting;
+import com.mobble.mobbleserver.domain.meeting.repository.MeetingRepository;
+import com.mobble.mobbleserver.global.exception.common.DomainException;
+import com.mobble.mobbleserver.global.exception.errorCode.meeting.MeetingErrorCode;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class MeetingValidator {
+
+    private final MeetingRepository meetingRepository;
+
+    public Meeting findMeetingByMeetingId(Long meetingId) {
+        return meetingRepository.findById(meetingId)
+                .orElseThrow(() -> new DomainException(MeetingErrorCode.NOT_FOUND_MEETING));
+    }
+}
