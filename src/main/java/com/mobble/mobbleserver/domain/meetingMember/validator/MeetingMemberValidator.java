db698a93583aa3d4181ec7d693172dc37e42a32e
taejunUM <utj0411@naver.com>
2025-09-03 17:23:29 +0900

    refactor: MeetingMemberValidator 메서드 구현

--- src/main/java/com/mobble/mobbleserver/domain/meetingMember/validator/MeetingMemberValidator.java
+++ src/main/java/com/mobble/mobbleserver/domain/meetingMember/validator/MeetingMemberValidator.java
@@ -5,6 +5,7 @@ import com.mobble.mobbleserver.domain.meetingMember.repository.MeetingMemberRepo
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 
+import java.util.List;
 import java.util.Optional;
 
 @Component
@@ -15,5 +16,9 @@ public class MeetingMemberValidator {
 
     public Optional<MeetingMember> findMeetingByMeetingIdAndMemberId(Long meetingId, Long memberId) {
         return meetingMemberRepository.findMeetingMemberByMeetingIdAndMemberId(meetingId, memberId);
+    }
+
+    public List<MeetingMember> findByMeetingId(Long meetingId) {
+        return meetingMemberRepository.findByMeetingId(meetingId);
     }
 }
