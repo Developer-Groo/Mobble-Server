7678945ac085d2801c253be0ca9c47939fabfd2c
taejunUM <utj0411@naver.com>
2025-09-04 21:37:52 +0900

    refactor: 메서드 네이밍 변경

--- src/main/java/com/mobble/mobbleserver/domain/meeting/service/MeetingService.java
+++ src/main/java/com/mobble/mobbleserver/domain/meeting/service/MeetingService.java
@@ -30,7 +30,7 @@ public class MeetingService {
     @Transactional
     public MeetingResponseDto createMeeting(Long memberId, Long clubId, MeetingRequestDto dto) {
         ClubMember hostMember = clubMemberValidator.findClubMemberByClubIdAndMemberIdOrThrow(clubId, memberId);
-        ClubPermissionPolicy.validateLeaderOrManager(hostMember);
+        ClubPermissionPolicy.validateLeaderOrManagerOrThrow(hostMember);
 
         Meeting meeting = dto.toEntity(hostMember);
 
@@ -58,7 +58,7 @@ public class MeetingService {
     @Transactional
     public MeetingResponseDto updateMeeting(Long memberId, Long clubId, Long meetingId, MeetingUpdateRequestDto dto) {
         ClubMember clubMember = clubMemberValidator.findClubMemberByClubIdAndMemberIdOrThrow(clubId, memberId);
-        ClubPermissionPolicy.validateLeaderOrManager(clubMember);
+        ClubPermissionPolicy.validateLeaderOrManagerOrThrow(clubMember);
         Meeting meeting = meetingValidator.findMeetingByMeetingIdOrThrow(meetingId);
 
         meeting.updateMeeting(
@@ -80,7 +80,7 @@ public class MeetingService {
     @Transactional
     public void deleteMeeting(Long memberId, Long clubId, Long meetingId) {
         ClubMember clubMember = clubMemberValidator.findClubMemberByClubIdAndMemberIdOrThrow(clubId, memberId);
-        ClubPermissionPolicy.validateLeaderOrManager(clubMember);
+        ClubPermissionPolicy.validateLeaderOrManagerOrThrow(clubMember);
         Meeting meeting = meetingValidator.findMeetingByMeetingIdOrThrow(meetingId);
 
         meetingRepository.delete(meeting);
