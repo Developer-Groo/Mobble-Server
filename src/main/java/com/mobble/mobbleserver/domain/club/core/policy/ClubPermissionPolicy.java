4e7e1dd8bd373fbd999612951c4401075ae9a71a
taejunUM <utj0411@naver.com>
2025-09-04 16:27:06 +0900

    refactor: ClubPermissionPolicy static 유틸 클래스 변경

--- src/main/java/com/mobble/mobbleserver/domain/club/core/policy/ClubPermissionPolicy.java
+++ src/main/java/com/mobble/mobbleserver/domain/club/core/policy/ClubPermissionPolicy.java
@@ -7,35 +7,33 @@ import com.mobble.mobbleserver.global.exception.errorCode.security.SecurityError
 import lombok.RequiredArgsConstructor;
 
 @RequiredArgsConstructor
-public class ClubPermissionPolicy {
-
-    private final ClubMember clubMember;
+public final class ClubPermissionPolicy {
 
     /**
      * 모임장(LEADER)만 가능한 권한 검증
      */
-    public void validateLeaderOnly() {
+    public static void validateLeaderOnly(ClubMember clubMember) {
         if (clubMember.getClubMemberRole() != ClubMemberRole.LEADER) throw new DomainException(SecurityErrorCode.ACCESS_DENIED);
     }
 
     /**
      * 관리자(LEADER, MANAGER)만 가능한 권한 검증
      */
-    public void validateLeaderOrManager() {
+    public static void validateLeaderOrManager(ClubMember clubMember) {
         if (clubMember.getClubMemberRole() != ClubMemberRole.LEADER && clubMember.getClubMemberRole() != ClubMemberRole.MANAGER) throw new DomainException(SecurityErrorCode.ACCESS_DENIED);
     }
 
     /**
      * 모임장 여부 판단 (예: 모임장 전용 기능 표시 활용(멤버 권한 변경 등))
      */
-    public boolean isLeader() {
+    public static boolean isLeader(ClubMember clubMember) {
         return clubMember.getClubMemberRole() == ClubMemberRole.LEADER;
     }
 
     /**
      * 관리자 여부 판단 (예: 관리자 전용 기능 표시 활용(수정, 삭제 버튼 노출), 응답 DTO 등 조건 분기 용도)
      */
-    public boolean isLeaderOrManager() {
+    public static boolean isLeaderOrManager(ClubMember clubMember) {
         return clubMember.getClubMemberRole() == ClubMemberRole.LEADER || clubMember.getClubMemberRole() == ClubMemberRole.MANAGER;
     }
 }
