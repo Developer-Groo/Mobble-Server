af0d02f65c8429bfad6e972948e1769f13010c41
Groo <woohyeon3382@naver.com>
2025-09-05 14:59:38 +0900

    chore: ClubPermissionPolicy 번경된 import 적용

--- src/main/java/com/mobble/mobbleserver/domain/comment/service/CommentService.java
+++ src/main/java/com/mobble/mobbleserver/domain/comment/service/CommentService.java
@@ -3,7 +3,7 @@ package com.mobble.mobbleserver.domain.comment.service;
 import com.mobble.mobbleserver.domain.article.entity.Article;
 import com.mobble.mobbleserver.domain.article.validator.ArticleValidator;
 import com.mobble.mobbleserver.domain.club.core.entity.Club;
-import com.mobble.mobbleserver.domain.club.core.policy.ClubPermissionPolicy;
+import com.mobble.mobbleserver.domain.club.policy.ClubPermissionPolicy;
 import com.mobble.mobbleserver.domain.clubMember.entity.ClubMember;
 import com.mobble.mobbleserver.domain.clubMember.validator.ClubMemberValidator;
 import com.mobble.mobbleserver.domain.comment.dto.request.CommentRequestDto;
