759653b2489921d2a550d33f604b9a9abe1f9572
Hojin02 <140497846+Hojin02@users.noreply.github.com>
2025-09-08 18:22:45 +0900

    test: ClubValidatorTest 클래스 설계

---
+++ src/test/java/com/mobble/mobbleserver/domain/club/core/validator/ClubValidatorTest.java
@@ -0,0 +1,23 @@
+package com.mobble.mobbleserver.domain.club.core.validator;
+
+import com.mobble.mobbleserver.domain.club.core.entity.Club;
+import com.mobble.mobbleserver.domain.club.core.repository.ClubRepository;
+import com.mobble.mobbleserver.global.exception.common.DomainException;
+import com.mobble.mobbleserver.global.exception.errorCode.club.ClubErrorCode;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.Optional;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.*;
+
+@ExtendWith(MockitoExtension.class)
+class ClubValidatorTest {
