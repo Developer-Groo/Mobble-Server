99008f66aad354a1d4b31261fe0c7d6023ab4db6
taejunUM <utj0411@naver.com>
2025-09-08 17:01:04 +0900

    refactor: 로그인 서비스로직 예외 추가

--- src/main/java/com/mobble/mobbleserver/account/auth/service/SocialLoginService.java
+++ src/main/java/com/mobble/mobbleserver/account/auth/service/SocialLoginService.java
@@ -10,6 +10,8 @@ import com.mobble.mobbleserver.domain.clubMember.entity.ClubMemberRole;
 import com.mobble.mobbleserver.domain.clubMember.repository.ClubMemberRepository;
 import com.mobble.mobbleserver.domain.member.entity.Member;
 import com.mobble.mobbleserver.domain.member.validator.MemberValidator;
+import com.mobble.mobbleserver.global.exception.common.DomainException;
+import com.mobble.mobbleserver.global.exception.errorCode.oAuth.OAuthErrorCode;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -39,7 +41,13 @@ public class SocialLoginService {
             return SocialLoginResponseDto.existMember(jwtToken);
         }
 
-        String signupToken = tokenProvider.createSignupToken(userInfo.name(), userInfo.email(), userInfo.socialProvider(), userInfo.socialId());
+        if (userInfo.email() == null) throw new DomainException(OAuthErrorCode.NO_USER_INFO); // for Apple Login
+
+        String signupToken = tokenProvider.createSignupToken(
+                userInfo.name(),
+                userInfo.email(),
+                userInfo.socialProvider(),
+                userInfo.socialId());
 
         return SocialLoginResponseDto.newMember(signupToken);
     }
