e3ff4f5e556222ed45b8f818827a74985ff6305c
taejunUM <utj0411@naver.com>
2025-09-09 23:03:34 +0900

    test: CommentLike 테스트 코드 추가

---
+++ src/test/java/com/mobble/mobbleserver/domain/like/commentLike/entity/CommentLikeTest.java
@@ -0,0 +1,54 @@
+package com.mobble.mobbleserver.domain.like.commentLike.entity;
+
+import com.mobble.mobbleserver.domain.comment.entity.Comment;
+import com.mobble.mobbleserver.domain.member.entity.Member;
+import com.mobble.mobbleserver.global.exception.common.DomainException;
+import com.mobble.mobbleserver.global.exception.errorCode.like.LikeErrorCode;
+import com.mobble.mobbleserver.support.fixture.comment.CommentTestFixture;
+import com.mobble.mobbleserver.support.fixture.member.MemberTestFixture;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
+
+public class CommentLikeTest {
+
+    private final Member mockMember = MemberTestFixture.createDefaultMember();
+    private final Comment mockComment = CommentTestFixture.createDefaultRootComment();
+
+    @Nested
+    @DisplayName("CommentLike 생성 테스트")
+    class CreateCommentLike {
+
+        @Test
+        @DisplayName("CommentLike 생성 성공")
+        void success_when_create_comment_like() {
+            // when
+            CommentLike like = CommentLike.createCommentLike(mockComment, mockMember);
+
+            // then
+            assertThat(like.getComment()).isEqualTo(mockComment);
+            assertThat(like.getMember()).isEqualTo(mockMember);
+        }
+
+        @Test
+        @DisplayName("comment 가 null 인 경우 예외 발생")
+        void fails_when_comment_is_null() {
+            // when & then
+            assertThatThrownBy(() -> CommentLike.createCommentLike(null, mockMember))
+                    .isInstanceOf(DomainException.class)
+                    .hasMessage(LikeErrorCode.COMMENT_REQUIRED.message());
+        }
+
+        @Test
+        @DisplayName("member 가 null 인 경우 예외 발생")
+        void fails_when_member_is_null() {
+            // when & then
+            assertThatThrownBy(() -> CommentLike.createCommentLike(mockComment, null))
+                    .isInstanceOf(DomainException.class)
+                    .hasMessage(LikeErrorCode.MEMBER_REQUIRED.message());
+        }
+    }
+}
