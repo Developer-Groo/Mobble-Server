f837e0739851512a4cb1f9208c14a5bf5db42a3a
taejunUM <utj0411@naver.com>
2025-09-10 20:52:37 +0900

    test: 댓글 좋아요 생성 테스트 코드 구현

--- src/test/java/com/mobble/mobbleserver/domain/like/commentLike/service/CommentLikeServiceTest.java
+++ src/test/java/com/mobble/mobbleserver/domain/like/commentLike/service/CommentLikeServiceTest.java
@@ -1,5 +1,8 @@
 package com.mobble.mobbleserver.domain.like.commentLike.service;
 
+import com.mobble.mobbleserver.domain.article.entity.Article;
+import com.mobble.mobbleserver.domain.club.core.entity.Club;
+import com.mobble.mobbleserver.domain.clubMember.entity.ClubMember;
 import com.mobble.mobbleserver.domain.clubMember.validator.ClubMemberValidator;
 import com.mobble.mobbleserver.domain.comment.entity.Comment;
 import com.mobble.mobbleserver.domain.comment.validator.CommentValidator;
@@ -7,12 +10,19 @@ import com.mobble.mobbleserver.domain.like.commentLike.entity.CommentLike;
 import com.mobble.mobbleserver.domain.like.commentLike.repository.CommentLikeRepository;
 import com.mobble.mobbleserver.domain.member.entity.Member;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
-import static org.mockito.Mockito.mock;
+import java.util.Optional;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
 class CommentLikeServiceTest {
@@ -29,18 +39,48 @@ class CommentLikeServiceTest {
     @InjectMocks
     CommentLikeService commentLikeService;
 
-    private static final Long CLUB_ID = 1L;
-    private static final Long MEMBER_Id = 2L;
+    private static final Long COMMENT_ID = 1L;
+    private static final Long MEMBER_ID = 2L;
 
+    private Club mockClub;
+    private Article mockArticle;
     private Comment mockComment;
     private Member mockMember;
+    private ClubMember mockClubMember;
     private CommentLike mockCommentLike;
 
     @BeforeEach
     void setUP() {
+        mockClub = mock(Club.class);
+        mockArticle = mock(Article.class);
         mockComment = mock(Comment.class);
         mockMember = mock(Member.class);
+        mockClubMember = mock(ClubMember.class);
         mockCommentLike = mock(CommentLike.class);
     }
-    
+
+    @DisplayName("댓글 좋아요가 없으면 생성")
+    @Test
+    void success_when_not_liked() {
+        // given
+        Long clubId = 10L;
+
+        given(mockComment.getId()).willReturn(COMMENT_ID);
+        given(mockMember.getId()).willReturn(MEMBER_ID);
+
+        given(mockComment.getArticle()).willReturn(mockArticle);
+        given(mockArticle.getClub()).willReturn(mockClub);
+        given(mockClub.getId()).willReturn(clubId);
+
+        given(commentValidator.findCommentByCommentIdOrThrow(COMMENT_ID)).willReturn(mockComment);
+        given(commentLikeRepository.findLikedByCommentIdAndMemberId(COMMENT_ID, MEMBER_ID)).willReturn(Optional.empty());
+        given(clubMemberValidator.findClubMemberByClubIdAndMemberIdOrThrow(clubId, MEMBER_ID)).willReturn(mockClubMember);
+
+        // when
+        boolean result = commentLikeService.toggleLike(COMMENT_ID, mockMember).isLiked();
+
+        // then
+        assertThat(result).isTrue();
+        verify(commentLikeRepository).save(any(CommentLike.class));
+    }
 }
