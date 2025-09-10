1e6ce788134197a555e8ccb08688f85736120508
taejunUM <utj0411@naver.com>
2025-09-10 20:56:51 +0900

    test: 좋아요 할 댓글이 없으면 예외처리

--- src/test/java/com/mobble/mobbleserver/domain/like/commentLike/service/CommentLikeServiceTest.java
+++ src/test/java/com/mobble/mobbleserver/domain/like/commentLike/service/CommentLikeServiceTest.java
@@ -9,6 +9,9 @@ import com.mobble.mobbleserver.domain.comment.validator.CommentValidator;
 import com.mobble.mobbleserver.domain.like.commentLike.entity.CommentLike;
 import com.mobble.mobbleserver.domain.like.commentLike.repository.CommentLikeRepository;
 import com.mobble.mobbleserver.domain.member.entity.Member;
+import com.mobble.mobbleserver.global.exception.common.DomainException;
+import com.mobble.mobbleserver.global.exception.errorCode.like.LikeErrorCode;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -20,6 +23,7 @@ import org.mockito.junit.jupiter.MockitoExtension;
 import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.BDDMockito.given;
 import static org.mockito.Mockito.*;
@@ -101,5 +105,17 @@ class CommentLikeServiceTest {
         assertThat(result).isFalse();
         verify(commentLikeRepository).delete(mockCommentLike);
         verify(commentLikeRepository, never()).save(any());
+    }
+
+    @DisplayName("좋아요 할 댓글이 존재하지 않으면 예외 발생")
+    @Test
+    void fail_when_comment_not_found() {
+        // given
+        given(commentValidator.findCommentByCommentIdOrThrow(COMMENT_ID)).willThrow(new DomainException(LikeErrorCode.COMMENT_REQUIRED));
+
+        // when & then
+        assertThatThrownBy(() -> commentLikeService.toggleLike(COMMENT_ID, mockMember))
+                .isInstanceOf(DomainException.class)
+                .hasMessage(LikeErrorCode.COMMENT_REQUIRED.message());
     }
 }
