245698d4d63fcc184240277e8bf0e6d03f2af8eb
taejunUM <utj0411@naver.com>
2025-09-10 19:58:52 +0900

    test: ClubLikeServiceTest 좋아요 생성 테스트 코드 작성

---
+++ src/test/java/com/mobble/mobbleserver/domain/like/clubLike/service/ClubLikeServiceTest.java
@@ -0,0 +1,67 @@
+package com.mobble.mobbleserver.domain.like.clubLike.service;
+
+import com.mobble.mobbleserver.domain.club.core.entity.Club;
+import com.mobble.mobbleserver.domain.club.core.validator.ClubValidator;
+import com.mobble.mobbleserver.domain.like.clubLike.entity.ClubLike;
+import com.mobble.mobbleserver.domain.like.clubLike.repository.ClubLikeRepository;
+import com.mobble.mobbleserver.domain.member.entity.Member;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.Optional;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
+
+@ExtendWith(MockitoExtension.class)
+class ClubLikeServiceTest {
+
+    @Mock
+    ClubLikeRepository clubLikeRepository;
+
+    @Mock
+    ClubValidator clubValidator;
+
+    @InjectMocks
+    ClubLikeService clubLikeService;
+
+    private static final Long CLUB_ID = 1L;
+    private static final Long MEMBER_ID = 2L;
+
+    private Club mockClub;
+    private Member mockMember;
+    private ClubLike mockClubLike;
+
+    @BeforeEach
+    void setUp() {
+        mockClub = mock(Club.class);
+        mockMember = mock(Member.class);
+        mockClubLike = mock(ClubLike.class);
+    }
+
+    @DisplayName("좋아요가 없으면 생성")
+    @Test
+    void success_when_not_liked() {
+        // given
+        given(mockClub.getId()).willReturn(CLUB_ID);
+        given(mockMember.getId()).willReturn(MEMBER_ID);
+        given(clubValidator.findClubByClubIdOrThrow(CLUB_ID)).willReturn(mockClub);
+        given(clubLikeRepository.findLikedByClubIdAndMemberId(CLUB_ID, MEMBER_ID)).willReturn(Optional.empty());
+        given(clubLikeRepository.save(any(ClubLike.class))).willReturn(mockClubLike);
+
+        // when
+        boolean result = clubLikeService.toggleLike(CLUB_ID, mockMember).isLiked();
+
+        // then
+        assertThat(result).isTrue();
+        verify(clubLikeRepository).save(any(ClubLike.class));
+    }
+}
