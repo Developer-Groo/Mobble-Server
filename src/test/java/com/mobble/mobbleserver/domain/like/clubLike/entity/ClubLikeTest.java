c7536899fb95a7ce6253c0fccecde29354e943a0
taejunUM <utj0411@naver.com>
2025-09-11 16:20:54 +0900

    test: ClubLikeTest @Nested 중첩클래스 제거

--- src/test/java/com/mobble/mobbleserver/domain/like/clubLike/entity/ClubLikeTest.java
+++ src/test/java/com/mobble/mobbleserver/domain/like/clubLike/entity/ClubLikeTest.java
@@ -9,50 +9,43 @@ import com.mobble.mobbleserver.support.fixture.club.ClubTestFixture;
 import com.mobble.mobbleserver.support.fixture.clubCategory.ClubCategoryTestFixture;
 import com.mobble.mobbleserver.support.fixture.member.MemberTestFixture;
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
 
-public class ClubLikeTest {
+class ClubLikeTest {
 
     private final Member mockMember = MemberTestFixture.createDefaultMember();
     private final ClubCategory mockClubCategory = ClubCategoryTestFixture.createDefaultCategory();
     private final Club mockClub = ClubTestFixture.createDefaultClub(mockClubCategory);
 
-    @Nested
-    @DisplayName("ClubLike 생성 테스트")
-    class CreateArticleLike {
+    @Test
+    @DisplayName("ClubLike 생성 성공")
+    void success_when_create_article_like() {
+        // when
+        ClubLike like = ClubLike.createClubLike(mockClub, mockMember);
 
-        @Test
-        @DisplayName("ClubLike 생성 성공")
-        void success_when_create_article_like() {
-            // when
-            ClubLike like = ClubLike.createClubLike(mockClub, mockMember);
+        //then
+        assertThat(like.getClub()).isEqualTo(mockClub);
+        assertThat(like.getMember()).isEqualTo(mockMember);
+    }
 
-            //then
-            assertThat(like.getClub()).isEqualTo(mockClub);
-            assertThat(like.getMember()).isEqualTo(mockMember);
-        }
+    @Test
+    @DisplayName("club 이 null 인 경우 예외 발생")
+    void fails_when_article_is_null() {
+        // when & then
+        assertThatThrownBy(() -> ClubLike.createClubLike(null, mockMember))
+                .isInstanceOf(DomainException.class)
+                .hasMessage(LikeErrorCode.CLUB_REQUIRED.message());
+    }
 
-        @Test
-        @DisplayName("club 이 null 인 경우 예외 발생")
-        void fails_when_article_is_null() {
-            // when & then
-            assertThatThrownBy(() -> ClubLike.createClubLike(null, mockMember))
-                    .isInstanceOf(DomainException.class)
-                    .hasMessage(LikeErrorCode.CLUB_REQUIRED.message());
-        }
-
-        @Test
-        @DisplayName("member 가 null 인 경우 예외 발생")
-        void fails_when_member_is_null() {
-            // when & then
-            assertThatThrownBy(() -> ClubLike.createClubLike(mockClub, null))
-                    .isInstanceOf(DomainException.class)
-                    .hasMessage(LikeErrorCode.MEMBER_REQUIRED.message());
-        }
+    @Test
+    @DisplayName("member 가 null 인 경우 예외 발생")
+    void fails_when_member_is_null() {
+        // when & then
+        assertThatThrownBy(() -> ClubLike.createClubLike(mockClub, null))
+                .isInstanceOf(DomainException.class)
+                .hasMessage(LikeErrorCode.MEMBER_REQUIRED.message());
     }
 }
-
