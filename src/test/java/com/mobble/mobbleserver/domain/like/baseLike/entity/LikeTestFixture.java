c8311bb96a92371a6723a45b13330c67ce6fccf9
taejunUM <utj0411@naver.com>
2025-09-11 18:06:18 +0900

    test: LikeTestFixture 중첩클래스 제거

--- src/test/java/com/mobble/mobbleserver/domain/like/baseLike/entity/LikeTestFixture.java
+++ src/test/java/com/mobble/mobbleserver/domain/like/baseLike/entity/LikeTestFixture.java
@@ -2,14 +2,11 @@ package com.mobble.mobbleserver.domain.like.baseLike.entity;
 
 import com.mobble.mobbleserver.domain.member.entity.Member;
 
-public class LikeTestFixture {
+public class LikeTestFixture extends BaseLike{
 
-    public static TestLike create(Member member) {
-        TestLike like = new TestLike();
+    public static LikeTestFixture create(Member member) {
+        LikeTestFixture like = new LikeTestFixture();
         like.assignMember(member);
         return like;
-    }
-
-    public static class TestLike extends BaseLike {
     }
 }
