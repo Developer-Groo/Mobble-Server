5fbcc12cd6fdd799548fe2cef9fe97d2d80b91eb
taejunUM <utj0411@naver.com>
2025-09-12 18:32:42 +0900

    test: @Nested로 조회, 삭제 테스트 코드 분리

--- src/test/java/com/mobble/mobbleserver/domain/like/articleLike/repository/ArticleLikeRepositoryTest.java
+++ src/test/java/com/mobble/mobbleserver/domain/like/articleLike/repository/ArticleLikeRepositoryTest.java
@@ -11,6 +11,7 @@ import com.mobble.mobbleserver.support.fixture.club.ClubTestFixture;
 import com.mobble.mobbleserver.support.fixture.member.MemberTestFixture;
 import jakarta.persistence.EntityManager;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
@@ -31,176 +32,186 @@ class ArticleLikeRepositoryTest {
     @Autowired
     private EntityManager em;
 
-    @Test
-    @DisplayName("게시글에 사용자가 좋아요를 누른 경우, ArticleLike 조회 성공")
-    void findLikedByArticleIdAndMemberId_success_when_liked() {
-        // given
-        Member member = MemberTestFixture.createDefaultMember();
-        ClubCategory category = ClubCategory.createClubCategory("SOCCER");
-        Club club = ClubTestFixture.createDefaultClub(category);
-        Article article = ArticleTestFixture.createWithMemberAndClub(member, club);
-        ArticleLike like = ArticleLike.createArticleLike(article, member);
+    @Nested
+    @DisplayName("좋아요 조회 기능")
+    class Find {
 
-        em.persist(category);
-        em.persist(member);
-        em.persist(club);
-        em.persist(article);
-        em.persist(like);
-        em.flush();
-        em.clear();
+        @Test
+        @DisplayName("게시글에 사용자가 좋아요를 누른 경우, ArticleLike 조회 성공")
+        void findLikedByArticleIdAndMemberId_success_when_liked() {
+            // given
+            Member member = MemberTestFixture.createDefaultMember();
+            ClubCategory category = ClubCategory.createClubCategory("SOCCER");
+            Club club = ClubTestFixture.createDefaultClub(category);
+            Article article = ArticleTestFixture.createWithMemberAndClub(member, club);
+            ArticleLike like = ArticleLike.createArticleLike(article, member);
 
-        // when
-        Optional<ArticleLike> result = articleLikeRepository.findLikedByArticleIdAndMemberId(article.getId(), member.getId());
+            em.persist(category);
+            em.persist(member);
+            em.persist(club);
+            em.persist(article);
+            em.persist(like);
+            em.flush();
+            em.clear();
 
-        // then
-        assertThat(result).isPresent();
-        assertThat(result.get().getMember().getId()).isEqualTo(member.getId());
-        assertThat(result.get().getArticle().getId()).isEqualTo(article.getId());
+            // when
+            Optional<ArticleLike> result = articleLikeRepository.findLikedByArticleIdAndMemberId(article.getId(), member.getId());
+
+            // then
+            assertThat(result).isPresent();
+            assertThat(result.get().getMember().getId()).isEqualTo(member.getId());
+            assertThat(result.get().getArticle().getId()).isEqualTo(article.getId());
+        }
+
+        @Test
+        @DisplayName("게시글에 사용자가 좋아요를 누르지 않은 경우, ArticleLike 조회 결과 없음")
+        void findLikedByArticleIdAndMemberId_success_when_not_liked() {
+            // given
+            Member member = MemberTestFixture.createDefaultMember();
+            ClubCategory category = ClubCategory.createClubCategory("SOCCER");
+            Club club = ClubTestFixture.createDefaultClub(category);
+            Article article = ArticleTestFixture.createWithMemberAndClub(member, club);
+
+            em.persist(category);
+            em.persist(member);
+            em.persist(club);
+            em.persist(article);
+            em.flush();
+            em.clear();
+
+            // when
+            Optional<ArticleLike> result = articleLikeRepository.findLikedByArticleIdAndMemberId(article.getId(), member.getId());
+
+            // then
+            assertThat(result).isNotPresent();
+        }
+
+        @Test
+        @DisplayName("Article ID로 ArticleLike 리스트 조회 성공")
+        void success_when_findAllByArticleId() {
+            // given
+            Member member1 = MemberTestFixture.createDefaultMember();
+            Member member2 = MemberTestFixture.createDefaultMember();
+            ClubCategory category = ClubCategory.createClubCategory("축구");
+            Club club = ClubTestFixture.createDefaultClub(category);
+            Article article = ArticleTestFixture.createWithMemberAndClub(member1, club);
+
+            em.persist(category);
+            em.persist(member1);
+            em.persist(member2);
+            em.persist(club);
+            em.persist(article);
+
+            ArticleLike like1 = ArticleLike.createArticleLike(article, member1);
+            ArticleLike like2 = ArticleLike.createArticleLike(article, member2);
+            em.persist(like1);
+            em.persist(like2);
+            em.flush();
+            em.clear();
+
+            // when
+            List<ArticleLike> articleLikes = articleLikeRepository.findAllByArticleId(article.getId());
+
+            // then
+            assertThat(articleLikes).hasSize(2);
+            assertThat(articleLikes).extracting(ArticleLike::getMember)
+                    .extracting(Member::getId)
+                    .containsExactlyInAnyOrder(member1.getId(), member2.getId());
+        }
+
+        @Test
+        @DisplayName("Article ID로 ArticleLike 리스트 조회 시 좋아요가 없는 경우, 빈 리스트 반환")
+        void success_when_findAllByArticleId_returns_empty_list() {
+            // given
+            Member member = MemberTestFixture.createDefaultMember();
+            ClubCategory category = ClubCategory.createClubCategory("축구");
+            Club club = ClubTestFixture.createDefaultClub(category);
+            Article article = ArticleTestFixture.createWithMemberAndClub(member, club);
+
+            em.persist(category);
+            em.persist(member);
+            em.persist(club);
+            em.persist(article);
+            em.flush();
+            em.clear();
+
+            // when
+            List<ArticleLike> articleLikes = articleLikeRepository.findAllByArticleId(article.getId());
+
+            // then
+            assertThat(articleLikes)
+                    .isNotNull()
+                    .isEmpty();
+        }
     }
 
-    @Test
-    @DisplayName("게시글에 사용자가 좋아요를 누르지 않은 경우, ArticleLike 조회 결과 없음")
-    void findLikedByArticleIdAndMemberId_success_when_not_liked() {
-        // given
-        Member member = MemberTestFixture.createDefaultMember();
-        ClubCategory category = ClubCategory.createClubCategory("SOCCER");
-        Club club = ClubTestFixture.createDefaultClub(category);
-        Article article = ArticleTestFixture.createWithMemberAndClub(member, club);
+    @Nested
+    @DisplayName("좋아요 삭제 기능")
+    class Delete {
 
-        em.persist(category);
-        em.persist(member);
-        em.persist(club);
-        em.persist(article);
-        em.flush();
-        em.clear();
+        @Test
+        @DisplayName("Article ID로 모든 좋아요 삭제 성공")
+        void success_when_delete_all_by_article_id() {
+            // given
+            Member member = MemberTestFixture.createDefaultMember();
+            ClubCategory category = ClubCategory.createClubCategory("SOCCER");
+            Club club = ClubTestFixture.createDefaultClub(category);
+            Article article = ArticleTestFixture.createWithMemberAndClub(member, club);
 
-        // when
-        Optional<ArticleLike> result = articleLikeRepository.findLikedByArticleIdAndMemberId(article.getId(), member.getId());
+            em.persist(category);
+            em.persist(member);
+            em.persist(club);
+            em.persist(article);
 
-        // then
-        assertThat(result).isNotPresent();
-    }
+            ArticleLike like1 = ArticleLike.createArticleLike(article, member);
+            ArticleLike like2 = ArticleLike.createArticleLike(article, member);
+            em.persist(like1);
+            em.persist(like2);
+            em.flush();
 
-    @Test
-    @DisplayName("Article ID로 ArticleLike 리스트 조회 성공")
-    void success_when_findAllByArticleId() {
-        // given
-        Member member1 = MemberTestFixture.createDefaultMember();
-        Member member2 = MemberTestFixture.createDefaultMember();
-        ClubCategory category = ClubCategory.createClubCategory("축구");
-        Club club = ClubTestFixture.createDefaultClub(category);
-        Article article = ArticleTestFixture.createWithMemberAndClub(member1, club);
+            // when
+            articleLikeRepository.deleteAllByArticleId(article.getId());
+            em.flush();
+            em.clear();
 
-        em.persist(category);
-        em.persist(member1);
-        em.persist(member2);
-        em.persist(club);
-        em.persist(article);
+            // then
+            List<ArticleLike> result = articleLikeRepository.findAllByArticleId(article.getId());
+            assertThat(result).isEmpty();
+        }
 
-        ArticleLike like1 = ArticleLike.createArticleLike(article, member1);
-        ArticleLike like2 = ArticleLike.createArticleLike(article, member2);
-        em.persist(like1);
-        em.persist(like2);
-        em.flush();
-        em.clear();
+        @Test
+        @DisplayName("Article ID 리스트로 좋아요 전체 삭제 성공")
+        void success_when_delete_all_by_article_ids() {
+            // given
+            Member member = MemberTestFixture.createDefaultMember();
+            ClubCategory category = ClubCategory.createClubCategory("SOCCER");
+            Club club = ClubTestFixture.createDefaultClub(category);
 
-        // when
-        List<ArticleLike> articleLikes = articleLikeRepository.findAllByArticleId(article.getId());
+            em.persist(category);
+            em.persist(member);
+            em.persist(club);
 
-        // then
-        assertThat(articleLikes).hasSize(2);
-        assertThat(articleLikes).extracting(ArticleLike::getMember)
-                .extracting(Member::getId)
-                .containsExactlyInAnyOrder(member1.getId(), member2.getId());
-    }
+            Article article1 = ArticleTestFixture.createWithMemberAndClub(member, club);
+            Article article2 = ArticleTestFixture.createWithMemberAndClub(member, club);
+            em.persist(article1);
+            em.persist(article2);
 
-    @Test
-    @DisplayName("Article ID로 ArticleLike 리스트 조회 시 좋아요가 없는 경우, 빈 리스트 반환")
-    void success_when_findAllByArticleId_returns_empty_list() {
-        // given
-        Member member = MemberTestFixture.createDefaultMember();
-        ClubCategory category = ClubCategory.createClubCategory("축구");
-        Club club = ClubTestFixture.createDefaultClub(category);
-        Article article = ArticleTestFixture.createWithMemberAndClub(member, club);
+            ArticleLike like1 = ArticleLike.createArticleLike(article1, member);
+            ArticleLike like2 = ArticleLike.createArticleLike(article2, member);
+            em.persist(like1);
+            em.persist(like2);
+            em.flush();
 
-        em.persist(category);
-        em.persist(member);
-        em.persist(club);
-        em.persist(article);
-        em.flush();
-        em.clear();
+            List<Long> articleIds = List.of(article1.getId(), article2.getId());
 
-        // when
-        List<ArticleLike> articleLikes = articleLikeRepository.findAllByArticleId(article.getId());
+            // when
+            articleLikeRepository.deleteAllArticleLikeByArticle_IdIn(articleIds);
+            em.flush();
+            em.clear();
 
-        // then
-        assertThat(articleLikes)
-                .isNotNull()
-                .isEmpty();
-    }
-
-    @Test
-    @DisplayName("Article ID로 모든 좋아요 삭제 성공")
-    void success_when_delete_all_by_article_id() {
-        // given
-        Member member = MemberTestFixture.createDefaultMember();
-        ClubCategory category = ClubCategory.createClubCategory("SOCCER");
-        Club club = ClubTestFixture.createDefaultClub(category);
-        Article article = ArticleTestFixture.createWithMemberAndClub(member, club);
-
-        em.persist(category);
-        em.persist(member);
-        em.persist(club);
-        em.persist(article);
-
-        ArticleLike like1 = ArticleLike.createArticleLike(article, member);
-        ArticleLike like2 = ArticleLike.createArticleLike(article, member);
-        em.persist(like1);
-        em.persist(like2);
-        em.flush();
-
-        // when
-        articleLikeRepository.deleteAllByArticleId(article.getId());
-        em.flush();
-        em.clear();
-
-        // then
-        List<ArticleLike> result = articleLikeRepository.findAllByArticleId(article.getId());
-        assertThat(result).isEmpty();
-    }
-
-    @Test
-    @DisplayName("Article ID 리스트로 좋아요 전체 삭제 성공")
-    void success_when_delete_all_by_article_ids() {
-        // given
-        Member member = MemberTestFixture.createDefaultMember();
-        ClubCategory category = ClubCategory.createClubCategory("SOCCER");
-        Club club = ClubTestFixture.createDefaultClub(category);
-
-        em.persist(category);
-        em.persist(member);
-        em.persist(club);
-
-        Article article1 = ArticleTestFixture.createWithMemberAndClub(member, club);
-        Article article2 = ArticleTestFixture.createWithMemberAndClub(member, club);
-        em.persist(article1);
-        em.persist(article2);
-
-        ArticleLike like1 = ArticleLike.createArticleLike(article1, member);
-        ArticleLike like2 = ArticleLike.createArticleLike(article2, member);
-        em.persist(like1);
-        em.persist(like2);
-        em.flush();
-
-        List<Long> articleIds = List.of(article1.getId(), article2.getId());
-
-        // when
-        articleLikeRepository.deleteAllArticleLikeByArticle_IdIn(articleIds);
-        em.flush();
-        em.clear();
-
-        // then
-        List<ArticleLike> remainingLikes = articleLikeRepository.findAll();
-        assertThat(remainingLikes).isEmpty();
+            // then
+            List<ArticleLike> remainingLikes = articleLikeRepository.findAll();
+            assertThat(remainingLikes).isEmpty();
+        }
     }
 }
