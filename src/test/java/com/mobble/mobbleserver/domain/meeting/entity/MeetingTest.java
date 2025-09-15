532a8fc7434c269b077e5d7a50965ac15397c418
taejunUM <utj0411@naver.com>
2025-09-15 19:34:26 +0900

    test: 모임 생성 성공 작성

--- src/test/java/com/mobble/mobbleserver/domain/meeting/entity/MeetingTest.java
+++ src/test/java/com/mobble/mobbleserver/domain/meeting/entity/MeetingTest.java
@@ -1,5 +1,62 @@
 package com.mobble.mobbleserver.domain.meeting.entity;
 
+import com.mobble.mobbleserver.domain.club.core.entity.Club;
+import com.mobble.mobbleserver.domain.clubCategory.entity.ClubCategory;
+import com.mobble.mobbleserver.domain.clubMember.entity.ClubMember;
+import com.mobble.mobbleserver.domain.clubMember.entity.ClubMemberRole;
+import com.mobble.mobbleserver.domain.clubMember.entity.JoinStatus;
+import com.mobble.mobbleserver.domain.member.entity.Member;
+import com.mobble.mobbleserver.support.fixture.club.ClubTestFixture;
+import com.mobble.mobbleserver.support.fixture.clubMember.ClubMemberTestFixture;
+import com.mobble.mobbleserver.support.fixture.member.MemberTestFixture;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.time.LocalDateTime;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
 class MeetingTest {
-    
+
+    private final Member mockMember = MemberTestFixture.createDefaultMember();
+    private final ClubCategory mockClubCategory = ClubCategory.createClubCategory("SOCCER");
+    private final Club mockClub = ClubTestFixture.createDefaultClub(mockClubCategory);
+    private final ClubMember mockClubMember = ClubMemberTestFixture.createDefaultClubMember(
+            mockMember,
+            mockClub,
+            ClubMemberRole.LEADER,
+            JoinStatus.APPROVED
+    );
+
+    private static final String TITLE = "정기모임";
+    private static final String LOCATION = "체육관";
+    private static final String COST = "5000";
+    private static final int LIMIT = 10;
+    private static final MeetingType TYPE = MeetingType.REGULAR_MEETING;
+    private static final LocalDateTime DATETIME = LocalDateTime.of(2025, 10, 10, 19, 0);
+
+    @Test
+    @DisplayName("모임 생성 성공")
+    void success_when_create_meeting() {
+        // given & when
+        Meeting meeting = Meeting.createMeeting(
+                mockClubMember,
+                TITLE,
+                DATETIME,
+                LOCATION,
+                COST,
+                LIMIT,
+                TYPE
+        );
+
+        // then
+        assertThat(meeting.getClubMember()).isEqualTo(mockClubMember);
+        assertThat(meeting.getTitle()).isEqualTo(TITLE);
+        assertThat(meeting.getDatetime()).isEqualTo(DATETIME);
+        assertThat(meeting.getLocation()).isEqualTo(LOCATION);
+        assertThat(meeting.getCost()).isEqualTo(COST);
+        assertThat(meeting.getMemberLimit()).isEqualTo(LIMIT);
+        assertThat(meeting.getType()).isEqualTo(TYPE);
+        assertThat(meeting.getMeetingMembers()).isEmpty();
+    }
 }
