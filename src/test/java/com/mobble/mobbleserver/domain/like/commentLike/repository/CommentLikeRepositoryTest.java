35ef090da9f4dfae2b72fad7c3410f3979e94842
taejunUM <utj0411@naver.com>
2025-09-15 15:41:42 +0900

    test: 메서드 네이밍 수정

--- src/test/java/com/mobble/mobbleserver/domain/like/commentLike/repository/CommentLikeRepositoryTest.java
+++ src/test/java/com/mobble/mobbleserver/domain/like/commentLike/repository/CommentLikeRepositoryTest.java
@@ -88,7 +88,7 @@ class CommentLikeRepositoryTest {
 
     @Test
     @DisplayName("Article ID로 댓글 좋아요 전체 삭제 성공")
-    void success_when_delete_all_by_comment_id() {
+    void success_when_delete_all_by_article_id() {
         Member member = MemberTestFixture.createDefaultMember();
         ClubCategory clubCategory = ClubCategory.createClubCategory("SOCCER");
         Club club = ClubTestFixture.createDefaultClub(clubCategory);
