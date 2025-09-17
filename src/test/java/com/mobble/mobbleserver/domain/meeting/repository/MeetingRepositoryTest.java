0cdb40dbdc02964cd758f421e5819eb5c0c88d1b
taejunUM <utj0411@naver.com>
2025-09-17 21:45:01 +0900

    test: clubId로 조회 시 Meeting이 없으면 빈 리스트 반환

--- src/test/java/com/mobble/mobbleserver/domain/meeting/repository/MeetingRepositoryTest.java
+++ src/test/java/com/mobble/mobbleserver/domain/meeting/repository/MeetingRepositoryTest.java
@@ -13,7 +13,6 @@ import com.mobble.mobbleserver.support.fixture.clubMember.ClubMemberTestFixture;
 import com.mobble.mobbleserver.support.fixture.meeting.MeetingTestFixture;
 import com.mobble.mobbleserver.support.fixture.member.MemberTestFixture;
 import jakarta.persistence.EntityManager;
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -21,6 +20,8 @@ import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.context.annotation.Import;
 
 import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 @DataJpaTest
 @Import(QueryDslConfig.class)
@@ -62,8 +63,26 @@ class MeetingRepositoryTest {
         List<Meeting> result = meetingRepository.findByClubMember_Club_Id(club.getId());
 
         // then
-        Assertions.assertThat(result).hasSize(2)
+        assertThat(result).hasSize(2)
                 .extracting(Meeting::getId)
                 .containsExactlyInAnyOrder(meeting1.getId(), meeting2.getId());
+    }
+
+    @Test
+    @DisplayName("Club ID로 Meeting 조회 시 Meeting 이 없는 경우, 빈 리스트 반환")
+    void success_when_returns_empty_list_by_club_id() {
+        // given
+        ClubCategory clubCategory = ClubCategory.createClubCategory("SOCCER");
+        Club club = ClubTestFixture.createDefaultClub(clubCategory);
+        em.persist(clubCategory);
+        em.persist(club);
+        em.flush();
+        em.clear();
+
+        // when
+        List<Meeting> result = meetingRepository.findByClubMember_Club_Id(club.getId());
+
+        // then
+        assertThat(result).isEmpty();
     }
 }
