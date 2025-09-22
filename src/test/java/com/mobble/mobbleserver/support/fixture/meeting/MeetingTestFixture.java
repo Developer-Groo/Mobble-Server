f633b09f6cb1cdcdf8d7026287220f8e861fb07f
taejunUM <utj0411@naver.com>
2025-09-22 21:01:14 +0900

    test: MeetingTestFixture 수정
    - ClubMember 파라미터 주입

--- src/test/java/com/mobble/mobbleserver/support/fixture/meeting/MeetingTestFixture.java
+++ src/test/java/com/mobble/mobbleserver/support/fixture/meeting/MeetingTestFixture.java
@@ -1,23 +1,14 @@
 package com.mobble.mobbleserver.support.fixture.meeting;
 
-import com.mobble.mobbleserver.domain.club.core.entity.Club;
-import com.mobble.mobbleserver.domain.clubCategory.entity.ClubCategory;
 import com.mobble.mobbleserver.domain.clubMember.entity.ClubMember;
 import com.mobble.mobbleserver.domain.meeting.entity.Meeting;
 import com.mobble.mobbleserver.domain.meeting.entity.MeetingType;
-import com.mobble.mobbleserver.domain.member.entity.Member;
-import com.mobble.mobbleserver.support.fixture.club.ClubTestFixture;
-import com.mobble.mobbleserver.support.fixture.member.MemberTestFixture;
 
 import java.time.LocalDateTime;
 
 public class MeetingTestFixture {
 
     public static Meeting createDefaultMeeting(ClubMember clubMember) {
-        Member member = MemberTestFixture.createDefaultMember();
-        ClubCategory clubCategory = ClubCategory.createClubCategory("SOCCER");
-        Club club = ClubTestFixture.createDefaultClub(clubCategory);
-
         return Meeting.createMeeting(
                 clubMember,
                 "정기 모임",
