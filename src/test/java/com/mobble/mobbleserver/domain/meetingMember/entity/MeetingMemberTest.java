c4f037f37573a600f42ca2b5e9671fca13881e8d
taejunUM <utj0411@naver.com>
2025-09-23 20:26:27 +0900

    test: createDefaultMeeting 메서드 변경 적용
    - Meeting 생성 시 clubMember 파라미터 전달

--- src/test/java/com/mobble/mobbleserver/domain/meetingMember/entity/MeetingMemberTest.java
+++ src/test/java/com/mobble/mobbleserver/domain/meetingMember/entity/MeetingMemberTest.java
@@ -1,9 +1,17 @@
 package com.mobble.mobbleserver.domain.meetingMember.entity;
 
+import com.mobble.mobbleserver.domain.club.core.entity.Club;
+import com.mobble.mobbleserver.domain.clubCategory.entity.ClubCategory;
+import com.mobble.mobbleserver.domain.clubMember.entity.ClubMember;
+import com.mobble.mobbleserver.domain.clubMember.entity.ClubMemberRole;
+import com.mobble.mobbleserver.domain.clubMember.entity.JoinStatus;
 import com.mobble.mobbleserver.domain.meeting.entity.Meeting;
 import com.mobble.mobbleserver.domain.member.entity.Member;
 import com.mobble.mobbleserver.global.exception.common.DomainException;
 import com.mobble.mobbleserver.global.exception.errorCode.meeting.MeetingMemberErrorCode;
+import com.mobble.mobbleserver.support.fixture.club.ClubTestFixture;
+import com.mobble.mobbleserver.support.fixture.clubCategory.ClubCategoryTestFixture;
+import com.mobble.mobbleserver.support.fixture.clubMember.ClubMemberTestFixture;
 import com.mobble.mobbleserver.support.fixture.meeting.MeetingTestFixture;
 import com.mobble.mobbleserver.support.fixture.member.MemberTestFixture;
 import org.junit.jupiter.api.DisplayName;
@@ -17,8 +25,11 @@ class MeetingMemberTest {
     @DisplayName("Meeting Member 생성 성공")
     void success_create_meeting_member() {
         // given
-        Meeting meeting = MeetingTestFixture.createDefaultMeeting();
         Member member = MemberTestFixture.createDefaultMember();
+        ClubCategory clubCategory = ClubCategoryTestFixture.createDefaultCategory();
+        Club club = ClubTestFixture.createDefaultClub(clubCategory);
+        ClubMember clubMember = ClubMemberTestFixture.createDefaultClubMember(member, club, ClubMemberRole.LEADER, JoinStatus.APPROVED);
+        Meeting meeting = MeetingTestFixture.createDefaultMeeting(clubMember);
 
         // when
         MeetingMember meetingMember = MeetingMember.createMeetingMember(meeting, member);
@@ -40,12 +51,16 @@ class MeetingMemberTest {
                 .isInstanceOf(DomainException.class)
                 .hasMessage(MeetingMemberErrorCode.MEETING_REQUIRED.message());
     }
-    
+
     @Test
     @DisplayName("Member 가 null 인 경우 예외 발생")
     void fail_when_member_is_null() {
         // given
-        Meeting meeting = MeetingTestFixture.createDefaultMeeting();
+        Member member = MemberTestFixture.createDefaultMember();
+        ClubCategory clubCategory = ClubCategoryTestFixture.createDefaultCategory();
+        Club club = ClubTestFixture.createDefaultClub(clubCategory);
+        ClubMember clubMember = ClubMemberTestFixture.createDefaultClubMember(member, club, ClubMemberRole.LEADER, JoinStatus.APPROVED);
+        Meeting meeting = MeetingTestFixture.createDefaultMeeting(clubMember);
 
         // when & then
         assertThatThrownBy(() -> MeetingMember.createMeetingMember(meeting, null))
