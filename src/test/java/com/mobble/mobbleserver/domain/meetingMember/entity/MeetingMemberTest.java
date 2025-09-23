81c4926c4631ed2486ea749d2a95ad847ee868c4
taejunUM <utj0411@naver.com>
2025-09-23 20:19:53 +0900

    test: Meeting이 null인 경우 예외 발생

--- src/test/java/com/mobble/mobbleserver/domain/meetingMember/entity/MeetingMemberTest.java
+++ src/test/java/com/mobble/mobbleserver/domain/meetingMember/entity/MeetingMemberTest.java
@@ -2,6 +2,8 @@ package com.mobble.mobbleserver.domain.meetingMember.entity;
 
 import com.mobble.mobbleserver.domain.meeting.entity.Meeting;
 import com.mobble.mobbleserver.domain.member.entity.Member;
+import com.mobble.mobbleserver.global.exception.common.DomainException;
+import com.mobble.mobbleserver.global.exception.errorCode.meeting.MeetingMemberErrorCode;
 import com.mobble.mobbleserver.support.fixture.meeting.MeetingTestFixture;
 import com.mobble.mobbleserver.support.fixture.member.MemberTestFixture;
 import org.junit.jupiter.api.DisplayName;
@@ -25,5 +27,17 @@ class MeetingMemberTest {
         assertThat(meetingMember).isNotNull();
         assertThat(meetingMember.getMeeting()).isEqualTo(meeting);
         assertThat(meetingMember.getMember()).isEqualTo(member);
+    }
+
+    @Test
+    @DisplayName("Meeting 이 null 인 경우 예외 발생")
+    void fail_when_meeting_is_null() {
+        // given
+        Member member = MemberTestFixture.createDefaultMember();
+
+        // when & then
+        assertThatThrownBy(() -> MeetingMember.createMeetingMember(null, member))
+                .isInstanceOf(DomainException.class)
+                .hasMessage(MeetingMemberErrorCode.MEETING_REQUIRED.message());
     }
 }
