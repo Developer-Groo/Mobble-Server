dcbdc4444cc078b7a69a056947e09148a1b8c063
Groo <woohyeon3382@naver.com>
2025-09-24 20:17:34 +0900

    feat: NotificationSettingRepository 구현

---
+++ src/main/java/com/mobble/mobbleserver/domain/notification/setting/repository/NotificationSettingRepository.java
@@ -0,0 +1,11 @@
+package com.mobble.mobbleserver.domain.notification.setting.repository;
+
+import com.mobble.mobbleserver.domain.notification.setting.entity.NotificationSetting;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface NotificationSettingRepository extends JpaRepository<NotificationSetting, Long> {
+
+    Optional<NotificationSetting> findByMember_Id(Long memberId);
+}
