3f35848d35193f3811eb7f97983c38ead970c6e9
Groo <woohyeon3382@naver.com>
2025-09-25 17:54:57 +0900

    docs: webmvc-api 버전 설정

--- build.gradle.kts
+++ build.gradle.kts
@@ -31,7 +31,7 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("org.springframework.boot:spring-boot-starter-websocket")
     implementation("org.springframework.boot:spring-boot-starter-security")
-    implementation("org.springdoc:springdoc-openapi-starter-webmvc-api")
+    implementation("org.springdoc:springdoc-openapi-starter-webmvc-api:2.8.5")
 
     // Lombok
     compileOnly("org.projectlombok:lombok")
