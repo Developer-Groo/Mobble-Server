16d6432900755fb559f0a9e719091f7fe1b5286e
taejunUM <utj0411@naver.com>
2025-09-26 17:36:03 +0900

    test: 활성 회원 조회 성공 (isDeleted = false)

---
+++ src/test/java/com/mobble/mobbleserver/domain/member/repository/MemberRepositoryTest.java
@@ -0,0 +1,51 @@
+package com.mobble.mobbleserver.domain.member.repository;
+
+import com.mobble.mobbleserver.account.auth.oauth.service.SocialProvider;
+import com.mobble.mobbleserver.config.QueryDslConfig;
+import com.mobble.mobbleserver.domain.member.entity.Member;
+import com.mobble.mobbleserver.support.fixture.member.MemberTestFixture;
+import jakarta.persistence.EntityManager;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.context.annotation.Import;
+
+import java.util.Optional;
+
+import static org.assertj.core.api.Assertions.*;
+
+@DataJpaTest
+@Import(QueryDslConfig.class)
+public class MemberRepositoryTest {
+
+    @Autowired
+    private MemberRepository memberRepository;
+
+    @Autowired
+    private EntityManager em;
+
+    private static final SocialProvider SOCIAL_PROVIDER = SocialProvider.NAVER;
+    private static final String SOCIAL_ID = "123456";
+
+    @Nested
+    @DisplayName("findByIdAndIsDeletedFalse")
+    class FindByIdAndIsDeletedFalse {
+
+        @Test
+        @DisplayName("활성 회원 조회 성공")
+        void success_when_get_member_not_deleted() {
+            // given
+            Member member = MemberTestFixture.createDefaultMember();
+            memberRepository.save(member);
+
+            // when
+            Optional<Member> result = memberRepository.findByIdAndIsDeletedFalse(member.getId());
+
+            // then
+            assertThat(result).isPresent();
+            assertThat(result.get().isDeleted()).isFalse();
+        }
+    }
+}
