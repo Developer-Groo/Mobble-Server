da53d771a84de3f6378908b88a7bb856bb28a5c2
taejunUM <utj0411@naver.com>
2025-09-26 15:22:41 +0900

    refactor: 미사용 repository 메서드 삭제

--- src/main/java/com/mobble/mobbleserver/domain/member/repository/MemberRepository.java
+++ src/main/java/com/mobble/mobbleserver/domain/member/repository/MemberRepository.java
@@ -13,8 +13,6 @@ public interface MemberRepository extends JpaRepository<Member, Long> {
     // 활성 회원(isDeleted = false)만 조회하는 메소드
     Optional<Member> findByIdAndIsDeletedFalse(Long memberId);
 
-    Optional<Member> findBySocialProviderAndSocialIdAndIsDeletedFalse(SocialProvider socialProvider, String socialId);
-
     // soft-delete(isDeleted = true) 회원을 포함한 모든 회원 조회
     List<Member> findAllByIsDeletedTrueAndDeletedAtBefore(LocalDateTime withdrewDate);
 
