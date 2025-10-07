20da27212d4ebc8fa12cce4ec67433625052af25
Hojin02 <140497846+Hojin02@users.noreply.github.com>
2025-10-08 02:28:26 +0900

    feat: GroundEntity 설계

---
+++ src/main/java/com/mobble/mobbleserver/domain/ground/entity/Ground.java
@@ -0,0 +1,27 @@
+package com.mobble.mobbleserver.domain.ground.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "ground")
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Ground {
+
+    @Id
+    @Column(name = "ground_code", length = 20)
+    private Long code;
+
+    private String sido;
+
+    private String sigungu;
+
+    private String eubmyeondong;
+
+}
