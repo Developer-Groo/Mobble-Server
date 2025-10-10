705927b1ad31c34b0cf000074eeecf2646af9829
Hojin02 <140497846+Hojin02@users.noreply.github.com>
2025-10-10 19:55:53 +0900

    style: 임시주석

--- src/test/java/com/mobble/mobbleserver/support/fixture/club/ClubTestFixture.java
+++ src/test/java/com/mobble/mobbleserver/support/fixture/club/ClubTestFixture.java
@@ -1,18 +1,18 @@
-package com.mobble.mobbleserver.support.fixture.club;
-
-import com.mobble.mobbleserver.domain.club.core.entity.Club;
-import com.mobble.mobbleserver.domain.clubCategory.entity.ClubCategory;
-
-public class ClubTestFixture {
-
-    public static Club createDefaultClub(ClubCategory clubCategory) {
-        return Club.createClub(
-                clubCategory,
-                "name",
-                "ground",
-                "address",
-                1,
-                true
-        );
-    }
-}
+//package com.mobble.mobbleserver.support.fixture.club;
+//
+//import com.mobble.mobbleserver.domain.club.core.entity.Club;
+//import com.mobble.mobbleserver.domain.clubCategory.entity.ClubCategory;
+//
+//public class ClubTestFixture {
+//
+//    public static Club createDefaultClub(ClubCategory clubCategory) {
+//        return Club.createClub(
+//                clubCategory,
+//                "name",
+//                "ground",
+//                "address",
+//                1,
+//                true
+//        );
+//    }
+//}
