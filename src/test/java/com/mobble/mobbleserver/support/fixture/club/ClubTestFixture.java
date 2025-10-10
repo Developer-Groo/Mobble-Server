faf12619403e732503e9f5465a49f6d396d52bf4
Hojin02 <140497846+Hojin02@users.noreply.github.com>
2025-10-10 20:00:07 +0900

    fix: fixture 수정

--- src/test/java/com/mobble/mobbleserver/support/fixture/club/ClubTestFixture.java
+++ src/test/java/com/mobble/mobbleserver/support/fixture/club/ClubTestFixture.java
@@ -1,18 +1,16 @@
-//package com.mobble.mobbleserver.support.fixture.club;
-//
-//import com.mobble.mobbleserver.domain.club.core.entity.Club;
-//import com.mobble.mobbleserver.domain.clubCategory.entity.ClubCategory;
-//
-//public class ClubTestFixture {
-//
-//    public static Club createDefaultClub(ClubCategory clubCategory) {
-//        return Club.createClub(
-//                clubCategory,
-//                "name",
-//                "ground",
-//                "address",
-//                1,
-//                true
-//        );
-//    }
-//}
+package com.mobble.mobbleserver.support.fixture.club;
+
+import com.mobble.mobbleserver.domain.club.core.entity.Club;
+import com.mobble.mobbleserver.domain.clubCategory.entity.ClubCategory;
+
+public class ClubTestFixture {
+
+    public static Club createDefaultClub(ClubCategory clubCategory) {
+        return Club.createClub(
+                clubCategory,
+                "name",
+                1,
+                true
+        );
+    }
+}
