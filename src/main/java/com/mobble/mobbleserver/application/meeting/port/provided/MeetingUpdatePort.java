db29a87b0650251d2f7a8c684bdae12fb93e2371
taejunUM <utj0411@naver.com>
2025-10-14 20:59:54 +0900

    refactor: MeetingUpdatePort 인터페이스 구현

---
+++ src/main/java/com/mobble/mobbleserver/application/meeting/port/provided/MeetingUpdatePort.java
@@ -0,0 +1,13 @@
+package com.mobble.mobbleserver.application.meeting.port.provided;
+
+import com.mobble.mobbleserver.domain.meeting.Meeting;
+import com.mobble.mobbleserver.infrastructure.web.meeting.dto.request.MeetingUpdateRequestDto;
+
+public interface MeetingUpdatePort {
+
+    Meeting updateMeeting(Long memberId,
+                          Long clubId,
+                          Long meetingId,
+                          MeetingUpdateRequestDto dto
+    );
+}
