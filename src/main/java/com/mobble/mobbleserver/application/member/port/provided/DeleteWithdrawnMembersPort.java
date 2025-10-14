7bb98c40ca3b83e8c7d6d7ae70f9cbfaa5aa67b8
taejunUM <utj0411@naver.com>
2025-10-14 22:34:35 +0900

    refactor: 클래스 네이밍 변경
    withdrew > softDeleted

--- src/main/java/com/mobble/mobbleserver/application/member/port/provided/DeleteWithdrawnMembersPort.java
+++
@@ -1,8 +0,0 @@
-package com.mobble.mobbleserver.application.member.port.provided;
-
-import java.time.LocalDateTime;
-
-public interface DeleteWithdrawnMembersPort {
-
-    void deleteWithdrawnMembers(LocalDateTime withdrewDate);
-}
--- src/main/java/com/mobble/mobbleserver/application/member/port/provided/MemberDeletePort.java
+++
@@ -1,6 +0,0 @@
-package com.mobble.mobbleserver.application.member.port.provided;
-
-public interface MemberDeletePort {
-
-    void deleteMember(Long memberId);
-}
