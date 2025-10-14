2bb78149040ec70a49e5e3365265db155637c941
taejunUM <utj0411@naver.com>
2025-10-14 10:17:35 +0900

    refactor: MemberReadPort 구현

---
+++ src/main/java/com/mobble/mobbleserver/application/member/port/required/MemberReadPort.java
@@ -0,0 +1,19 @@
+package com.mobble.mobbleserver.application.member.port.required;
+
+import com.mobble.mobbleserver.account.auth.oauth.service.SocialProvider;
+import com.mobble.mobbleserver.domain.member.Member;
+
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.Optional;
+
+public interface MemberReadPort {
+
+    // 활성 회원(isDeleted = false)만 조회하는 메소드
+    Optional<Member> findByIdAndIsDeletedFalse(Long memberId);
+
+    // soft-delete(isDeleted = true) 회원을 포함한 모든 회원 조회
+    List<Member> findAllByIsDeletedTrueAndDeletedAtBefore(LocalDateTime withdrewDate);
+
+    Optional<Member> findBySocialProviderAndSocialId(SocialProvider socialProvider, String socialId);
+}
