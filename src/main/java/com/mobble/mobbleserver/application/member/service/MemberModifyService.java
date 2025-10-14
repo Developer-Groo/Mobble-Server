4270a1ef1d49a9f21d005d58348f8179b3a14003
taejunUM <utj0411@naver.com>
2025-10-14 10:42:10 +0900

    refactor: updateMember 반환 타입 변경

--- src/main/java/com/mobble/mobbleserver/application/member/service/MemberModifyService.java
+++ src/main/java/com/mobble/mobbleserver/application/member/service/MemberModifyService.java
@@ -7,7 +7,6 @@ import com.mobble.mobbleserver.domain.member.Member;
 import com.mobble.mobbleserver.global.exception.common.DomainException;
 import com.mobble.mobbleserver.global.exception.errorCode.member.MemberErrorCode;
 import com.mobble.mobbleserver.infrastructure.web.member.dto.request.MemberUpdateRequestDto;
-import com.mobble.mobbleserver.infrastructure.web.member.dto.response.MemberResponseDto;
 import com.mobble.mobbleserver.refactor.ground.entity.Ground;
 import com.mobble.mobbleserver.refactor.ground.repository.GroundRepository;
 import lombok.RequiredArgsConstructor;
@@ -24,13 +23,12 @@ public class MemberModifyService implements MemberUpdatePort, MemberDeletePort {
     private final MemberReadPort memberReadPort;
 
     @Override
-    public MemberResponseDto updateMember(Long memberId, MemberUpdateRequestDto dto) {
+    public Member updateMember(Long memberId, MemberUpdateRequestDto dto) {
         Member member = findMemberByMemberIdOrThrow(memberId);
         Ground ground = groundRepository.findGroundByCode(dto.groundCode())
                 .orElseThrow();
-        Member updateMember = member.updateMember(ground, dto.profileImage());
 
-        return MemberResponseDto.toDto(updateMember);
+        return member.updateMember(ground, dto.profileImage());
     }
 
     @Override
