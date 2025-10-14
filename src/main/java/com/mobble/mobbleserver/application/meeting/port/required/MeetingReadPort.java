b4aa19ea6685147cc6b65d16e16e0083442d0a1c
taejunUM <utj0411@naver.com>
2025-10-14 21:13:23 +0900

    refactor: findById 타입 수정
    Member > Optional<Member>

--- src/main/java/com/mobble/mobbleserver/application/meeting/port/required/MeetingReadPort.java
+++ src/main/java/com/mobble/mobbleserver/application/meeting/port/required/MeetingReadPort.java
@@ -3,10 +3,11 @@ package com.mobble.mobbleserver.application.meeting.port.required;
 import com.mobble.mobbleserver.domain.meeting.Meeting;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface MeetingReadPort {
 
-    Meeting findById(Long meetingId);
-    
+    Optional<Meeting> findById(Long meetingId);
+
     List<Meeting> findByClubMember_Club_Id(Long clubId);
 }
