ddfcc0198415fb3ee3555b756a6e243d6a8b11b0
taejunUM <utj0411@naver.com>
2025-10-14 22:37:01 +0900

    refactor: MemberDeleteService, MemberModifyService 클래스 병합

--- src/main/java/com/mobble/mobbleserver/application/member/service/MemberModifyService.java
+++ src/main/java/com/mobble/mobbleserver/application/member/service/MemberModifyService.java
@@ -1,8 +1,10 @@
 package com.mobble.mobbleserver.application.member.service;
 
-import com.mobble.mobbleserver.application.member.port.provided.MemberDeletePort;
+import com.mobble.mobbleserver.application.member.port.provided.MemberSoftDeletePort;
 import com.mobble.mobbleserver.application.member.port.provided.MemberUpdatePort;
+import com.mobble.mobbleserver.application.member.port.provided.MembersDeletePort;
 import com.mobble.mobbleserver.application.member.port.required.MemberReadPort;
+import com.mobble.mobbleserver.application.member.port.required.MemberWritePort;
 import com.mobble.mobbleserver.domain.member.Member;
 import com.mobble.mobbleserver.global.exception.common.DomainException;
 import com.mobble.mobbleserver.global.exception.errorCode.member.MemberErrorCode;
@@ -10,17 +12,23 @@ import com.mobble.mobbleserver.infrastructure.web.member.dto.request.MemberUpdat
 import com.mobble.mobbleserver.refactor.ground.entity.Ground;
 import com.mobble.mobbleserver.refactor.ground.repository.GroundRepository;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Slf4j
 @Service
 @Transactional
 @RequiredArgsConstructor
-public class MemberModifyService implements MemberUpdatePort, MemberDeletePort {
+public class MemberModifyService implements MemberUpdatePort, MemberSoftDeletePort, MembersDeletePort {
 
     private final GroundRepository groundRepository;
 
     private final MemberReadPort memberReadPort;
+    private final MemberWritePort memberWritePort;
 
     @Override
     public Member updateMember(Long memberId, MemberUpdateRequestDto dto) {
@@ -32,9 +40,25 @@ public class MemberModifyService implements MemberUpdatePort, MemberDeletePort {
     }
 
     @Override
-    public void deleteMember(Long memberId) {
+    public void softDeleteMember(Long memberId) {
         Member member = findMemberByMemberIdOrThrow(memberId);
         member.softDelete();
+    }
+
+    @Override
+    public void deleteMembers(LocalDateTime softDeletedDate) {
+        log.info("soft deleted members delete time: {}", softDeletedDate);
+
+        List<Member> softDeletedMembers = memberReadPort.findAllByIsDeletedTrueAndDeletedAtBefore(softDeletedDate);
+
+        if (softDeletedMembers.isEmpty()) {
+            log.info("Not found soft deleted members to delete");
+            return;
+        }
+        log.info("Deleting {} soft deleted members.", softDeletedMembers.size());
+
+        memberWritePort.deleteAll(softDeletedMembers);
+        log.info("Finished deleting soft deleted members.");
     }
 
     private Member findMemberByMemberIdOrThrow(Long memberId) {
