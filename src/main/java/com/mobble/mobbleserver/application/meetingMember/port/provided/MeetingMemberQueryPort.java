8b5763bed3311636a1a0964573067092c8a5215a
taejunUM <utj0411@naver.com>
2025-10-15 20:06:12 +0900

    refactor: MeetingMemberQueryPort 메서드 시그니처 변경

--- src/main/java/com/mobble/mobbleserver/application/meetingMember/port/provided/MeetingMemberQueryPort.java
+++ src/main/java/com/mobble/mobbleserver/application/meetingMember/port/provided/MeetingMemberQueryPort.java
@@ -1,10 +1,11 @@
 package com.mobble.mobbleserver.application.meetingMember.port.provided;
 
 import com.mobble.mobbleserver.infrastructure.web.meetingMember.dto.response.MeetingAttendanceResponseDto;
+import com.mobble.mobbleserver.infrastructure.web.meetingMember.dto.response.MeetingMemberListResponseDto;
 
 public interface MeetingMemberQueryPort {
 
     MeetingAttendanceResponseDto getIsAttended(Long meetingId, Long memberId);
 
-    MeetingAttendanceResponseDto getMeetingMembers(Long meetingId, Long memberId);
+    MeetingMemberListResponseDto getMeetingMembers(Long meetingId, Long memberId);
 }
