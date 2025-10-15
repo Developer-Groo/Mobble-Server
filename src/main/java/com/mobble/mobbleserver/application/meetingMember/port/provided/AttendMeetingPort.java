8a5b4e93f1f287546e675f99e062dcdfa2331362
taejunUM <utj0411@naver.com>
2025-10-15 18:05:48 +0900

    refactor: 미사용 클래스 삭제

--- src/main/java/com/mobble/mobbleserver/application/meetingMember/port/provided/AttendMeetingPort.java
+++
@@ -1,6 +0,0 @@
-package com.mobble.mobbleserver.application.meetingMember.port.provided;
-
-public interface AttendMeetingPort {
-
-    AttendMeetingResult toggleAttendance(Long meetingId, Long memberId);
-}
--- src/main/java/com/mobble/mobbleserver/application/meetingMember/port/provided/AttendMeetingResult.java
+++
@@ -1,4 +0,0 @@
-package com.mobble.mobbleserver.application.meetingMember.port.provided;
-
-public record AttendMeetingResult(Long meetingId, boolean attended) {
-}
