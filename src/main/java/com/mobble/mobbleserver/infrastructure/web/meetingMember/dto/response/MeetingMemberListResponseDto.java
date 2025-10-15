1f44da5b110987cb68ef42536e1a92112c3f8174
taejunUM <utj0411@naver.com>
2025-10-15 22:29:43 +0900

    refactor: MeetingMemberListResponseDto 수정

--- src/main/java/com/mobble/mobbleserver/infrastructure/web/meetingMember/dto/response/MeetingMemberListResponseDto.java
+++ src/main/java/com/mobble/mobbleserver/infrastructure/web/meetingMember/dto/response/MeetingMemberListResponseDto.java
@@ -6,7 +6,9 @@ import java.util.List;
 
 public record MeetingMemberListResponseDto(Long meetingId, List<MeetingMemberResponseDto> meetingMembers) {
 
-    public static MeetingMemberListResponseDto toDto(Long meetingId, List<MeetingMember> meetingMemberList) {
+    public static MeetingMemberListResponseDto toDto(List<MeetingMember> meetingMemberList) {
+        Long meetingId = meetingMemberList.get(0).getMeeting().getId();
+
         List<MeetingMemberResponseDto> meetingMembers = meetingMemberList.stream()
                 .map(meetingMember -> MeetingMemberResponseDto.toDto(meetingMember.getMember()))
                 .toList();
