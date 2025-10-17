e5061837a54550c335605ff2454969fd78861d2b
Groo <woohyeon3382@naver.com>
2025-10-17 22:13:08 +0900

    style: 주석 추가

--- src/main/java/com/mobble/mobbleserver/refactor/chat/directChatRoom/repository/DirectChatRoomQueryRepository.java
+++ src/main/java/com/mobble/mobbleserver/refactor/chat/directChatRoom/repository/DirectChatRoomQueryRepository.java
@@ -8,5 +8,6 @@ public interface DirectChatRoomQueryRepository {
 
     boolean existsDirectChatRoomByBetweenMembers(Long senderId, Long receiverId);
 
+    // Todo: N+1 문제 해결 필요 Member 접근 시 발생 가능성 있음
     List<DirectRoomInfo> findDirectChatRoomsAllByMemberId(Long memberId);
 }
