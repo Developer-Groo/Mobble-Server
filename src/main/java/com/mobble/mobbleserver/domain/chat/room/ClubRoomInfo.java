041eec90b19e5eeaabfcbeb0c13ccba65eec95a5
Groo <woohyeon3382@naver.com>
2025-10-17 22:03:11 +0900

    refactor: ClubRoomInfo 엔티티 수정
    - 불필요한 메서드 제거
    - 생성자 네이밍 수정
    - RoomInfo 상속 구현

--- src/main/java/com/mobble/mobbleserver/domain/chat/room/ClubRoomInfo.java
+++ src/main/java/com/mobble/mobbleserver/domain/chat/room/ClubRoomInfo.java
@@ -2,52 +2,28 @@ package com.mobble.mobbleserver.domain.chat.room;
 
 import com.mobble.mobbleserver.refactor.club.core.entity.Club;
 import jakarta.persistence.*;
-import lombok.AccessLevel;
-import lombok.Builder;
-import lombok.Getter;
-import lombok.RequiredArgsConstructor;
+import lombok.*;
 
 @Getter
 @Entity
-@RequiredArgsConstructor(access = AccessLevel.PROTECTED)
-public class ClubRoomInfo {
-
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "club_chat_room_id")
-    private Long id;
+@PrimaryKeyJoinColumn(name = "room_info_id")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class ClubRoomInfo extends RoomInfo{
 
     @OneToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "club_id", nullable = false, unique = true)
     private Club club;
 
-    @OneToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "chat_room_id", nullable = false, unique = true)
-    private ChatRoom chatRoom;
-
     @Builder(access = AccessLevel.PRIVATE)
-    private ClubRoomInfo(Club club, ChatRoom chatRoom) {
+    private ClubRoomInfo(ChatRoom chatRoom, Club club) {
+        super.setChatRoom(chatRoom);
         this.club = club;
-        this.chatRoom = chatRoom;
     }
 
-    public static ClubRoomInfo createClubChatRoom(Club club, ChatRoom chatRoom) {
+    static ClubRoomInfo create(ChatRoom chatRoom, Club club) {
         return ClubRoomInfo.builder()
-                .club(club)
                 .chatRoom(chatRoom)
+                .club(club)
                 .build();
-    }
-
-    public void attachTo(Club club) {
-        if (this.club != club) throw new IllegalArgumentException("");
-
-        club.setClubChatRoomInternal(this);
-    }
-
-    public void detach() {
-        if (this.club == null) return;
-
-        this.club.setClubChatRoomInternal(null);
-        this.club = null;
     }
 }
