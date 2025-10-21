298d5e4ce47b0224df5938414e60cd229c3ea3ed
Groo <woohyeon3382@naver.com>
2025-10-21 20:56:56 +0900

    feat: findChatRoomById 메서드 시그니처 정의

--- src/main/java/com/mobble/mobbleserver/application/chat/room/port/required/ChatRoomReadPort.java
+++ src/main/java/com/mobble/mobbleserver/application/chat/room/port/required/ChatRoomReadPort.java
@@ -1,4 +1,12 @@
 package com.mobble.mobbleserver.application.chat.room.port.required;
 
+import com.mobble.mobbleserver.domain.chat.room.ChatRoom;
+
+import java.util.Optional;
+
 public interface ChatRoomReadPort {
+
+    Optional<ChatRoom> findChatRoomById(Long chatRoomId);
+
+    boolean existsClubRoomInfo(Long clubId);
 }
--- src/main/java/com/mobble/mobbleserver/application/chat/room/port/required/ChatRoomWritePort.java
+++ src/main/java/com/mobble/mobbleserver/application/chat/room/port/required/ChatRoomWritePort.java
@@ -2,5 +2,4 @@ package com.mobble.mobbleserver.application.chat.room.port.required;
 
 public interface ChatRoomWritePort {
 
-    boolean existsClubRoomInfoByClubId(Long clubId);
 }
