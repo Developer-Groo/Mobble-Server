7b3bfc5195455f2d3dd5858bc2e5cf85c1a2f68d
Groo <woohyeon3382@naver.com>
2025-10-21 20:57:27 +0900

    feat: findChatRoomById 메서드 구현

--- src/main/java/com/mobble/mobbleserver/infrastructure/persistence/chat/ChatRoomPersistenceAdapter.java
+++ src/main/java/com/mobble/mobbleserver/infrastructure/persistence/chat/ChatRoomPersistenceAdapter.java
@@ -2,24 +2,33 @@ package com.mobble.mobbleserver.infrastructure.persistence.chat;
 
 import com.mobble.mobbleserver.application.chat.room.port.required.ChatRoomReadPort;
 import com.mobble.mobbleserver.application.chat.room.port.required.ChatRoomWritePort;
+import com.mobble.mobbleserver.domain.chat.room.ChatRoom;
 import com.mobble.mobbleserver.infrastructure.persistence.chat.club.ClubRoomInfoRepository;
+import com.mobble.mobbleserver.infrastructure.persistence.chat.common.ChatRoomRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
 
 @Repository
 @RequiredArgsConstructor
 public class ChatRoomPersistenceAdapter implements ChatRoomWritePort, ChatRoomReadPort {
 
+    private final ChatRoomRepository chatRoomRepository;
     private final ClubRoomInfoRepository clubRoomInfoRepository;
 
-    /* ChatRoomWritePort-Club */
-    @Override
-    public boolean existsClubRoomInfoByClubId(Long clubId) {
-        return clubRoomInfoRepository.existsClubRoomInfoByClubId(clubId);
-    }
+    /* ChatRoomWritePort */
 
 
     /* ChatRoomReadPort */
+    @Override
+    public Optional<ChatRoom> findChatRoomById(Long chatRoomId) {
+        return chatRoomRepository.findById(chatRoomId);
+    }
 
-
+    /* ChatRoomReadPort-Club */
+    @Override
+    public boolean existsClubRoomInfo(Long clubId) {
+        return clubRoomInfoRepository.existsClubRoomInfoByClubId(clubId);
+    }
 }
