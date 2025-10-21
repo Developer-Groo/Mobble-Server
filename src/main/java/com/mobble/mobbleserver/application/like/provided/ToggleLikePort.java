4c648990db19eb19c9a296e15db02a8c9514ca67
taejunUM <utj0411@naver.com>
2025-10-21 17:14:53 +0900

    refactor: toggleLike 반환 타입 변경
    dto > void

--- src/main/java/com/mobble/mobbleserver/application/like/provided/ToggleLikePort.java
+++ src/main/java/com/mobble/mobbleserver/application/like/provided/ToggleLikePort.java
@@ -1,9 +1,8 @@
 package com.mobble.mobbleserver.application.like.provided;
 
 import com.mobble.mobbleserver.domain.like.baseLike.LikeType;
-import com.mobble.mobbleserver.infrastructure.web.like.dto.response.LikeToggleResponseDto;
 
 public interface ToggleLikePort {
 
-    LikeToggleResponseDto toggleLike(LikeType likeType, Long targetId, Long memberId);
+    void toggleLike(LikeType likeType, Long targetId, Long memberId);
 }
