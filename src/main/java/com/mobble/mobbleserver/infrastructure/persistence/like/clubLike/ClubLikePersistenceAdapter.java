726f147a3cbf8c0f4635ac9263d24b07da6e9221
taejunUM <utj0411@naver.com>
2025-10-21 16:15:25 +0900

    refactor: ClubLikePersistenceAdapter 구현

---
+++ src/main/java/com/mobble/mobbleserver/infrastructure/persistence/like/clubLike/ClubLikePersistenceAdapter.java
@@ -0,0 +1,39 @@
+package com.mobble.mobbleserver.infrastructure.persistence.like.clubLike;
+
+import com.mobble.mobbleserver.application.like.required.LikeReadPort;
+import com.mobble.mobbleserver.application.like.required.LikeWritePort;
+import com.mobble.mobbleserver.domain.like.baseLike.LikeType;
+import com.mobble.mobbleserver.domain.like.clubLike.ClubLike;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository("clubLikePersistenceAdapter")
+@RequiredArgsConstructor
+public class ClubLikePersistenceAdapter implements LikeWritePort<ClubLike>, LikeReadPort<ClubLike> {
+
+    private final JpaClubLikeRepository jpaClubLikeRepository;
+
+    /**
+     * LikeWritePort
+     */
+    @Override
+    public ClubLike save(ClubLike clubLike) {
+        return jpaClubLikeRepository.save(clubLike);
+    }
+
+    @Override
+    public void delete(ClubLike clubLike) {
+        jpaClubLikeRepository.delete(clubLike);
+    }
+
+    /**
+     * LikeReadPort
+     */
+    @Override
+    public Optional<ClubLike> findLike(LikeType likeType, Long targetId, Long memberId) {
+        return jpaClubLikeRepository.findLikedByClubIdAndMemberId(targetId, memberId);
+    }
+
+}
