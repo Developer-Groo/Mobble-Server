d1be337a3c89e597a75126f81ef896a0b60105d9
Hojin02 <140497846+Hojin02@users.noreply.github.com>
2025-10-21 11:01:19 +0900

    refactor: 패키지 변경에 따른 임포트 수정

--- src/test/java/com/mobble/mobbleserver/support/fixture/article/ArticleTestFixture.java
+++ src/test/java/com/mobble/mobbleserver/support/fixture/article/ArticleTestFixture.java
@@ -2,7 +2,7 @@ package com.mobble.mobbleserver.support.fixture.article;
 
 import com.mobble.mobbleserver.refactor.article.entity.Article;
 import com.mobble.mobbleserver.refactor.article.entity.ArticleType;
-import com.mobble.mobbleserver.refactor.club.core.entity.Club;
+import com.mobble.mobbleserver.domain.club.core.Club;
 import com.mobble.mobbleserver.refactor.clubCategory.entity.ClubCategory;
 import com.mobble.mobbleserver.domain.member.Member;
 import com.mobble.mobbleserver.support.fixture.club.ClubTestFixture;
