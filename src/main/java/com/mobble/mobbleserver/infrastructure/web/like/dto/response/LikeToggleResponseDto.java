ccb573e2d1254fa365d07ffb61e82b987e8d0030
taejunUM <utj0411@naver.com>
2025-10-22 17:52:11 +0900

    refactor: 미사용 dto 제거

--- src/main/java/com/mobble/mobbleserver/infrastructure/web/like/dto/response/LikeToggleResponseDto.java
+++
@@ -1,8 +0,0 @@
-package com.mobble.mobbleserver.infrastructure.web.like.dto.response;
-
-public record LikeToggleResponseDto(Long targetId, boolean isLiked) {
-
-    public static LikeToggleResponseDto toDto(Long targetId, boolean isLiked) {
-        return new LikeToggleResponseDto(targetId, isLiked);
-    }
-}
