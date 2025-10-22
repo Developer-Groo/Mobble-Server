88d7987b0b0b9859e3e8c7a2e0d8666e23159267
Groo <woohyeon3382@naver.com>
2025-10-22 18:18:23 +0900

    refactor: DirectChatRoomController 메서드 이동 및 필요하지 않은 필드 제거

--- src/main/java/com/mobble/mobbleserver/refactor/chat/directChatRoom/controller/DirectChatRoomController.java
+++ src/main/java/com/mobble/mobbleserver/refactor/chat/directChatRoom/controller/DirectChatRoomController.java
@@ -1,20 +1,15 @@
 package com.mobble.mobbleserver.refactor.chat.directChatRoom.controller;
 
-import com.mobble.mobbleserver.application.chat.room.port.provided.common.ChatRoomExitPort;
-import com.mobble.mobbleserver.application.chat.room.port.provided.direct.DirectChatRoomCreatePort;
-import com.mobble.mobbleserver.application.chat.room.port.provided.direct.DirectChatRoomQueryPort;
 import com.mobble.mobbleserver.refactor.chat.chatMessage.dto.request.ChatMessageRequestDto;
 import com.mobble.mobbleserver.refactor.chat.chatMessage.dto.response.ChatMessageResponseDto;
-import com.mobble.mobbleserver.refactor.chat.directChatRoom.dto.request.DirectChatMessageRequestDto;
-import com.mobble.mobbleserver.refactor.chat.directChatRoom.dto.request.DirectChatRoomCreateRequestDto;
-import com.mobble.mobbleserver.refactor.chat.directChatRoom.dto.response.DirectChatRoomPreviewResponseDto;
+import com.mobble.mobbleserver.infrastructure.web.chat.room.direct.dto.request.DirectChatMessageRequestDto;
 import com.mobble.mobbleserver.refactor.chat.directChatRoom.service.DirectChatRoomService;
 import jakarta.validation.Valid;
-import jakarta.validation.constraints.Positive;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
@@ -28,56 +23,20 @@ public class DirectChatRoomController {
 
     private final DirectChatRoomService directChatRoomService;
 
-    private final DirectChatRoomCreatePort directChatRoomCreatePort;
-    private final ChatRoomExitPort chatRoomExitPort;
-
-    private final DirectChatRoomQueryPort directChatRoomQueryPort;
-
     @MessageMapping("/direct/chat/send")
     public void handleMessage(
-            @RequestBody DirectChatMessageRequestDto dto
+            @RequestBody DirectChatMessageRequestDto dto,
+            @AuthenticationPrincipal(expression = "memberId") Long memberId
     ) {
-        Long memberId = 1L;
-
         directChatRoomService.sendDirectMessage(dto, memberId);
-    }
-
-    @PostMapping("/chat-rooms/direct")
-    public ResponseEntity<DirectChatRoomPreviewResponseDto> createDirectChatRoom(
-            @RequestBody DirectChatRoomCreateRequestDto dto
-    ) {
-        Long memberId = 1L;
-
-        return ResponseEntity.status(HttpStatus.CREATED)
-                .body(directChatRoomCreatePort.createDirectChatRoom(dto, memberId));
-    }
-
-    @GetMapping("/chat-rooms/direct")
-    public ResponseEntity<List<DirectChatRoomPreviewResponseDto>> getDirectChatRooms() {
-        Long memberId = 1L;
-
-        return ResponseEntity.status(HttpStatus.OK)
-                .body(directChatRoomQueryPort.getDirectChatRoomsPreview(memberId));
     }
 
     @GetMapping("/chat-rooms/messages")
     public ResponseEntity<List<ChatMessageResponseDto>> getDirectChatRoomMessages(
-            @RequestBody @Valid ChatMessageRequestDto dto
+            @RequestBody @Valid ChatMessageRequestDto dto,
+            @AuthenticationPrincipal(expression = "memberId") Long memberId
     ) {
-        Long memberId = 1L;
-
         return ResponseEntity.status(HttpStatus.OK)
                 .body(directChatRoomService.getDirectChatRoomMessages(memberId, dto));
-    }
-
-    @DeleteMapping("/chat-rooms/{chat-room-id}/direct/leave")
-    public ResponseEntity<Void> leaveDirectChatRoom(
-            @PathVariable("chat-room-id") @Positive Long chatRoomId
-    ) {
-        Long memberId = 1L;
-        chatRoomExitPort.leave(chatRoomId, memberId);
-
-        return ResponseEntity.status(HttpStatus.NO_CONTENT)
-                .build();
     }
 }
