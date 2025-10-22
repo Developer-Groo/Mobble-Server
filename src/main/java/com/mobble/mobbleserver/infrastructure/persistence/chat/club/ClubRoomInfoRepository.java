35fb88ee22f8a1e3bec2ab311d1d174ae7aca4dc
Groo <woohyeon3382@naver.com>
2025-10-22 17:33:56 +0900

    refactor: 필요 메서드 시그니처 정의

--- src/main/java/com/mobble/mobbleserver/infrastructure/persistence/chat/club/ClubRoomInfoRepository.java
+++ src/main/java/com/mobble/mobbleserver/infrastructure/persistence/chat/club/ClubRoomInfoRepository.java
@@ -3,7 +3,11 @@ package com.mobble.mobbleserver.infrastructure.persistence.chat.club;
 import com.mobble.mobbleserver.domain.chat.room.ClubRoomInfo;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface ClubRoomInfoRepository extends JpaRepository<ClubRoomInfo, Long> {
 
-    boolean existsClubRoomInfoByClubId(Long clubId);
+    Optional<ClubRoomInfo> findByClub_Id(Long clubId);
+
+    boolean existsByClub_Id(Long clubId);
 }
