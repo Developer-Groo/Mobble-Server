640b94c0cd1268c7def1c623246135b8efbe1b69
Groo <woohyeon3382@naver.com>
2025-10-22 17:32:46 +0900

    feat: ChatRoomPersistenceAdapter 구현

--- src/main/java/com/mobble/mobbleserver/infrastructure/persistence/chat/ChatRoomPersistenceAdapter.java
+++ src/main/java/com/mobble/mobbleserver/infrastructure/persistence/chat/ChatRoomPersistenceAdapter.java
@@ -3,11 +3,17 @@ package com.mobble.mobbleserver.infrastructure.persistence.chat;
 import com.mobble.mobbleserver.application.chat.room.port.required.ChatRoomReadPort;
 import com.mobble.mobbleserver.application.chat.room.port.required.ChatRoomWritePort;
 import com.mobble.mobbleserver.domain.chat.room.ChatRoom;
+import com.mobble.mobbleserver.domain.chat.room.ClubRoomInfo;
+import com.mobble.mobbleserver.domain.chat.room.DirectRoomInfo;
+import com.mobble.mobbleserver.domain.chat.room.Participant;
 import com.mobble.mobbleserver.infrastructure.persistence.chat.club.ClubRoomInfoRepository;
 import com.mobble.mobbleserver.infrastructure.persistence.chat.common.ChatRoomRepository;
+import com.mobble.mobbleserver.infrastructure.persistence.chat.common.ParticipantRepository;
+import com.mobble.mobbleserver.infrastructure.persistence.chat.direct.DirectRoomInfoRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
@@ -15,7 +21,9 @@ import java.util.Optional;
 public class ChatRoomPersistenceAdapter implements ChatRoomWritePort, ChatRoomReadPort {
 
     private final ChatRoomRepository chatRoomRepository;
+    private final ParticipantRepository participantRepository;
     private final ClubRoomInfoRepository clubRoomInfoRepository;
+    private final DirectRoomInfoRepository directRoomInfoRepository;
 
     /* ChatRoomWritePort */
     @Override
@@ -34,9 +42,31 @@ public class ChatRoomPersistenceAdapter implements ChatRoomWritePort, ChatRoomRe
         return chatRoomRepository.findById(chatRoomId);
     }
 
+    /* ChatRoomReadPort-Participant */
+    @Override
+    public List<Participant> findAllByChatRoomIdsAndMemberId(List<Long> chatRoomId, Long memberId) {
+        return participantRepository.findAllByChatRoomIdsAndMemberId(chatRoomId, memberId);
+    }
+
     /* ChatRoomReadPort-Club */
     @Override
+    public Optional<ClubRoomInfo> findClubRoomInfoByClubId(Long clubId) {
+        return clubRoomInfoRepository.findByClub_Id(clubId);
+    }
+
+    @Override
     public boolean existsClubRoomInfo(Long clubId) {
-        return clubRoomInfoRepository.existsClubRoomInfoByClubId(clubId);
+        return clubRoomInfoRepository.existsByClub_Id(clubId);
+    }
+
+    /* ChatRoomReadPort-Direct */
+    @Override
+    public List<DirectRoomInfo> findDirectChatRoomsAllByMemberId(Long memberId) {
+        return directRoomInfoRepository.findDirectChatRoomsAllByMemberId(memberId);
+    }
+
+    @Override
+    public boolean existsDirectChatRoomByBetweenMembers(Long senderId, Long receiverId) {
+        return directRoomInfoRepository.existsDirectChatRoomByBetweenMembers(senderId, receiverId);
     }
 }
