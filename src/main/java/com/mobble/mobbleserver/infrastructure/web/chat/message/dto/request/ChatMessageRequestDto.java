8647d668c22a285cf751f8444223f4998215ea0d
Groo <woohyeon3382@naver.com>
2025-10-23 20:59:24 +0900

    refactor: 구조 변경에 따른 dto 변경

--- src/main/java/com/mobble/mobbleserver/infrastructure/web/chat/message/dto/request/ChatMessageRequestDto.java
+++ src/main/java/com/mobble/mobbleserver/infrastructure/web/chat/message/dto/request/ChatMessageRequestDto.java
@@ -1,18 +1,6 @@
 package com.mobble.mobbleserver.infrastructure.web.chat.message.dto.request;
 
-import jakarta.validation.constraints.Positive;
-import org.springframework.format.annotation.DateTimeFormat;
+import com.mobble.mobbleserver.domain.chat.message.MessageType;
 
-import java.time.LocalDateTime;
-
-public record ChatMessageRequestDto(
-        @Positive
-        Long chatRoomId,
-
-        @Positive
-        Long lastMessageId,
-
-        @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME) // Todo: error message 적용 필요
-        LocalDateTime lastCreatedAt
-) {
+public record ChatMessageRequestDto(String content, MessageType type) {
 }
