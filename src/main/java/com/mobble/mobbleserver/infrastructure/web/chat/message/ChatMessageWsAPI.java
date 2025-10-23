bb16533832b51ea115e9d3e41a5c7bf89ae4afff
Groo <woohyeon3382@naver.com>
2025-10-23 21:01:29 +0900

    refactor: ChatMessageWsAPI 구조 변경 및 병합 설계

--- src/main/java/com/mobble/mobbleserver/infrastructure/web/chat/message/ChatMessageWsAPI.java
+++ src/main/java/com/mobble/mobbleserver/infrastructure/web/chat/message/ChatMessageWsAPI.java
@@ -1,40 +1,28 @@
 package com.mobble.mobbleserver.infrastructure.web.chat.message;
 
-import com.mobble.mobbleserver.infrastructure.web.chat.room.club.dto.request.ClubChatMessageRequestDto;
-import com.mobble.mobbleserver.infrastructure.web.chat.room.direct.dto.request.DirectChatMessageRequestDto;
-import com.mobble.mobbleserver.refactor.chat.clubChatRoom.service.ClubChatRoomService;
-import com.mobble.mobbleserver.refactor.chat.directChatRoom.service.DirectChatRoomService;
+import com.mobble.mobbleserver.application.chat.message.port.provided.SendMessagePort;
+import com.mobble.mobbleserver.infrastructure.web.chat.message.dto.request.ChatMessageRequestDto;
 import lombok.RequiredArgsConstructor;
+import org.springframework.messaging.handler.annotation.DestinationVariable;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@Validated
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api")
 public class ChatMessageWsAPI {
-    // Todo: api 병합 설계 필요
 
-    private final ClubChatRoomService clubChatRoomService;
-    private final DirectChatRoomService directChatRoomService;
+    private final SendMessagePort sendMessagePort;
 
-    @MessageMapping("/group/chat/send")
+    @MessageMapping("/rooms/{chat-room-id}/messages/send")
     public void handleMessage(
-            ClubChatMessageRequestDto dto,
+            @DestinationVariable Long chatRoomId,
+            @RequestBody ChatMessageRequestDto dto,
             @AuthenticationPrincipal(expression = "memberId") Long memberId
     ) {
-        clubChatRoomService.sendGroupMessage(dto, memberId);
-    }
-
-    @MessageMapping("/direct/chat/send")
-    public void handleMessage(
-            @RequestBody DirectChatMessageRequestDto dto,
-            @AuthenticationPrincipal(expression = "memberId") Long memberId
-    ) {
-        directChatRoomService.sendDirectMessage(dto, memberId);
+        sendMessagePort.send(chatRoomId, memberId, dto);
     }
 }
