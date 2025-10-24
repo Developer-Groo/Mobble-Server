da54b20fd91ce989919c777e2bd2a201d5811723
Hojin02 <140497846+Hojin02@users.noreply.github.com>
2025-10-24 17:03:15 +0900

    refactor: 패키징 변경에 따른 임포트 수정

--- src/main/java/com/mobble/mobbleserver/infrastructure/web/club/core/dto/response/ClubResponseDto.java
+++ src/main/java/com/mobble/mobbleserver/infrastructure/web/club/core/dto/response/ClubResponseDto.java
@@ -5,7 +5,7 @@ import com.mobble.mobbleserver.infrastructure.persistence.club.core.projection.C
 import com.mobble.mobbleserver.refactor.adress.dto.response.AddressResponseDto;
 import com.mobble.mobbleserver.refactor.adress.entity.Address;
 import com.mobble.mobbleserver.refactor.club.ageGroup.entity.AgeGroupType;
-import com.mobble.mobbleserver.refactor.ground.dto.response.GroundResponseDto;
+import com.mobble.mobbleserver.infrastructure.web.ground.dto.response.GroundResponseDto;
 
 import java.util.List;
 
