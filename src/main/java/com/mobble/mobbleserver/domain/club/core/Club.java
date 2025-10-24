b8f8645af755806cff9b7b9048037cc02a6591ed
Hojin02 <140497846+Hojin02@users.noreply.github.com>
2025-10-24 21:25:23 +0900

    refactor: 패키징 변경에 따른 임포트 수

--- src/main/java/com/mobble/mobbleserver/domain/club/core/Club.java
+++ src/main/java/com/mobble/mobbleserver/domain/club/core/Club.java
@@ -5,7 +5,7 @@ import com.mobble.mobbleserver.domain.chat.room.ClubRoomInfo;
 import com.mobble.mobbleserver.global.exception.common.DomainException;
 import com.mobble.mobbleserver.global.exception.errorCode.club.ClubErrorCode;
 import com.mobble.mobbleserver.refactor.adress.entity.Address;
-import com.mobble.mobbleserver.refactor.clubCategory.entity.ClubCategory;
+import com.mobble.mobbleserver.domain.ClubCategory.ClubCategory;
 import jakarta.persistence.*;
 import lombok.AccessLevel;
 import lombok.Builder;
