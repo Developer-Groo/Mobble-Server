7ebce697d1fc5b4170b7b5b3a3288ca7d3d13b6a
Groo <woohyeon3382@naver.com>
2025-10-25 20:44:26 +0900

    refactor: 사용하지 않는 파일 제거

--- src/main/java/com/mobble/mobbleserver/infrastructure/persistence/notification/NotificationPersistenceAdapter.java
+++
@@ -1,9 +0,0 @@
-package com.mobble.mobbleserver.infrastructure.persistence.notification;
-
-import lombok.RequiredArgsConstructor;
-import org.springframework.stereotype.Repository;
-
-@Repository
-@RequiredArgsConstructor
-public class NotificationPersistenceAdapter {
-}
