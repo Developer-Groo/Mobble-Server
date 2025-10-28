4ee1a882be22031f94603728ef20697b6d48d543
taejunUM <utj0411@naver.com>
2025-10-28 17:10:33 +0900

    refactor: CommentLike 필드 수정

--- src/main/java/com/mobble/mobbleserver/refactor/like/commentLike/entity/CommentLike.java
+++ src/main/java/com/mobble/mobbleserver/refactor/like/commentLike/entity/CommentLike.java
@@ -1,11 +1,10 @@
 package com.mobble.mobbleserver.refactor.like.commentLike.entity;
 
-import com.mobble.mobbleserver.domain.comment.Comment;
-import com.mobble.mobbleserver.domain.member.Member;
 import com.mobble.mobbleserver.global.exception.common.DomainException;
 import com.mobble.mobbleserver.global.exception.errorCode.like.LikeErrorCode;
 import com.mobble.mobbleserver.refactor.like.baseLike.entity.AbstractLike;
-import jakarta.persistence.*;
+import jakarta.persistence.Entity;
+import jakarta.persistence.JoinColumn;
 import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
@@ -14,24 +13,22 @@ import lombok.NoArgsConstructor;
 @Entity
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
-@AttributeOverride(name = "id", column = @Column(name = "comment_like_id"))
 public class CommentLike extends AbstractLike {
 
-    @ManyToOne(fetch = FetchType.LAZY, optional = false)
     @JoinColumn(name = "comment_id", nullable = false)
-    private Comment comment;
+    private Long commentId;
 
     @Builder(access = AccessLevel.PRIVATE)
-    private CommentLike(Comment comment, Member member) {
-        if (comment == null) throw new DomainException(LikeErrorCode.COMMENT_REQUIRED);
-        this.comment = comment;
-        assignMember(member);
+    private CommentLike(Long memberId, Long commentId) {
+        super(memberId);
+        if (commentId == null) throw new DomainException(LikeErrorCode.COMMENT_REQUIRED);
+        this.commentId = commentId;
     }
 
-    public static CommentLike createCommentLike(Comment comment, Member member) {
+    public static CommentLike createCommentLike(Long memberId, Long commentId) {
         return CommentLike.builder()
-                .comment(comment)
-                .member(member)
+                .commentId(commentId)
+                .memberId(memberId)
                 .build();
     }
 }
