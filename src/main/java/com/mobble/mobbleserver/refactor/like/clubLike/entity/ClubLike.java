2d3442974125409575299784e9484d1dfc02b98a
taejunUM <utj0411@naver.com>
2025-10-28 17:10:21 +0900

    refactor: ClubLike 필드 수정

--- src/main/java/com/mobble/mobbleserver/refactor/like/clubLike/entity/ClubLike.java
+++ src/main/java/com/mobble/mobbleserver/refactor/like/clubLike/entity/ClubLike.java
@@ -1,11 +1,10 @@
 package com.mobble.mobbleserver.refactor.like.clubLike.entity;
 
-import com.mobble.mobbleserver.domain.club.core.Club;
-import com.mobble.mobbleserver.domain.member.Member;
 import com.mobble.mobbleserver.global.exception.common.DomainException;
 import com.mobble.mobbleserver.global.exception.errorCode.like.LikeErrorCode;
 import com.mobble.mobbleserver.refactor.like.baseLike.entity.AbstractLike;
-import jakarta.persistence.*;
+import jakarta.persistence.Entity;
+import jakarta.persistence.JoinColumn;
 import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
@@ -14,24 +13,22 @@ import lombok.NoArgsConstructor;
 @Entity
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
-@AttributeOverride(name = "id", column = @Column(name = "club_like_id"))
 public class ClubLike extends AbstractLike {
 
-    @ManyToOne(fetch = FetchType.LAZY, optional = false)
     @JoinColumn(name = "club_id", nullable = false)
-    private Club club;
+    private Long clubId;
 
     @Builder(access = AccessLevel.PRIVATE)
-    private ClubLike(Club club, Member member) {
-        if (club == null) throw new DomainException(LikeErrorCode.CLUB_REQUIRED);
-        this.club = club;
-        assignMember(member);
+    private ClubLike(Long memberId, Long clubId) {
+        super(memberId);
+        if (clubId == null) throw new DomainException(LikeErrorCode.CLUB_REQUIRED);
+        this.clubId = clubId;
     }
 
-    public static ClubLike createClubLike(Club club, Member member) {
+    public static ClubLike createClubLike(Long memberId, Long clubId) {
         return ClubLike.builder()
-                .club(club)
-                .member(member)
+                .clubId(clubId)
+                .memberId(memberId)
                 .build();
     }
 }
