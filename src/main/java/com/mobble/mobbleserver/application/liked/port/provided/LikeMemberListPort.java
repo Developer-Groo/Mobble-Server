785944eca14eb998612e5b5a169d685a89ed8c07
taejunUM <utj0411@naver.com>
2025-10-29 11:03:19 +0900

    refactor: LikeMemberListPort 구현

---
+++ src/main/java/com/mobble/mobbleserver/application/liked/port/provided/LikeMemberListPort.java
@@ -0,0 +1,11 @@
+package com.mobble.mobbleserver.application.liked.port.provided;
+
+import com.mobble.mobbleserver.domain.like.core.AbstractLike;
+import com.mobble.mobbleserver.domain.like.core.LikeType;
+
+import java.util.List;
+
+public interface LikeMemberListPort {
+
+    List<? extends AbstractLike> getLikeEntities(LikeType likeType, Long targetId);
+}
