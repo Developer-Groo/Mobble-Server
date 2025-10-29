c824aba64b6bac964a5782140d59f678d1112e05
taejunUM <utj0411@naver.com>
2025-10-29 11:04:55 +0900

    refactor: CommentLikePersistenceAdapter 구현

---
+++ src/main/java/com/mobble/mobbleserver/infrastructure/persistence/like/core/commentLike/CommentLikePersistenceAdapter.java
@@ -0,0 +1,37 @@
+package com.mobble.mobbleserver.infrastructure.persistence.like.core.commentLike;
+
+import com.mobble.mobbleserver.application.liked.port.required.LikeReadPort;
+import com.mobble.mobbleserver.application.liked.port.required.LikeWritePort;
+import com.mobble.mobbleserver.domain.like.core.CommentLike;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository("commentLikePersistenceAdapter")
+@RequiredArgsConstructor
+public class CommentLikePersistenceAdapter implements LikeWritePort<CommentLike>, LikeReadPort<CommentLike> {
+
+    private final JpaCommentLikeRepository jpaCommentLikeRepository;
+
+    /**
+     * LikeWritePort
+     */
+    @Override
+    public CommentLike save(CommentLike commentLike) {
+        return jpaCommentLikeRepository.save(commentLike);
+    }
+
+    @Override
+    public void delete(CommentLike commentLike) {
+        jpaCommentLikeRepository.delete(commentLike);
+    }
+
+    /**
+     * LikeReadPort
+     */
+    @Override
+    public Optional<CommentLike> findLike(Long targetId, Long memberId) {
+        return jpaCommentLikeRepository.findLikedByCommentIdAndMemberId(targetId, memberId);
+    }
+}
