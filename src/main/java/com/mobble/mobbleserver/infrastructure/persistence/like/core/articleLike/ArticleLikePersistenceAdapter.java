5c64ec9bc4baf0aeaa569e207326c1f44309aa20
taejunUM <utj0411@naver.com>
2025-10-29 11:04:31 +0900

    refactor: ArticleLikePersistenceAdapter 구현

---
+++ src/main/java/com/mobble/mobbleserver/infrastructure/persistence/like/core/articleLike/ArticleLikePersistenceAdapter.java
@@ -0,0 +1,48 @@
+package com.mobble.mobbleserver.infrastructure.persistence.like.core.articleLike;
+
+import com.mobble.mobbleserver.application.liked.port.required.LikeMemberListReadPort;
+import com.mobble.mobbleserver.application.liked.port.required.LikeReadPort;
+import com.mobble.mobbleserver.application.liked.port.required.LikeWritePort;
+import com.mobble.mobbleserver.domain.like.core.ArticleLike;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+
+@Repository("articleLikePersistenceAdapter")
+@RequiredArgsConstructor
+public class ArticleLikePersistenceAdapter implements LikeWritePort<ArticleLike>, LikeReadPort<ArticleLike>, LikeMemberListReadPort<ArticleLike> {
+
+    private final JpaArticleLikeRepository jpaArticleLikeRepository;
+
+    /**
+     * LikeWritePort
+     */
+    @Override
+    public ArticleLike save(ArticleLike articleLike) {
+        return jpaArticleLikeRepository.save(articleLike);
+    }
+
+    @Override
+    public void delete(ArticleLike articleLike) {
+        jpaArticleLikeRepository.delete(articleLike);
+    }
+
+    /**
+     * LikeReadPort
+     */
+    @Override
+    public Optional<ArticleLike> findLike(Long targetId, Long memberId) {
+        return jpaArticleLikeRepository.findLikedByArticleIdAndMemberId(targetId, memberId);
+    }
+
+    /**
+     * LikeMemberListReadPort
+     */
+    @Override
+    public List<ArticleLike> getLikeEntities(Long articleId) {
+        return jpaArticleLikeRepository.findAllByArticleId(articleId);
+
+    }
+}
