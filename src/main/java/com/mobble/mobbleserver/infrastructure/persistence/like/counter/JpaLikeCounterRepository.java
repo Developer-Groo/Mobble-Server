ba52a6b55aae625a249acd341c1c3cf01cdfa22e
taejunUM <utj0411@naver.com>
2025-10-30 20:55:31 +0900

    refactor: JpaLikeCounterRepository 구현

---
+++ src/main/java/com/mobble/mobbleserver/infrastructure/persistence/like/counter/JpaLikeCounterRepository.java
@@ -0,0 +1,12 @@
+package com.mobble.mobbleserver.infrastructure.persistence.like.counter;
+
+import com.mobble.mobbleserver.domain.like.core.LikeType;
+import com.mobble.mobbleserver.domain.like.counter.LikeCounter;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface JpaLikeCounterRepository extends JpaRepository<LikeCounter, Long>, LikeCounterQueryDslRepository {
+
+    Optional<LikeCounter> findByLikeTypeAndTargetId(LikeType likeType, Long targetId);
+}
