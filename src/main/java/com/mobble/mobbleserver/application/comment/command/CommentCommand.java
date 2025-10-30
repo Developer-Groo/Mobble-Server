627e5348d273525c68eee62f570a00d18f547561
Groo <woohyeon3382@naver.com>
2025-10-30 21:04:48 +0900

    feat: clubId 파라미터 추가

--- src/main/java/com/mobble/mobbleserver/application/comment/command/CommentCommand.java
+++ src/main/java/com/mobble/mobbleserver/application/comment/command/CommentCommand.java
@@ -2,24 +2,41 @@ package com.mobble.mobbleserver.application.comment.command;
 
 public record CommentCommand() {
 
-    public record CreateRootCommentCommand(Long memberId, Long articleId, String content) {
+    public record CreateRootCommentCommand(Long memberId, Long clubId, Long articleId, String content) {
 
-        public static  CreateRootCommentCommand create(Long memberId, Long articleId, String content) {
-            return new CreateRootCommentCommand(memberId, articleId, content);
+        public static  CreateRootCommentCommand create(
+                Long memberId,
+                Long clubId,
+                Long articleId,
+                String content
+        ) {
+            return new CreateRootCommentCommand(memberId, clubId, articleId, content);
         }
     }
 
-    public record CreateReplyCommentCommand(Long memberId, Long articleId, Long parentId, String content) {
+    public record CreateReplyCommentCommand(Long memberId, Long clubId, Long articleId, Long parentId, String content) {
 
-        public static  CreateReplyCommentCommand create(Long memberId, Long articleId, Long parentId , String content) {
-            return new CreateReplyCommentCommand(memberId, articleId, parentId, content);
+        public static  CreateReplyCommentCommand create(
+                Long memberId,
+                Long clubId,
+                Long articleId,
+                Long parentId,
+                String content
+        ) {
+            return new CreateReplyCommentCommand(memberId, clubId, articleId, parentId, content);
         }
     }
 
-    public record UpdateCommentCommand(Long memberId, Long articleId, Long commentId, String content) {
+    public record UpdateCommentCommand(
+            Long memberId,
+            Long clubId,
+            Long articleId,
+            Long commentId,
+            String content
+    ) {
 
-        public static  UpdateCommentCommand create(Long memberId, Long articleId, Long commentId, String content) {
-            return new UpdateCommentCommand(memberId, articleId, commentId, content);
+        public static  UpdateCommentCommand create(Long memberId, Long clubId, Long articleId, Long commentId, String content) {
+            return new UpdateCommentCommand(memberId, clubId, articleId, commentId, content);
         }
     }
 }
