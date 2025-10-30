c3063e4173e6140b63be1c883cec0a9237fc9004
Groo <woohyeon3382@naver.com>
2025-10-30 20:22:20 +0900

    refactor: 구현된 CommentBody 적용

--- src/main/java/com/mobble/mobbleserver/domain/comment/Comment.java
+++ src/main/java/com/mobble/mobbleserver/domain/comment/Comment.java
@@ -11,10 +11,8 @@ import lombok.NoArgsConstructor;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Objects;
 
 import static java.util.Objects.requireNonNull;
-import static org.springframework.util.Assert.hasText;
 import static org.springframework.util.Assert.isTrue;
 
 @Getter
@@ -32,66 +30,64 @@ public class Comment extends BaseEntity {
     private Member member;
 
     @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "article_id",  nullable = false)
+    @JoinColumn(name = "article_id", nullable = false)
     private Article article;
 
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "parent_id")
     private Comment parent;
 
-    @Column(name = "content", nullable = false)
-    private String content;
+    @Embedded
+    private CommentBody body;
 
     @OneToMany(mappedBy = "parent", cascade = CascadeType.ALL, orphanRemoval = true)
-    private List<Comment> children =  new ArrayList<>();
+    private List<Comment> children = new ArrayList<>();
 
     @Builder(access = AccessLevel.PRIVATE)
     private Comment(
             Article article,
             Member member,
             Comment parent,
-            String content
+            CommentBody body
     ) {
         this.article = article;
         this.member = member;
         this.parent = parent;
-        this.content = content;
+        this.body = body;
     }
 
-    public static Comment createRootComment(Member member, Article article, String content) {
-        assertRoot(member, article, content);
+    public static Comment createRootComment(Member member, Article article, CommentBody body) {
+        assertRoot(member, article);
 
         return Comment.builder()
                 .member(member)
                 .article(article)
                 .parent(null)
-                .content(content)
+                .body(body)
                 .build();
     }
 
     public static Comment createReplyComment(
             Member member,
             Comment parent,
-            String content
+            CommentBody body
     ) {
-        assertReply(member, parent, content);
+        assertReply(member, parent);
 
         Comment child = Comment.builder()
                 .member(member)
                 .article(parent.article)
                 .parent(parent)
-                .content(content)
+                .body(body)
                 .build();
 
-        assertSameArticle(parent.article, child.article);
         parent.addChild(child);
 
         return child;
     }
 
-    public Comment updateContent(String content) {
-        assertContent(content);
-        this.content = content;
+    public Comment updateContent(CommentBody body) {
+        this.body = body;
 
         return this;
     }
@@ -106,29 +102,15 @@ public class Comment extends BaseEntity {
     }
 
     /* Assert 검증 */
-    private static void assertRoot(Member member, Article article, String content) {
+    private static void assertRoot(Member member, Article article) {
         requireNonNull(member, "member must not be null");
         requireNonNull(article, "article must not be null");
-        requireNonNull(content, "content must not be null");
-        hasText(content, "content must not be empty");
     }
-    private static void assertReply(Member member, Comment parent, String content) {
+
+    private static void assertReply(Member member, Comment parent) {
         requireNonNull(member, "member must not be null");
         requireNonNull(parent, "parent must not be null");
-        requireNonNull(content, "content must not be null");
-        hasText(content, "content must not be empty");
-        isTrue(!parent.hasParent(), "root comment has not be parent");
         requireNonNull(parent.article, "parent.article must not be null");
-    }
-
-    private static void assertContent(String content) {
-        requireNonNull(content, "content must not be null");
-        hasText(content, "content must not be empty");
-    }
-
-    private static void assertSameArticle(Article article1, Article article2) {
-        requireNonNull(article1);
-        requireNonNull(article2);
-        isTrue(Objects.equals(article1.getId(), article2.getId()), "Parent and child must belong to the same article");
+        isTrue(!parent.hasParent(), "parent must be a root comment");
     }
 }
