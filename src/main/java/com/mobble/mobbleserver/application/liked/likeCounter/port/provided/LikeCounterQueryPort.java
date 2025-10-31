7a50efcec26026b847dd8cd42b9803b28dd1ee41
taejunUM <utj0411@naver.com>
2025-10-31 16:59:36 +0900

    refactor: LikeCounterQueryPort 반환 타입 변경

--- src/main/java/com/mobble/mobbleserver/application/liked/likeCounter/port/provided/LikeCounterQueryPort.java
+++ src/main/java/com/mobble/mobbleserver/application/liked/likeCounter/port/provided/LikeCounterQueryPort.java
@@ -1,8 +1,11 @@
 package com.mobble.mobbleserver.application.liked.likeCounter.port.provided;
 
 import com.mobble.mobbleserver.domain.like.core.LikeType;
+import com.mobble.mobbleserver.infrastructure.web.like.likeCounter.dto.response.LikeCountResponseDto;
+
+import java.util.List;
 
 public interface LikeCounterQueryPort {
 
-    long getCount(LikeType likeType, Long targetId);
+    List<LikeCountResponseDto> getCounts(LikeType likeType, List<Long> targetIds);
 }
