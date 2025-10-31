97d77c1b6b434fe187fee0c0fa95518645460130
taejunUM <utj0411@naver.com>
2025-10-31 17:00:01 +0900

    refactor: LikeCounterQueryService getCounts 로직 수정

--- src/main/java/com/mobble/mobbleserver/application/liked/likeCounter/service/LikeCounterQueryService.java
+++ src/main/java/com/mobble/mobbleserver/application/liked/likeCounter/service/LikeCounterQueryService.java
@@ -4,9 +4,14 @@ import com.mobble.mobbleserver.application.liked.likeCounter.port.provided.LikeC
 import com.mobble.mobbleserver.application.liked.likeCounter.port.required.LikeCounterReadPort;
 import com.mobble.mobbleserver.domain.like.core.LikeType;
 import com.mobble.mobbleserver.domain.like.counter.LikeCounter;
+import com.mobble.mobbleserver.infrastructure.web.like.likeCounter.dto.response.LikeCountResponseDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
@@ -16,9 +21,22 @@ public class LikeCounterQueryService implements LikeCounterQueryPort {
     private final LikeCounterReadPort likeCounterReadPort;
 
     @Override
-    public long getCount(LikeType likeType, Long targetId) {
-        return likeCounterReadPort.findByLikeTypeAndTargetId(likeType, targetId)
-                .map(LikeCounter::getCount)
-                .orElse(0L);
+    public List<LikeCountResponseDto> getCounts(LikeType likeType, List<Long> targetIds) {
+        List<LikeCounter> counters = likeCounterReadPort.findAllByLikeTypeAndTargetIds(likeType, targetIds);
+
+        Map<Long, Long> counterMap = toCounterMap(counters);
+
+        return targetIds.stream()
+                .map(targetId -> LikeCountResponseDto.toDto(targetId, counterMap.getOrDefault(targetId, 0L)))
+                .toList();
+    }
+
+    private Map<Long, Long> toCounterMap(List<LikeCounter> counters) {
+        return counters.stream()
+                .collect(Collectors.toMap(
+                        LikeCounter::getTargetId,
+                        LikeCounter::getCount,
+                        (a, b) -> b
+                ));
     }
 }
