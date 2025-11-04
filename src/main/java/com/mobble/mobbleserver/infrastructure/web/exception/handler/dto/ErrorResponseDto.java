70f29ae2a4ea2f632f26e62ada8dd1511b155967
Groo <woohyeon3382@naver.com>
2025-11-04 18:20:57 +0900

    refactor: exception 의 handler 패키지 경로 변경
    - 기존 global 에서 infrastructure -> web 내부로

---
+++ src/main/java/com/mobble/mobbleserver/infrastructure/web/exception/handler/dto/ErrorResponseDto.java
@@ -0,0 +1,38 @@
+package com.mobble.mobbleserver.infrastructure.web.exception.handler.dto;
+
+import com.mobble.mobbleserver.global.exception.common.ErrorCode;
+import com.mobble.mobbleserver.util.DateTimeUtils;
+
+import java.time.LocalDateTime;
+import java.util.Map;
+
+public record ErrorResponseDto(
+        LocalDateTime timestamp,
+        String message,
+        int status,
+        String code,
+        Map<String, String> errors
+) {
+
+    // Validation or Binding Error
+    public static ErrorResponseDto toDto(ErrorCode errorCode, Map<String, String> errors) {
+        return new ErrorResponseDto(
+                DateTimeUtils.now(),
+                errorCode.message(),
+                errorCode.httpStatus().value(),
+                errorCode.httpStatus().name(),
+                errors
+        );
+    }
+
+    // Global or Domain Error
+    public static ErrorResponseDto toDto(ErrorCode errorCode) {
+        return new ErrorResponseDto(
+                DateTimeUtils.now(),
+                errorCode.message(),
+                errorCode.httpStatus().value(),
+                errorCode.httpStatus().name(),
+                null
+        );
+    }
+}
