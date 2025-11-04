6aec1ae8ddb8584d69be7747b544fd412e59f4a9
Groo <woohyeon3382@naver.com>
2025-11-04 18:21:52 +0900

    refactor: 기존 key string 값에서 단순 메세지로 변경

--- src/main/java/com/mobble/mobbleserver/infrastructure/web/comment/dto/request/CommentRequestDto.java
+++ src/main/java/com/mobble/mobbleserver/infrastructure/web/comment/dto/request/CommentRequestDto.java
@@ -4,8 +4,8 @@ import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
 
 public record CommentRequestDto(
-        @NotBlank(message = "COMMENT:CONTENT_NOT_BLANK")
-        @Size(max = 100, message = "COMMENT:CONTENT_TOO_LONG")
+        @NotBlank(message = "content must not be blank")
+        @Size(max = 100, message = "content must be 100 characters or fewer")
         String content
 ) {
 }
