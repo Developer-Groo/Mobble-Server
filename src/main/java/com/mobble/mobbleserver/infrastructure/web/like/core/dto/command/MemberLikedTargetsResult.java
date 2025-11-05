b4436b4eccfdcfda32b6bdff0db3dd59166ea7f8
taejunUM <utj0411@naver.com>
2025-11-05 18:07:13 +0900

    refactor: MemberLikedTargetsResult 구현

---
+++ src/main/java/com/mobble/mobbleserver/infrastructure/web/like/core/dto/command/MemberLikedTargetsResult.java
@@ -0,0 +1,12 @@
+package com.mobble.mobbleserver.infrastructure.web.like.core.dto.command;
+
+import com.mobble.mobbleserver.domain.like.core.LikeType;
+
+import java.util.List;
+
+public record MemberLikedTargetsResult(LikeType likeType, Long memberId, List<Long> likedTargetIds) {
+
+    public static MemberLikedTargetsResult toDto(LikeType likeType, Long memberId, List<Long> likedTargetIds) {
+        return new MemberLikedTargetsResult(likeType, memberId, likedTargetIds);
+    }
+}
