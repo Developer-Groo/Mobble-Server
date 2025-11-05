687c83a7ce3d7e2ba2e26da525e0c655446ba4e9
taejunUM <utj0411@naver.com>
2025-11-05 18:06:56 +0900

    refactor: TargetLookupPersistenceAdapter 구현

---
+++ src/main/java/com/mobble/mobbleserver/infrastructure/persistence/like/validate/TargetLookupPersistenceAdapter.java
@@ -0,0 +1,28 @@
+package com.mobble.mobbleserver.infrastructure.persistence.like.validate;
+
+import com.mobble.mobbleserver.application.article.port.required.ArticleReadPort;
+import com.mobble.mobbleserver.application.club.core.port.required.ClubReadPort;
+import com.mobble.mobbleserver.application.comment.port.required.CommentReadPort;
+import com.mobble.mobbleserver.application.liked.core.port.required.TargetLookupPort;
+import com.mobble.mobbleserver.domain.like.core.LikeType;
+import com.mobble.mobbleserver.infrastructure.web.like.core.dto.command.TargetInfo;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class TargetLookupPersistenceAdapter implements TargetLookupPort {
+
+    private final ArticleReadPort articleReadPort;
+    private final ClubReadPort clubReadPort;
+    private final CommentReadPort commentReadPort;
+
+    @Override
+    public TargetInfo targetLoad(LikeType likeType, Long targetId) {
+        return switch (likeType) {
+            case ARTICLE -> TargetInfo.toDto(likeType, targetId, articleReadPort.existsById(targetId));
+            case CLUB -> TargetInfo.toDto(likeType, targetId, clubReadPort.existsById(targetId));
+            case COMMENT -> TargetInfo.toDto(likeType, targetId, commentReadPort.existsById(targetId));
+        };
+    }
+}
