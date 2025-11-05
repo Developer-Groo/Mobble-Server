fb9ff99487a1176b46bd5c99b974fb626a666985
taejunUM <utj0411@naver.com>
2025-11-05 22:29:27 +0900

    refactor: JpaClubRepository 네이밍 변경

--- src/main/java/com/mobble/mobbleserver/infrastructure/persistence/club/core/ClubPersistenceAdapter.java
+++ src/main/java/com/mobble/mobbleserver/infrastructure/persistence/club/core/ClubPersistenceAdapter.java
@@ -15,7 +15,7 @@ import java.util.Optional;
 @RequiredArgsConstructor
 public class ClubPersistenceAdapter implements ClubWritePort, ClubReadPort {
 
-    private final JpaClubDslRepository repository;
+    private final JpaClubRepository repository;
 
     @Override
     public Club save(Club club) {
--- src/main/java/com/mobble/mobbleserver/infrastructure/persistence/club/core/JpaClubRepository.java
+++ src/main/java/com/mobble/mobbleserver/infrastructure/persistence/club/core/JpaClubRepository.java
@@ -3,6 +3,6 @@ package com.mobble.mobbleserver.infrastructure.persistence.club.core;
 import com.mobble.mobbleserver.domain.club.core.Club;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface JpaClubDslRepository extends JpaRepository<Club, Long>, ClubQueryDslRepository {
+public interface JpaClubRepository extends JpaRepository<Club, Long>, ClubQueryDslRepository {
 
 }
