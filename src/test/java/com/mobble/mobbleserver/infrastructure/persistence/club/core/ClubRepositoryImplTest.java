62faec0e74d074bfc40e0453c84aaf1976ef50e6
taejunUM <utj0411@naver.com>
2025-11-05 22:29:54 +0900

    refactor: JpaClubRepository 네이밍 변경

--- src/test/java/com/mobble/mobbleserver/infrastructure/persistence/club/core/ClubRepositoryImplTest.java
+++ src/test/java/com/mobble/mobbleserver/infrastructure/persistence/club/core/ClubRepositoryImplTest.java
@@ -22,7 +22,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 public class ClubRepositoryImplTest {
 
     @Autowired
-    private JpaClubDslRepository clubRepository;
+    private JpaClubRepository clubRepository;
 
     @Autowired
     private EntityManager em;
