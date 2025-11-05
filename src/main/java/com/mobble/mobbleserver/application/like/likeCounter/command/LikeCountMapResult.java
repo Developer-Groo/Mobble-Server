70c5e5fb3dad6aea96b19ddc4a57a6481a9f1b82
taejunUM <utj0411@naver.com>
2025-11-05 21:34:19 +0900

    refactor: LikeCountMapResult 구현

---
+++ src/main/java/com/mobble/mobbleserver/application/like/likeCounter/command/LikeCountMapResult.java
@@ -0,0 +1,12 @@
+package com.mobble.mobbleserver.application.like.likeCounter.command;
+
+import com.mobble.mobbleserver.domain.like.core.LikeType;
+
+import java.util.Map;
+
+public record LikeCountMapResult(LikeType likeType, Map<Long, Long> countsByTargetId) {
+
+    public static LikeCountMapResult toDto(LikeType likeType, Map<Long, Long> countsByTargetId) {
+        return new LikeCountMapResult(likeType, countsByTargetId);
+    }
+}
