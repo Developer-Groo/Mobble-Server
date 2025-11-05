74203c2d9b7f65f29d15465994ae530e20c38178
taejunUM <utj0411@naver.com>
2025-11-05 18:03:50 +0900

    refactor: CommentLikeQueryDslRepository 메서드 추가

--- src/main/java/com/mobble/mobbleserver/infrastructure/persistence/like/core/commentLike/CommentLikeQueryDslRepository.java
+++ src/main/java/com/mobble/mobbleserver/infrastructure/persistence/like/core/commentLike/CommentLikeQueryDslRepository.java
@@ -7,4 +7,6 @@ public interface CommentLikeQueryDslRepository {
     void deleteAllByArticleId(Long articleId);
 
     void deleteAllByArticleIds(List<Long> articleIds);
+
+    List<Long> findLikedCommentIdListByMemberId(List<Long> commentIds, Long memberId);
 }
