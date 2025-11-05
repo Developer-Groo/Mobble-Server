e426273807ca0983d6c47577e1261e7d31ce0721
taejunUM <utj0411@naver.com>
2025-11-05 18:00:40 +0900

    refactor: LikeCounterReadPort 구현

--- src/main/java/com/mobble/mobbleserver/application/liked/likeCounter/port/required/LikeCounterReadPort.java
+++ src/main/java/com/mobble/mobbleserver/application/liked/likeCounter/port/required/LikeCounterReadPort.java
@@ -4,8 +4,11 @@ import com.mobble.mobbleserver.domain.like.core.LikeType;
 import com.mobble.mobbleserver.domain.like.counter.LikeCounter;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface LikeCounterReadPort {
+
+    Optional<LikeCounter> findByLikeTypeAndTargetId(LikeType likeType, Long targetId);
 
     List<LikeCounter> findAllByLikeTypeAndTargetIds(LikeType likeType, List<Long> targetIds);
 }
