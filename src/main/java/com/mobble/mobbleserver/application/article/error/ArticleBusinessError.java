4a1908b1f2dfc5286456b0762a3c0321514ea24d
Groo <woohyeon3382@naver.com>
2025-11-06 18:39:24 +0900

    feat: ArticleBusinessError 구현

---
+++ src/main/java/com/mobble/mobbleserver/application/article/error/ArticleBusinessError.java
@@ -0,0 +1,29 @@
+package com.mobble.mobbleserver.application.article.error;
+
+import com.mobble.mobbleserver.shared.error.CommonError;
+import com.mobble.mobbleserver.shared.error.ErrorCategory;
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+public enum ArticleBusinessError implements CommonError {
+    NOT_FOUND("article not found", ErrorCategory.NOT_FOUND),
+    NO_PERMISSION("do not have permission to access this article", ErrorCategory.PERMISSION_DENIED);
+
+    private final String message;
+    private final ErrorCategory category;
+
+    @Override
+    public String code() {
+        return "ARTICLE_BUSINESS" + name();
+    }
+
+    @Override
+    public String message() {
+        return message;
+    }
+
+    @Override
+    public ErrorCategory category() {
+        return category;
+    }
+}
