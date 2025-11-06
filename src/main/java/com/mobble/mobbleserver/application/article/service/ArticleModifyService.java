a8d132001b7773781fcb57d627d63b10eff02f3d
Groo <woohyeon3382@naver.com>
2025-11-06 17:10:15 +0900

    refactor: ArticleModifyService 로직 수정

--- src/main/java/com/mobble/mobbleserver/application/article/service/ArticleModifyService.java
+++ src/main/java/com/mobble/mobbleserver/application/article/service/ArticleModifyService.java
@@ -7,26 +7,20 @@ import com.mobble.mobbleserver.application.article.port.required.ArticleReadPort
 import com.mobble.mobbleserver.application.article.port.required.ArticleWritePort;
 import com.mobble.mobbleserver.application.club.core.port.required.ClubReadPort;
 import com.mobble.mobbleserver.application.clubMember.port.required.ClubMemberReadPort;
-import com.mobble.mobbleserver.application.comment.command.response.RootCommentResult;
 import com.mobble.mobbleserver.application.comment.port.provided.CommentQueryPort;
 import com.mobble.mobbleserver.application.comment.port.required.CommentReadPort;
 import com.mobble.mobbleserver.application.comment.port.required.CommentWritePort;
 import com.mobble.mobbleserver.application.member.port.required.MemberReadPort;
 import com.mobble.mobbleserver.domain.article.Article;
+import com.mobble.mobbleserver.domain.article.ArticleContent;
 import com.mobble.mobbleserver.domain.article.ArticleType;
-import com.mobble.mobbleserver.domain.club.core.Club;
 import com.mobble.mobbleserver.domain.clubMember.ClubMember;
 import com.mobble.mobbleserver.domain.clubMember.ClubMemberRole;
 import com.mobble.mobbleserver.domain.comment.Comment;
-import com.mobble.mobbleserver.domain.member.Member;
 import com.mobble.mobbleserver.global.exception.common.DomainException;
 import com.mobble.mobbleserver.global.exception.errorCode.article.ArticleErrorCode;
-import com.mobble.mobbleserver.global.exception.errorCode.club.ClubErrorCode;
 import com.mobble.mobbleserver.global.exception.errorCode.club.ClubMemberErrorCode;
-import com.mobble.mobbleserver.global.exception.errorCode.member.MemberErrorCode;
-import com.mobble.mobbleserver.infrastructure.persistence.article.projection.ArticleLikeInfoDto;
 import com.mobble.mobbleserver.infrastructure.web.article.dto.request.ArticleRequestDto;
-import com.mobble.mobbleserver.infrastructure.web.article.dto.response.ArticleResponseDto;
 import com.mobble.mobbleserver.infrastructure.web.article.dto.response.ArticleUpdatedResponseDto;
 import com.mobble.mobbleserver.refactor.like.articleLike.repository.ArticleLikeRepository;
 import com.mobble.mobbleserver.refactor.like.commentLike.repository.CommentLikeRepository;
@@ -35,7 +29,6 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
-import java.util.Map;
 
 @Service
 @Transactional
@@ -57,34 +50,31 @@ public class ArticleModifyService implements ArticleCreatePort, ArticleUpdatePor
     private final ArticleLikeRepository articleLikeRepository;
 
     @Override
-    public ArticleResponseDto createArticle(Long memberId, Long clubId, ArticleRequestDto dto) {
-        Club club = findClubByClubIdOrThrow(clubId);
-        Member member = findMemberByMemberIdOrThrow(memberId);
-        ClubMember clubMember = findClubMemberByClubIdAndMemberIdOrThrow(clubId, memberId);
+    public Article createArticle(Long memberId, Long clubId, ArticleRequestDto dto) {
+        ClubMember clubMember = assertMemberByMemberIdAndClubId(memberId, clubId);
 
         assertCanPost(clubMember, dto.articleType());
-        Article article = dto.toEntity(club, member);
 
-        return ArticleResponseDto.toDto(articleWritePort.save(article));
+        ArticleContent content = ArticleContent.of(dto.title(), dto.content());
+        Article article = Article.createArticle(clubMember.getClub(), clubMember.getMember(), dto.articleType(), content);
+
+        return articleWritePort.save(article);
     }
 
     @Override
-    public ArticleUpdatedResponseDto updateArticle(Long articleId, Long memberId, ArticleRequestDto dto) {
-        Article article = findArticleByArticleIdAndMemberIdOrThrow(articleId, memberId);
-        Long clubId = article.getClub().getId();
-        ClubMember clubMember = findClubMemberByClubIdAndMemberIdOrThrow(clubId, memberId);
+    public Article updateArticle(Long clubId, Long articleId, Long memberId, ArticleRequestDto dto) {
+        ClubMember clubMember = assertMemberByMemberIdAndClubId(memberId, clubId);
+        Article article = assertArticleByArticleIdAndMemberId(articleId, memberId);
 
-        assertCanPost(clubMember, dto.articleType());
-        article.updateArticle(dto.articleType(), dto.title(), dto.content());
+        ArticleContent content = ArticleContent.of(dto.title(), dto.content());
 
-        return ArticleUpdatedResponseDto.toDto(article);
+        return article.updateArticle(content);
     }
 
     @Override
-    public void deleteArticle(Long articleId, Long memberId) {
-        Article article = findArticleByArticleIdOrThrow(articleId);
-        Long clubId = article.getClub().getId();
-        ClubMember clubMember = findClubMemberByClubIdAndMemberIdOrThrow(clubId, memberId);
+    public void deleteArticle(Long clubId, Long articleId, Long memberId) {
+        ClubMember clubMember = assertMemberByMemberIdAndClubId(memberId, clubId);
+        Article article = assertArticleByArticleId(articleId);
 
         boolean isOwner = articleReadPort.existsArticleByIdAndMemberId(articleId, memberId);
 
@@ -94,60 +84,50 @@ public class ArticleModifyService implements ArticleCreatePort, ArticleUpdatePor
 
         List<Comment> comments = commentReadPort.findCommentsWithRepliesByArticleId(articleId);
 
+        // Todo: 댓글 삭제 시 댓글의 좋아요는 댓글 도메인에서 지우도록 수정
         commentLikeRepository.deleteAllByArticleId(articleId);
         commentWritePort.deleteAll(comments);
         articleLikeRepository.deleteAllByArticleId(articleId);
         articleWritePort.delete(article);
     }
 
-    private Article findArticleByArticleIdOrThrow(Long articleId) {
+    /* ==== Private Helper ==== */
+    private Article assertArticleByArticleId(Long articleId) {
         return articleReadPort.findById(articleId)
                 .orElseThrow(() -> new DomainException(ArticleErrorCode.NOT_FOUND));
     }
 
-    private Article findArticleByArticleIdAndMemberIdOrThrow(Long articleId, Long memberId) {
+    private Article assertArticleByArticleIdAndMemberId(Long articleId, Long memberId) {
         return articleReadPort.findByIdAndMemberId(articleId, memberId)
                 .orElseThrow(() -> new DomainException(ArticleErrorCode.NOT_FOUND_TO_MEMBER));
     }
 
-    private void assertCanPost(ClubMember clubMember, ArticleType articleType) {
-        if (!clubMember.canPost(articleType)) {
-            throw new DomainException(ArticleErrorCode.NOTICE_NO_PERMISSION);
-        }
-    }
-
-    private Member findMemberByMemberIdOrThrow(Long memberId) {
-        return memberReadPort.findByIdAndIsDeletedFalse(memberId)
-                .orElseThrow(() -> new DomainException(MemberErrorCode.NOT_FOUND_MEMBER));
-    }
-
-    private Club findClubByClubIdOrThrow(Long clubId) {
-        return clubReadPort.findById(clubId)
-                .orElseThrow(() -> new DomainException((ClubErrorCode.NOT_FOUND)));
-    }
-
-    private ClubMember findClubMemberByClubIdAndMemberIdOrThrow(Long clubId, Long memberId) {
-        return clubMemberReadPort.findClubMemberByClubIdAndMemberId(clubId, memberId)
+    private ClubMember assertMemberByMemberIdAndClubId(Long memberId, Long clubId) {
+        return clubMemberReadPort.findClubMemberByClubIdAndMemberId(memberId, clubId)
                 .orElseThrow(() -> new DomainException(ClubMemberErrorCode.NOT_JOINED_CLUB));
     }
 
-    private ArticleResponseDto convertToArticleResponseDto(Article article, Long memberId) {
-        Map<Long, ArticleLikeInfoDto> likeInfoMap = getArticleLikeInfo(List.of(article), memberId);
-        ArticleLikeInfoDto likeInfo = likeInfoMap.getOrDefault(article.getId(), new ArticleLikeInfoDto(0, false));
-        List<RootCommentResult> comments = commentQueryPort.getCommentListByArticle(article.getId(), memberId);
-        int commentCount = comments.size();
-
-        boolean isMine = articleReadPort.existsArticleByIdAndMemberId(article.getId(), memberId);
-
-        return ArticleResponseDto.toDto(article, isMine, likeInfo, commentCount, comments);
+    private void assertCanPost(ClubMember clubMember, ArticleType articleType) {
+        if (clubMember.canPost(articleType)) throw new DomainException(ArticleErrorCode.NOTICE_NO_PERMISSION);
     }
 
-    private Map<Long, ArticleLikeInfoDto> getArticleLikeInfo(List<Article> articles, Long memberId) {
-        List<Long> articleIds = articles.stream()
-                .map(Article::getId)
-                .distinct()
-                .toList();
+//    private ArticleResponseDto convertToArticleResponseDto(Article article, Long memberId) {
+//        Map<Long, ArticleLikeInfoDto> likeInfoMap = getArticleLikeInfo(List.of(article), memberId);
+//        ArticleLikeInfoDto likeInfo = likeInfoMap.getOrDefault(article.getId(), new ArticleLikeInfoDto(0, false));
+//        List<RootCommentResult> comments = commentQueryPort.getCommentListByArticle(article.getId(), memberId);
+//        int commentCount = comments.size();
+//
+//        boolean isMine = articleReadPort.existsArticleByIdAndMemberId(article.getId(), memberId);
+//
+//        return ArticleResponseDto.toDto(article, isMine, likeInfo, commentCount, comments);
+//    }
 
-        return articleReadPort.findLikeInfoByArticleIdsAndMemberId(articleIds, memberId);
-    }
+//    private Map<Long, ArticleLikeInfoDto> getArticleLikeInfo(List<Article> articles, Long memberId) {
+//        List<Long> articleIds = articles.stream()
+//                .map(Article::getId)
+//                .distinct()
+//                .toList();
+//
+//        return articleReadPort.findLikeInfoByArticleIdsAndMemberId(articleIds, memberId);
+//    }
 }
