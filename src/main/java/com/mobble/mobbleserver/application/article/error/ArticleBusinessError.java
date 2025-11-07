40edb852bc9b9a583a43bb3b9e4f0983a17d7ea2
Groo <woohyeon3382@naver.com>
2025-11-07 17:53:53 +0900

    feat: error code 추가

--- src/main/java/com/mobble/mobbleserver/application/article/error/ArticleBusinessError.java
+++ src/main/java/com/mobble/mobbleserver/application/article/error/ArticleBusinessError.java
@@ -7,7 +7,8 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public enum ArticleBusinessError implements CommonError {
     NOT_FOUND("article not found", ErrorCategory.NOT_FOUND),
-    NO_PERMISSION("do not have permission to access this article", ErrorCategory.PERMISSION_DENIED);
+    NO_PERMISSION("do not have permission to access this article", ErrorCategory.PERMISSION_DENIED),
+    CLUB_MISMATCH("article does not belong to this club", ErrorCategory.VALIDATION);
 
     private final String message;
     private final ErrorCategory category;
