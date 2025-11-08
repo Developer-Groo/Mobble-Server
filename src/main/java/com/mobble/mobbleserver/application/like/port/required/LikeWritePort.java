6946dc426e1659b34f9a25108183f08f4bfb2582
taejunUM <utj0411@naver.com>
2025-11-09 05:05:11 +0900

    refactor: LikeWritePort Like 단건, 다건 삭제 메서드 추가

--- src/main/java/com/mobble/mobbleserver/application/like/port/required/LikeWritePort.java
+++ src/main/java/com/mobble/mobbleserver/application/like/port/required/LikeWritePort.java
@@ -2,9 +2,15 @@ package com.mobble.mobbleserver.application.like.port.required;
 
 import com.mobble.mobbleserver.domain.like.LikeType;
 
+import java.util.List;
+
 public interface LikeWritePort {
 
     void save(LikeType likeType, Long targetId, Long memberId);
 
     void delete(LikeType likeType, Long targetId, Long memberId);
+
+    void deleteByLikeTypeAndTargetId(LikeType likeType, Long targetId);
+
+    void deleteAllByLikeTypeAndTargetIds(LikeType likeType, List<Long> targetIds);
 }
