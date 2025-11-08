cf78368b53f449fb1313bbdac75e07cd3a351c51
taejunUM <utj0411@naver.com>
2025-11-09 05:05:36 +0900

    refactor: LikeCounterWritePort Likecounter 단건, 다건 삭제 메서드 추가

--- src/main/java/com/mobble/mobbleserver/application/like/port/required/LikeCounterWritePort.java
+++ src/main/java/com/mobble/mobbleserver/application/like/port/required/LikeCounterWritePort.java
@@ -2,9 +2,15 @@ package com.mobble.mobbleserver.application.like.port.required;
 
 import com.mobble.mobbleserver.domain.like.LikeType;
 
+import java.util.List;
+
 public interface LikeCounterWritePort {
 
     void increment(LikeType likeType, Long targetId);
 
     void decrement(LikeType likeType, Long targetId);
+
+    void deleteByLikeTypeAndTargetId(LikeType likeType, Long targetId);
+
+    void deleteAllByLikeTypeAndTargetIds(LikeType likeType, List<Long> targetIds);
 }
