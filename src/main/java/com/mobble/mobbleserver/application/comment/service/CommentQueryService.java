be9de27c6d1d4e5e66f76fd2f44b3ca6aaa790f3
Groo <woohyeon3382@naver.com>
2025-11-11 23:57:17 +0900

    refactor: 기존 댓글 조회 로직 수정

--- src/main/java/com/mobble/mobbleserver/application/comment/service/CommentQueryService.java
+++ src/main/java/com/mobble/mobbleserver/application/comment/service/CommentQueryService.java
@@ -1,41 +1,45 @@
 package com.mobble.mobbleserver.application.comment.service;
 
+import com.mobble.mobbleserver.application.article.error.ArticleBusinessError;
 import com.mobble.mobbleserver.application.article.port.required.ArticleReadPort;
 import com.mobble.mobbleserver.application.comment.command.response.RootCommentResult;
 import com.mobble.mobbleserver.application.comment.port.provided.CommentQueryPort;
 import com.mobble.mobbleserver.application.comment.port.required.CommentReadPort;
+import com.mobble.mobbleserver.application.common.exception.BusinessException;
+import com.mobble.mobbleserver.application.like.port.provided.LikeQueryPort;
 import com.mobble.mobbleserver.domain.article.Article;
 import com.mobble.mobbleserver.domain.comment.Comment;
-import com.mobble.mobbleserver.global.exception.common.DomainException;
-import com.mobble.mobbleserver.global.exception.errorCode.article.ArticleErrorCode;
-import com.mobble.mobbleserver.infrastructure.persistence.comment.projection.CommentLikeInfoDto;
+import com.mobble.mobbleserver.domain.like.LikeType;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 import java.util.Map;
-import java.util.stream.Stream;
 
 @Service
 @RequiredArgsConstructor
 @Transactional(readOnly = true)
 public class CommentQueryService implements CommentQueryPort {
 
+    private final LikeQueryPort likeQueryPort;
+
     private final CommentReadPort commentReadPort;
     private final ArticleReadPort articleReadPort;
 
     @Override
-    public List<RootCommentResult> getCommentListByArticle(Long articleId, Long memberId) {
+    public List<RootCommentResult> getCommentList(Long articleId, Long memberId) {
         Article article = assertArticleByArticleId(articleId);
 
-        // Todo: Like 조회 로직 변경 필요
-        List<Comment> comments = commentReadPort.findCommentsWithRepliesByArticleId(article.getId());
-        Map<Long, CommentLikeInfoDto> likeInfoMap = getCommentLikeInfo(comments, memberId);
-
-        return comments.stream()
-                .map(comment -> RootCommentResult.toDto(comment, likeInfoMap))
+        List<Comment> comments = commentReadPort.findCommentsByArticleId(article.getId());
+        List<Long> commentIds = comments.stream()
+                .map(Comment::getId)
                 .toList();
+
+        Map<Long, Long> likeCounts = likeQueryPort.getLikeCounts(LikeType.COMMENT, commentIds);
+        List<Long> likedList = likeQueryPort.getLikedIds(LikeType.COMMENT, memberId, commentIds);
+
+        return RootCommentResult.create(comments, likeCounts, likedList);
     }
 
     @Override
