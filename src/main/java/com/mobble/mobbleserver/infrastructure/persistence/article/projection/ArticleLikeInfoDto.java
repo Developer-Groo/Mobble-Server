2981a6d146581a7b4f3101208711828d73204b6b
Groo <woohyeon3382@naver.com>
2025-11-12 20:51:21 +0900

    refactor: 사용하지 않는 파일 제거

--- src/main/java/com/mobble/mobbleserver/infrastructure/persistence/article/projection/ArticleLikeInfoDto.java
+++
@@ -1,8 +0,0 @@
-package com.mobble.mobbleserver.infrastructure.persistence.article.projection;
-
-public record ArticleLikeInfoDto(int likeCount, boolean isLiked) {
-
-    public static ArticleLikeInfoDto toDto(int likeCount, boolean isLiked) {
-        return new ArticleLikeInfoDto(likeCount, isLiked);
-    }
-}
--- src/main/java/com/mobble/mobbleserver/infrastructure/persistence/article/projection/ArticleLikeProjection.java
+++
@@ -1,4 +0,0 @@
-package com.mobble.mobbleserver.infrastructure.persistence.article.projection;
-
-public record ArticleLikeProjection(Long articleId, Long memberId) {
-}
