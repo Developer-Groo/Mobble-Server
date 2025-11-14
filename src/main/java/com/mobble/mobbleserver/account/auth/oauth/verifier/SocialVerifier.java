3ddc8492fb15bd4857930379ea9d9060cc4974ae
taejunUM <utj0411@naver.com>
2025-11-14 18:20:51 +0900

    refactor: 기존 verifier 제거

--- src/main/java/com/mobble/mobbleserver/account/auth/oauth/verifier/SocialVerifier.java
+++
@@ -1,8 +0,0 @@
-package com.mobble.mobbleserver.account.auth.oauth.verifier;
-
-import com.mobble.mobbleserver.application.account.command.SocialUserInfo;
-
-public interface SocialVerifier {
-
-    SocialUserInfo verify(String accessToken);
-}
