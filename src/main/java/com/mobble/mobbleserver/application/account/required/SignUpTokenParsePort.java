9e229e78a0292c5f278a81bfae24392fcc671a5f
taejunUM <utj0411@naver.com>
2025-11-14 17:33:47 +0900

    refactor: SignUpTokenParsePort 제거

--- src/main/java/com/mobble/mobbleserver/application/account/required/SignUpTokenParsePort.java
+++
@@ -1,8 +0,0 @@
-package com.mobble.mobbleserver.application.account.required;
-
-import com.mobble.mobbleserver.application.account.command.SocialUserInfo;
-
-public interface SignUpTokenParsePort {
-
-    SocialUserInfo getSignUpTokenInfo(String signUpToken);
-}
