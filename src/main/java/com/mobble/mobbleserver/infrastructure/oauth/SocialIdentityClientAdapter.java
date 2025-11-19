5e799248b9d97587e5684f5333ad00730977df96
taejunUM <utj0411@naver.com>
2025-11-19 21:50:04 +0900

    refactor: SocialIdentityClientAdapter 리팩토링

--- src/main/java/com/mobble/mobbleserver/infrastructure/oauth/SocialIdentityClientAdapter.java
+++ src/main/java/com/mobble/mobbleserver/infrastructure/oauth/SocialIdentityClientAdapter.java
@@ -5,31 +5,36 @@ import com.mobble.mobbleserver.application.account.command.SocialUserInfo;
 import com.mobble.mobbleserver.application.account.required.SocialIdentityClientPort;
 import com.mobble.mobbleserver.global.exception.common.DomainException;
 import com.mobble.mobbleserver.global.exception.errorCode.oAuth.OAuthErrorCode;
-import com.mobble.mobbleserver.infrastructure.oauth.provider.SocialIdentityClient;
+import com.mobble.mobbleserver.infrastructure.oauth.provider.AbstractSocialClient;
+import com.mobble.mobbleserver.infrastructure.oauth.provider.userInfoResult.OAuth2UserInfo;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 
 import java.util.List;
-import java.util.Map;
-import java.util.stream.Collectors;
 
 @Component
+@RequiredArgsConstructor
 public class SocialIdentityClientAdapter implements SocialIdentityClientPort {
 
-    private final Map<SocialProvider, SocialIdentityClient> clientMap;
-
-    public SocialIdentityClientAdapter(List<SocialIdentityClient> clients) {
-        this.clientMap = clients.stream()
-                .collect(Collectors.toMap(
-                        SocialIdentityClient::getProvider,
-                        client -> client
-                ));
-    }
+    private final List<AbstractSocialClient> clientList;
 
     @Override
-    public SocialUserInfo fetchUserInfo(SocialProvider socialProvider, String token) {
-        SocialIdentityClient client = clientMap.get(socialProvider);
-        if (client == null) throw new DomainException(OAuthErrorCode.UNSUPPORTED_SOCIAL_PROVIDER);
+    public SocialUserInfo verify(SocialProvider socialProvider, String token) {
+        AbstractSocialClient client = findClient(socialProvider);
 
-        return client.fetchUserInfo(token);
+        OAuth2UserInfo userInfo = client.fetchUserInfo(token);
+
+        return new SocialUserInfo(
+                userInfo.getEmail(),
+                userInfo.getProvider(),
+                userInfo.getProviderId()
+        );
+    }
+
+    private AbstractSocialClient findClient(SocialProvider provider) {
+        return clientList.stream()
+                .filter(client -> client.getProvider() == provider)
+                .findFirst()
+                .orElseThrow(() -> new DomainException(OAuthErrorCode.UNSUPPORTED_SOCIAL_PROVIDER));
     }
 }
