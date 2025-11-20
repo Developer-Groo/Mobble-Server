1308a56be8d4d09cdc6bedcc7349c0227e27b319
Groo <woohyeon3382@naver.com>
2025-11-20 19:08:18 +0900

    refactor: 변경된 네이밍 적용

--- src/main/java/com/mobble/mobbleserver/application/club/port/provided/ClubQueryPort.java
+++ src/main/java/com/mobble/mobbleserver/application/club/port/provided/ClubQueryPort.java
@@ -2,7 +2,7 @@ package com.mobble.mobbleserver.application.club.port.provided;
 
 import com.mobble.mobbleserver.infrastructure.web.club.dto.request.ClubSearchRequestDto;
 import com.mobble.mobbleserver.infrastructure.web.club.dto.response.ClubResponseDto;
-import com.mobble.mobbleserver.infrastructure.web.club.dto.response.ClubSummaryDto;
+import com.mobble.mobbleserver.infrastructure.web.club.dto.response.ClubPreviewResponseDto;
 
 import java.util.List;
 
@@ -10,5 +10,5 @@ public interface ClubQueryPort {
 
     ClubResponseDto findClubById(Long clubId, Long memberId);
 
-    List<ClubSummaryDto> searchClubs(ClubSearchRequestDto dto, Long memberId);
+    List<ClubPreviewResponseDto> searchClubs(ClubSearchRequestDto dto, Long memberId);
 }
