5b1546e496bd31a3b214a4e5c62d8495aea48cc1
Groo <woohyeon3382@naver.com>
2025-11-21 21:13:52 +0900

    refactor: 사용하지 않는 필드 제거

--- src/main/java/com/mobble/mobbleserver/infrastructure/web/club/dto/response/ClubPreviewResponseDto.java
+++ src/main/java/com/mobble/mobbleserver/infrastructure/web/club/dto/response/ClubPreviewResponseDto.java
@@ -1,36 +1,4 @@
 package com.mobble.mobbleserver.infrastructure.web.club.dto.response;
 
-import com.mobble.mobbleserver.domain.club.Club;
-import com.mobble.mobbleserver.domain.common.Location;
-import com.mobble.mobbleserver.infrastructure.persistence.club.projection.ClubLikeInfoDto;
-
-import java.util.List;
-
-public record ClubPreviewResponseDto(
-        Long id,
-        String name,
-        String category,
-        List<Location> groundNames,
-        int headcount,
-        int likeCount,
-        boolean liked,
-        boolean isAutoJoin
-) {
-
-    public static ClubPreviewResponseDto toDto(
-            Club club,
-            List<Location> groundList,
-            ClubLikeInfoDto likeInfo
-    ) {
-        return new ClubPreviewResponseDto(
-                club.getId(),
-                club.getName(),
-                "",
-                groundList,
-                club.getMemberCount(),
-                likeInfo.likeCount(),
-                likeInfo.isLiked(),
-                club.isAutoJoin()
-        );
-    }
+public record ClubPreviewResponseDto() {
 }
