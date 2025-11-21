7de25fb686001a898696cef2b07258b1e1637ef3
Groo <woohyeon3382@naver.com>
2025-11-21 21:16:06 +0900

    refactor: port 에 사용하지 않는 메서드 시그니처 제거

--- src/main/java/com/mobble/mobbleserver/application/club/port/required/ClubReadPort.java
+++ src/main/java/com/mobble/mobbleserver/application/club/port/required/ClubReadPort.java
@@ -1,19 +1,9 @@
 package com.mobble.mobbleserver.application.club.port.required;
 
 import com.mobble.mobbleserver.domain.club.Club;
-import com.mobble.mobbleserver.infrastructure.persistence.club.projection.ClubLikeInfoDto;
-import com.mobble.mobbleserver.infrastructure.web.club.dto.request.ClubSearchRequestDto;
-
-import java.util.List;
 import java.util.Optional;
 
 public interface ClubReadPort {
 
-    ClubLikeInfoDto findLikeInfoByClubIdAndMemberId(Long id, Long id1);
-
     Optional<Club> findById(Long id);
-
-    List<Club> searchClubs(ClubSearchRequestDto dto);
-
-    boolean existsById(Long clubId);
 }
