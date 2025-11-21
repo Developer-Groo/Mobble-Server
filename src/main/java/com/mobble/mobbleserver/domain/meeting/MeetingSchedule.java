d4413250556746832f8bfb21a37e214161b8ba19
taejunUM <utj0411@naver.com>
2025-11-21 16:08:17 +0900

    refactor: MeetingSchedule 값객체 추가

---
+++ src/main/java/com/mobble/mobbleserver/domain/meeting/MeetingSchedule.java
@@ -0,0 +1,35 @@
+package com.mobble.mobbleserver.domain.meeting;
+
+import com.mobble.mobbleserver.domain.common.exception.DomainException;
+import com.mobble.mobbleserver.domain.meeting.error.MeetingError;
+import jakarta.persistence.Column;
+import jakarta.persistence.Embeddable;
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+import lombok.Value;
+
+import java.time.LocalDateTime;
+
+import static java.util.Objects.requireNonNull;
+
+@Value
+@Embeddable
+@NoArgsConstructor(access = AccessLevel.PROTECTED, force = true)
+public class MeetingSchedule {
+
+    @Column(name = "datetime", nullable = false)
+    LocalDateTime datetime;
+
+    private MeetingSchedule(LocalDateTime datetime) {
+        requireNonNull(datetime, "dateTime must not be null");
+
+        LocalDateTime now = LocalDateTime.now();
+        if (datetime.isBefore(now)) throw new DomainException(MeetingError.INVALID_DATETIME);
+
+        this.datetime = datetime;
+    }
+
+    public static MeetingSchedule of(LocalDateTime datetime) {
+        return new MeetingSchedule(datetime);
+    }
+}
