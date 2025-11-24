a28bc6b13647fee9c88471f8d44feea4e7004459
Groo <woohyeon3382@naver.com>
2025-11-24 17:19:57 +0900

    refactor: 열거형 순서 변경 및 문자열 변경

--- src/main/java/com/mobble/mobbleserver/domain/clubMember/JoinStatus.java
+++ src/main/java/com/mobble/mobbleserver/domain/clubMember/JoinStatus.java
@@ -6,11 +6,11 @@ import lombok.RequiredArgsConstructor;
 @Getter
 @RequiredArgsConstructor
 public enum JoinStatus {
-    WAITING("승인 대기"),
-    APPROVED("가입 승인"),
-    REJECTED("가입 거절"),
-    KICKED("멤버 강퇴"),
-    WITHDRAWN("멤버 탈퇴");
+    APPROVED("승인"),
+    WAITING("대기"),
+    REJECTED("거절"),
+    KICKED("강퇴"),
+    WITHDRAWN("탈퇴");
 
     private final String displayName;
 }
