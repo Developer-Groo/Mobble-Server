913242df8bd223d080ac6bdddf3593470492b90a
taejunUM <utj0411@naver.com>
2025-11-25 21:07:17 +0900

    refactor: 불필요한 MeetingMember DTO 제거

--- src/main/java/com/mobble/mobbleserver/infrastructure/web/meetingMember/dto/response/MeetingAttendanceResponseDto.java
+++
@@ -1,8 +0,0 @@
-package com.mobble.mobbleserver.infrastructure.web.meetingMember.dto.response;
-
-public record MeetingAttendanceResponseDto(Long meetingId, boolean isAttending) {
-
-    public static MeetingAttendanceResponseDto toDto(Long meetingId, boolean isAttending) {
-        return new MeetingAttendanceResponseDto(meetingId, isAttending);
-    }
-}
