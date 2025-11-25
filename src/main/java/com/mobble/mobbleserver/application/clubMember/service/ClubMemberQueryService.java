3982f71d1549682441188cc8a192a2f184504e4c
Groo <woohyeon3382@naver.com>
2025-11-26 01:38:13 +0900

    refactor: custom error 적용

--- src/main/java/com/mobble/mobbleserver/application/clubMember/service/ClubMemberQueryService.java
+++ src/main/java/com/mobble/mobbleserver/application/clubMember/service/ClubMemberQueryService.java
@@ -1,12 +1,12 @@
 package com.mobble.mobbleserver.application.clubMember.service;
 
+import com.mobble.mobbleserver.application.club.error.ClubBusinessError;
 import com.mobble.mobbleserver.application.club.port.required.ClubReadPort;
 import com.mobble.mobbleserver.application.clubMember.port.provided.ClubMemberQueryPort;
 import com.mobble.mobbleserver.application.clubMember.port.required.ClubMemberReadPort;
+import com.mobble.mobbleserver.application.exception.BusinessException;
 import com.mobble.mobbleserver.domain.club.Club;
 import com.mobble.mobbleserver.domain.clubMember.ClubMember;
-import com.mobble.mobbleserver.global.exception.common.DomainException;
-import com.mobble.mobbleserver.global.exception.errorCode.club.ClubErrorCode;
 import com.mobble.mobbleserver.infrastructure.web.clubMember.dto.response.ClubMemberResponseDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -35,9 +35,9 @@ public class ClubMemberQueryService implements ClubMemberQueryPort {
         return null;
     }
 
+    /* ==== Private Helper ==== */
     private Club findClubByClubIdOrThrow(Long clubId) {
         return clubReadPort.findById(clubId)
-                .orElseThrow(() -> new DomainException((ClubErrorCode.NOT_FOUND)));
+                .orElseThrow(() -> new BusinessException(ClubBusinessError.NOT_FOUND));
     }
-
 }
