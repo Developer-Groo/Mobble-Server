6eaedd28f8f76ae0de5106b550834b7ac846a288
Groo <woohyeon3382@naver.com>
2025-11-26 01:36:47 +0900

    refactor: 사용하지 않는 메서드 시그니처 제거

--- src/main/java/com/mobble/mobbleserver/application/clubMember/port/required/ClubMemberReadPort.java
+++ src/main/java/com/mobble/mobbleserver/application/clubMember/port/required/ClubMemberReadPort.java
@@ -2,7 +2,6 @@ package com.mobble.mobbleserver.application.clubMember.port.required;
 
 import com.mobble.mobbleserver.domain.clubMember.ClubMember;
 import com.mobble.mobbleserver.domain.clubMember.ClubMemberRole;
-import com.mobble.mobbleserver.domain.clubMember.JoinStatus;
 
 import java.util.List;
 import java.util.Optional;
@@ -11,13 +10,9 @@ public interface ClubMemberReadPort {
 
     Optional<ClubMember> findClubMemberByClubIdAndMemberId(Long clubId, Long memberId);
 
-    long countByClubIdAndJoinStatus(Long id, JoinStatus joinStatus);
-
     List<ClubMemberRole> findDistinctRolesByMemberIdAndRoleIn(Long id, List<ClubMemberRole> leader);
 
     List<ClubMember> findByClubId(Long clubId);
 
     List<ClubMember> findAllClubMemberByMemberId(Long memberId);
-
-    Optional<ClubMember> findByClubIdAndClubMemberRole(Long clubId, ClubMemberRole clubMemberRole);
 }
