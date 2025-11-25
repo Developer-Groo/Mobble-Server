5c7cec80067ff7f9447727c8916bb8fd74587992
Groo <woohyeon3382@naver.com>
2025-11-26 01:41:54 +0900

    refactor: 변경된 port 시그니처 적용

--- src/main/java/com/mobble/mobbleserver/infrastructure/web/clubMember/ClubMemberAPI.java
+++ src/main/java/com/mobble/mobbleserver/infrastructure/web/clubMember/ClubMemberAPI.java
@@ -1,14 +1,15 @@
 package com.mobble.mobbleserver.infrastructure.web.clubMember;
 
+import com.mobble.mobbleserver.application.clubMember.command.UpdateRoleCommand;
+import com.mobble.mobbleserver.application.clubMember.command.UpdateStatusCommand;
 import com.mobble.mobbleserver.application.clubMember.port.provided.ClubMemberJoinPort;
-import com.mobble.mobbleserver.application.clubMember.port.provided.ClubMemberDeletePort;
+import com.mobble.mobbleserver.application.clubMember.port.provided.ClubMemberLeavePort;
 import com.mobble.mobbleserver.application.clubMember.port.provided.ClubMemberQueryPort;
 import com.mobble.mobbleserver.application.clubMember.port.provided.ClubMemberUpdatePort;
 import com.mobble.mobbleserver.domain.clubMember.ClubMember;
 import com.mobble.mobbleserver.infrastructure.web.clubMember.dto.request.UpdateClubMemberRoleDto;
 import com.mobble.mobbleserver.infrastructure.web.clubMember.dto.request.UpdateClubMemberStatusDto;
 import com.mobble.mobbleserver.infrastructure.web.clubMember.dto.response.ClubMemberResponseDto;
-import com.mobble.mobbleserver.infrastructure.web.clubMember.dto.response.ClubMemberRoleUpdateResultDto;
 import com.mobble.mobbleserver.infrastructure.web.clubMember.dto.response.ClubMemberUpsertResponseDto;
 import jakarta.validation.constraints.Positive;
 import lombok.RequiredArgsConstructor;
@@ -29,7 +30,7 @@ public class ClubMemberAPI {
     private final ClubMemberJoinPort clubMemberJoinPort;
     private final ClubMemberQueryPort clubMemberQueryPort;
     private final ClubMemberUpdatePort clubMemberUpdatePort;
-    private final ClubMemberDeletePort clubMemberDeletePort;
+    private final ClubMemberLeavePort clubMemberLeavePort;
 
     @PostMapping("/join/{club-id}")
     public ResponseEntity<ClubMemberResponseDto> join(
@@ -37,34 +38,36 @@ public class ClubMemberAPI {
             @AuthenticationPrincipal(expression = "memberId") Long memberId
 
     ) {
-        ClubMember join = clubMemberJoinPort.join(memberId, clubId);
+        ClubMember clubMember = clubMemberJoinPort.join(memberId, clubId);
 
         return ResponseEntity.status(HttpStatus.CREATED)
-                .body(ClubMemberResponseDto.toDto(join));
+                .body(ClubMemberResponseDto.toDto(clubMember));
     }
 
     @PatchMapping("/{club-id}/members/status")
-    public ResponseEntity<ClubMemberUpsertResponseDto> updateClubMemberStatus(
+    public ResponseEntity<ClubMemberResponseDto> updateStatus(
             @PathVariable("club-id") @Positive Long clubId,
             @RequestBody UpdateClubMemberStatusDto dto,
             @AuthenticationPrincipal(expression = "memberId") Long memberId
     ) {
+        UpdateStatusCommand command = UpdateStatusCommand.create(clubId, memberId, dto.targetMemberId(), dto.targetStatus());
+        ClubMember clubMember = clubMemberUpdatePort.updateJoinStatus(command);
+
         return ResponseEntity.status(HttpStatus.OK)
-                .body(clubMemberUpdatePort.updateClubMemberJoinStatus(clubId, memberId, dto));
+                .body(ClubMemberResponseDto.toDto(clubMember));
     }
 
-    // Todo 클라이언트에서 기존 accessToken 제거 필요
     @PatchMapping("/{club-id}/members/role")
-    public ResponseEntity<ClubMemberUpsertResponseDto> updateClubMemberRole(
+    public ResponseEntity<ClubMemberResponseDto> updateRole(
             @PathVariable("club-id") @Positive Long clubId,
             @RequestBody UpdateClubMemberRoleDto dto,
             @AuthenticationPrincipal(expression = "memberId") Long memberId
     ) {
-        ClubMemberRoleUpdateResultDto result = clubMemberUpdatePort.updateClubMemberRole(clubId, memberId, dto);
+        UpdateRoleCommand command = UpdateRoleCommand.create(clubId, memberId, dto.targetMemberId(), dto.newRole());
+        ClubMember clubMember = clubMemberUpdatePort.updateRole(command);
 
         return ResponseEntity.status(HttpStatus.OK)
-                .header("Authorization", "Bearer " + result.jwtToken())
-                .body(result.clubMember());
+                .body(ClubMemberResponseDto.toDto(clubMember));
     }
 
     @DeleteMapping("/{club-id}/members/withdraw")
@@ -72,7 +75,7 @@ public class ClubMemberAPI {
             @PathVariable("club-id") @Positive Long clubId,
             @AuthenticationPrincipal(expression = "memberId") Long memberId
     ) {
-        clubMemberDeletePort.leaveClub(memberId, clubId);
+        clubMemberLeavePort.leave(memberId, clubId);
 
         return ResponseEntity.status(HttpStatus.NO_CONTENT)
                 .build();
