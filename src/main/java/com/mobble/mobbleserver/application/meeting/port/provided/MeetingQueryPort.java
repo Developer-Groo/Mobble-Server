c9bb9ffdd61293747b9c5fba940ebf4a4e8a455a
taejunUM <utj0411@naver.com>
2025-11-25 16:49:53 +0900

    refactor: findMeetingsByClubId 파라미터 수정

--- src/main/java/com/mobble/mobbleserver/application/meeting/port/provided/MeetingQueryPort.java
+++ src/main/java/com/mobble/mobbleserver/application/meeting/port/provided/MeetingQueryPort.java
@@ -6,7 +6,7 @@ import java.util.List;
 
 public interface MeetingQueryPort {
 
-    List<Meeting> findMeetingsByClubId(Long clubId);
+    List<Meeting> findMeetingsByClubId(Long memberId, Long clubId);
 
     List<Meeting> findUpcomingMeetingsByClubId(Long memberId, Long clubId);
 
