28fbf7a2320df19261d325501d0460f51d8b3bc4
taejunUM <utj0411@naver.com>
2025-11-25 18:21:29 +0900

    refactor: MeetingResult 구현

---
+++ src/main/java/com/mobble/mobbleserver/application/meeting/command/response/MeetingResult.java
@@ -0,0 +1,66 @@
+package com.mobble.mobbleserver.application.meeting.command.response;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.mobble.mobbleserver.domain.meeting.Meeting;
+import com.mobble.mobbleserver.domain.meeting.MeetingType;
+import com.mobble.mobbleserver.domain.member.Member;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+public record MeetingResult(
+        Long meetingId,
+        Long clubId,
+        String title,
+
+        @JsonFormat(pattern = "yyyy-MM-dd'T'HH:mm")
+        LocalDateTime schedule,
+        String location,
+        String cost,
+        Integer memberLimit,
+        int attendeeCount,
+        MeetingType type,
+        int dDay,
+        List<MemberInfo> attendedMembers,
+        boolean isAttended
+) {
+
+    public static List<MeetingResult> create(List<Meeting> meetings, List<Long> attendedList) {
+        return meetings.stream()
+                .map(meeting -> toResult(meeting, attendedList))
+                .toList();
+    }
+
+    public static MeetingResult toResult(Meeting meeting, List<Long> attendedList) {
+        return new MeetingResult(
+                meeting.getId(),
+                meeting.getClubMember().getClub().getId(),
+                meeting.getTitle(),
+                meeting.getSchedule().getDatetime(),
+                meeting.getLocation(),
+                meeting.getCost(),
+                meeting.getMemberLimit(),
+                meeting.getAttendeeCount(),
+                meeting.getType(),
+                meeting.calculateDDay(),
+                toAttendedMembers(meeting),
+                attendedList.contains(meeting.getId())
+        );
+    }
+
+    private static List<MemberInfo> toAttendedMembers(Meeting meeting) {
+        return meeting.getAttendedMembers().stream()
+                .map(MemberInfo::toMemberInfo)
+                .toList();
+    }
+
+    private record MemberInfo(String name, String profileImage) {
+
+        public static MemberInfo toMemberInfo(Member member) {
+            return new MemberInfo(
+                    member.getName(),
+                    member.getProfileImage()
+            );
+        }
+    }
+}
