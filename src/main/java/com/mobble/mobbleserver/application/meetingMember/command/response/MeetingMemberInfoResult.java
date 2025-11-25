0e0bda223bb0421efa3d293a6e5125ef2351d65d
taejunUM <utj0411@naver.com>
2025-11-25 21:03:58 +0900

    refactor: MeetingMemberInfoResult 내부 메서드 네이밍 변경

--- src/main/java/com/mobble/mobbleserver/application/meetingMember/command/response/MeetingMemberInfoResult.java
+++ src/main/java/com/mobble/mobbleserver/application/meetingMember/command/response/MeetingMemberInfoResult.java
@@ -4,7 +4,7 @@ import com.mobble.mobbleserver.domain.member.Member;
 
 public record MeetingMemberInfoResult(Long memberId, String name, String profileImage) {
 
-    public static MeetingMemberInfoResult toMemberInfo(Member member) {
+    public static MeetingMemberInfoResult toMeetingMemberInfo(Member member) {
         return new MeetingMemberInfoResult(
                 member.getId(),
                 member.getName(),
