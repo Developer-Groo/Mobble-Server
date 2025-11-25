aad8515cea3ba71ce295663868439037b7dba99d
taejunUM <utj0411@naver.com>
2025-11-25 15:19:02 +0900

    refactor: MeetingMember 참석 기능 로직 수정

--- src/main/java/com/mobble/mobbleserver/application/meetingMember/service/MeetingMemberModifyService.java
+++ src/main/java/com/mobble/mobbleserver/application/meetingMember/service/MeetingMemberModifyService.java
@@ -2,29 +2,21 @@ package com.mobble.mobbleserver.application.meetingMember.service;
 
 import com.mobble.mobbleserver.application.meeting.port.required.MeetingReadPort;
 import com.mobble.mobbleserver.application.meetingMember.port.provided.AttendMeetingPort;
-import com.mobble.mobbleserver.application.meetingMember.port.required.MeetingMemberReadPort;
-import com.mobble.mobbleserver.application.meetingMember.port.required.MeetingMemberWritePort;
 import com.mobble.mobbleserver.application.member.port.required.MemberReadPort;
 import com.mobble.mobbleserver.domain.meeting.Meeting;
-import com.mobble.mobbleserver.domain.meeting.MeetingMember;
 import com.mobble.mobbleserver.domain.member.Member;
 import com.mobble.mobbleserver.global.exception.common.DomainException;
 import com.mobble.mobbleserver.global.exception.errorCode.meeting.MeetingErrorCode;
-import com.mobble.mobbleserver.global.exception.errorCode.meeting.MeetingMemberErrorCode;
 import com.mobble.mobbleserver.global.exception.errorCode.member.MemberErrorCode;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-
-import java.util.Optional;
 
 @Service
 @Transactional
 @RequiredArgsConstructor
 public class MeetingMemberModifyService implements AttendMeetingPort {
 
-    private final MeetingMemberWritePort meetingMemberWritePort;
-    private final MeetingMemberReadPort meetingMemberReadPort;
     private final MeetingReadPort meetingReadPort;
     private final MemberReadPort memberReadPort;
 
@@ -33,15 +25,10 @@ public class MeetingMemberModifyService implements AttendMeetingPort {
         Meeting meeting = assertMeetingByMeetingId(meetingId);
         Member member = assertMemberByMemberId(memberId);
 
-        Optional<MeetingMember> result = meetingMemberReadPort.findMeetingMemberByMeetingIdAndMemberId(meetingId, memberId);
-
-        if (result.isPresent()) {
-            meetingMemberWritePort.delete(result.get());
+        if (meeting.hasAttendee(memberId)) {
+            meeting.cancelAttend(member.getId());
         } else {
-            int currentCount = meetingMemberReadPort.countByMeetingId(meeting.getId());
-            if (currentCount >= meeting.getMemberLimit()) throw new DomainException(MeetingMemberErrorCode.FULL_CAPACITY);
-
-            meetingMemberWritePort.save(MeetingMember.createMeetingMember(meeting, member));
+            meeting.attend(member);
         }
     }
 
