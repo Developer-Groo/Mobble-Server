fa740cd9e1af9089b8eec9904718a00edc80a529
taejunUM <utj0411@naver.com>
2025-11-25 21:13:22 +0900

    refactor: MeetingMemberAPI 수정

--- src/main/java/com/mobble/mobbleserver/infrastructure/web/meetingMember/MeetingMemberAPI.java
+++ src/main/java/com/mobble/mobbleserver/infrastructure/web/meetingMember/MeetingMemberAPI.java
@@ -2,8 +2,7 @@ package com.mobble.mobbleserver.infrastructure.web.meetingMember;
 
 import com.mobble.mobbleserver.application.meetingMember.port.provided.AttendMeetingPort;
 import com.mobble.mobbleserver.application.meetingMember.port.provided.MeetingMemberQueryPort;
-import com.mobble.mobbleserver.domain.meeting.MeetingMember;
-import com.mobble.mobbleserver.infrastructure.web.meetingMember.dto.response.MeetingAttendanceResponseDto;
+import com.mobble.mobbleserver.application.meetingMember.response.MeetingMemberResult;
 import com.mobble.mobbleserver.infrastructure.web.meetingMember.dto.response.MeetingMemberListResponseDto;
 import jakarta.validation.constraints.Positive;
 import lombok.RequiredArgsConstructor;
@@ -12,8 +11,6 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
-
-import java.util.List;
 
 @Validated
 @RestController
@@ -39,9 +36,9 @@ public class MeetingMemberAPI {
     public ResponseEntity<MeetingMemberListResponseDto> getMeetingMembers(
             @PathVariable("meeting-id") @Positive Long meetingId
     ) {
-        List<MeetingMember> meetingMembers = meetingMemberQueryPort.getMeetingMembers(meetingId);
+        MeetingMemberResult result = meetingMemberQueryPort.getMeetingMembers(meetingId);
 
         return ResponseEntity.status(HttpStatus.OK)
-                .body(MeetingMemberListResponseDto.toDto(meetingMembers));
+                .body(MeetingMemberListResponseDto.toDto(result.meetingId(), result.attendedMembers()));
     }
 }
