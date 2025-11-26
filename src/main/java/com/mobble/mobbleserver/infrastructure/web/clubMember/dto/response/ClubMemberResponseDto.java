a4a232ecb228311b6f28b5c83479d604796a969c
Groo <woohyeon3382@naver.com>
2025-11-26 14:32:59 +0900

    refactor: response dto 필드 수정

--- src/main/java/com/mobble/mobbleserver/infrastructure/web/clubMember/dto/response/ClubMemberResponseDto.java
+++ src/main/java/com/mobble/mobbleserver/infrastructure/web/clubMember/dto/response/ClubMemberResponseDto.java
@@ -2,21 +2,36 @@ package com.mobble.mobbleserver.infrastructure.web.clubMember.dto.response;
 
 import com.mobble.mobbleserver.domain.clubMember.ClubMember;
 import com.mobble.mobbleserver.domain.clubMember.JoinStatus;
+import com.mobble.mobbleserver.domain.member.Member;
+import com.mobble.mobbleserver.util.DateTimeUtils;
+
+import java.time.LocalDateTime;
 
 public record ClubMemberResponseDto(
+        Long clubMemberId,
         Long memberId,
-        String memberName,
-        // todo: 프로필 이미지
-        String memberRole,
-        JoinStatus joinStatus
+        String name,
+        String profileImageUrl,
+        String role,
+        String status,
+        LocalDateTime statusUpdatedAt
 ) {
 
     public static ClubMemberResponseDto toDto(ClubMember clubMember) {
+        Member member = clubMember.getMember();
+
+        String imageUrl = member.getProfileImage() != null
+                ? member.getProfileImage().getUrl()
+                : null;
+
         return new ClubMemberResponseDto(
-                clubMember.getMember().getId(),
-                clubMember.getMember().getName(),
-                clubMember.getClubMemberRole().getDisplayName(),
-                clubMember.getJoinStatus()
+                clubMember.getId(),
+                member.getId(),
+                member.getName(),
+                imageUrl,
+                clubMember.getClubMemberRole().name(),
+                clubMember.getJoinStatus().name(),
+                DateTimeUtils.toKST(clubMember.getStatusUpdatedAt())
         );
     }
 }
