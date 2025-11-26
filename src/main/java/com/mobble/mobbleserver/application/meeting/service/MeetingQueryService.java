3bd341c7ac337733265e06c28129410dcf4401be
Groo <woohyeon3382@naver.com>
2025-11-26 16:21:51 +0900

    refactor: custom error 적용

--- src/main/java/com/mobble/mobbleserver/application/meeting/service/MeetingQueryService.java
+++ src/main/java/com/mobble/mobbleserver/application/meeting/service/MeetingQueryService.java
@@ -1,16 +1,17 @@
 package com.mobble.mobbleserver.application.meeting.service;
 
-import com.mobble.mobbleserver.application.club.core.port.required.ClubReadPort;
-import com.mobble.mobbleserver.application.meeting.result.MeetingResult;
+import com.mobble.mobbleserver.application.club.error.ClubBusinessError;
+import com.mobble.mobbleserver.application.club.port.required.ClubReadPort;
+import com.mobble.mobbleserver.application.exception.BusinessException;
 import com.mobble.mobbleserver.application.meeting.port.provided.MeetingQueryPort;
 import com.mobble.mobbleserver.application.meeting.port.required.MeetingReadPort;
+import com.mobble.mobbleserver.application.meeting.result.MeetingResult;
 import com.mobble.mobbleserver.application.meetingMember.port.provided.MeetingMemberQueryPort;
 import com.mobble.mobbleserver.application.member.port.required.MemberReadPort;
-import com.mobble.mobbleserver.domain.club.core.Club;
+import com.mobble.mobbleserver.domain.club.Club;
 import com.mobble.mobbleserver.domain.meeting.Meeting;
 import com.mobble.mobbleserver.domain.member.Member;
 import com.mobble.mobbleserver.global.exception.common.DomainException;
-import com.mobble.mobbleserver.global.exception.errorCode.club.ClubErrorCode;
 import com.mobble.mobbleserver.global.exception.errorCode.member.MemberErrorCode;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -31,7 +32,6 @@ public class MeetingQueryService implements MeetingQueryPort {
     private final MemberReadPort memberReadPort;
     private final ClubReadPort clubReadPort;
 
-    // 전체 미팅 조회
     @Override
     public List<MeetingResult> findMeetings(Long memberId, Long clubId) {
         Member member = assertMemberByMemberId(memberId);
@@ -43,7 +43,6 @@ public class MeetingQueryService implements MeetingQueryPort {
         return MeetingResult.create(meetings, atendedList);
     }
 
-    // 다가오는 미팅 조회
     @Override
     public List<MeetingResult> findUpcomingMeetings(Long memberId, Long clubId) {
         Member member = assertMemberByMemberId(memberId);
@@ -62,8 +61,9 @@ public class MeetingQueryService implements MeetingQueryPort {
         return memberReadPort.findByIdAndIsDeletedFalse(memberId)
                 .orElseThrow(() -> new DomainException(MemberErrorCode.NOT_FOUND_MEMBER));
     }
+
     private Club assertClubByClubId(Long clubId) {
         return clubReadPort.findById(clubId)
-                .orElseThrow(() -> new DomainException(ClubErrorCode.NOT_FOUND));
+                .orElseThrow(() -> new BusinessException(ClubBusinessError.NOT_FOUND));
     }
 }
