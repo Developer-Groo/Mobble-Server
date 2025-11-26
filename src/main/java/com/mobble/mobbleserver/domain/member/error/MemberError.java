d591ae21c9f57d2a95144829e35991edcd86bd3c
taejunUM <utj0411@naver.com>
2025-11-26 17:13:26 +0900

    refactor: MemberError 구현

---
+++ src/main/java/com/mobble/mobbleserver/domain/member/error/MemberError.java
@@ -0,0 +1,29 @@
+package com.mobble.mobbleserver.domain.member.error;
+
+import com.mobble.mobbleserver.shared.error.CommonError;
+import com.mobble.mobbleserver.shared.error.ErrorCategory;
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+public enum MemberError implements CommonError {
+    REQUIRED_TERMS_AGREED("agreement to the terms of service is required", ErrorCategory.VALIDATION),
+    REQUIRED_PRIVACY_AGREED("agreement to the privacy policy is required", ErrorCategory.VALIDATION);
+
+    private final String message;
+    private final ErrorCategory category;
+
+    @Override
+    public String code() {
+        return "MEMBER." + name();
+    }
+
+    @Override
+    public String message() {
+        return message;
+    }
+
+    @Override
+    public ErrorCategory category() {
+        return category;
+    }
+}
