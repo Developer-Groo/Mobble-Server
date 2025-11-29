a1887d6351b7910c52934f9051f2e5e0dbe5dc0b
Groo <woohyeon3382@naver.com>
2025-11-29 12:46:51 +0900

    feat: findAll 메서드 추가 정의

--- src/main/java/com/mobble/mobbleserver/application/image/port/required/ImageReadPort.java
+++ src/main/java/com/mobble/mobbleserver/application/image/port/required/ImageReadPort.java
@@ -2,9 +2,12 @@ package com.mobble.mobbleserver.application.image.port.required;
 
 import com.mobble.mobbleserver.domain.image.Image;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface ImageReadPort {
 
     Optional<Image> findById(Long id);
+
+    List<Image> findAllByIds(List<Long> imageIds);
 }
