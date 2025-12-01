fb18b1da4dcb1e60dbb1a8019f8302c81062cce8
Groo <woohyeon3382@naver.com>
2025-12-01 20:35:11 +0900

    feat: default image 조회 메서드 port 에 추가 정의

--- src/main/java/com/mobble/mobbleserver/application/image/port/required/ImageReadPort.java
+++ src/main/java/com/mobble/mobbleserver/application/image/port/required/ImageReadPort.java
@@ -1,6 +1,7 @@
 package com.mobble.mobbleserver.application.image.port.required;
 
 import com.mobble.mobbleserver.domain.image.Image;
+import com.mobble.mobbleserver.domain.image.ImageType;
 
 import java.util.List;
 import java.util.Optional;
@@ -10,4 +11,6 @@ public interface ImageReadPort {
     Optional<Image> findById(Long id);
 
     List<Image> findAllByIds(List<Long> imageIds);
+
+    Optional<Image> findDefaultByType(ImageType imageType);
 }
