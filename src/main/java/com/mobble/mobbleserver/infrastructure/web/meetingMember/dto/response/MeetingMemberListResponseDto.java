1ae6950d2e6e7801aeb1561e8642d2db7860989f
taejunUM <utj0411@naver.com>
2025-12-02 17:15:59 +0900

    refactor: Member > MeetingMemberInfoResult 변환 위치 변경
    - MeetingMemberListResponseDto > MeetingMemberResult

--- src/main/java/com/mobble/mobbleserver/infrastructure/web/meetingMember/dto/response/MeetingMemberListResponseDto.java
+++ src/main/java/com/mobble/mobbleserver/infrastructure/web/meetingMember/dto/response/MeetingMemberListResponseDto.java
@@ -1,19 +1,12 @@
 package com.mobble.mobbleserver.infrastructure.web.meetingMember.dto.response;
 
 import com.mobble.mobbleserver.application.meetingMember.result.MeetingMemberInfoResult;
-import com.mobble.mobbleserver.domain.member.Member;
 
 import java.util.List;
 
 public record MeetingMemberListResponseDto(Long meetingId, List<MeetingMemberInfoResult> attendedMembers) {
 
-    public static MeetingMemberListResponseDto toDto(Long meetingId, List<Member> attendedMembers) {
-        return new MeetingMemberListResponseDto(meetingId, toAttendedMembers(attendedMembers));
-    }
-
-    private static List<MeetingMemberInfoResult> toAttendedMembers(List<Member> attendedMembers) {
-        return attendedMembers.stream()
-                .map(MeetingMemberInfoResult::toMeetingMemberInfo)
-                .toList();
+    public static MeetingMemberListResponseDto toDto(Long meetingId, List<MeetingMemberInfoResult> attendedMembers) {
+        return new MeetingMemberListResponseDto(meetingId, attendedMembers);
     }
 }
