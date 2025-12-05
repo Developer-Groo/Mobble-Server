1dc47ce2311255b5c551a0d52c495d69ca3c6eee
taejunUM <utj0411@naver.com>
2025-12-05 16:55:50 +0900

    refactor: Meeting 생성, 수정 시 작성자 정보 반환

--- src/main/java/com/mobble/mobbleserver/infrastructure/web/meeting/dto/response/MeetingResponseDto.java
+++ src/main/java/com/mobble/mobbleserver/infrastructure/web/meeting/dto/response/MeetingResponseDto.java
@@ -1,8 +1,10 @@
 package com.mobble.mobbleserver.infrastructure.web.meeting.dto.response;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
+import com.mobble.mobbleserver.application.meetingMember.result.MeetingMemberInfoResult;
 import com.mobble.mobbleserver.domain.meeting.Meeting;
 import com.mobble.mobbleserver.domain.meeting.MeetingType;
+import com.mobble.mobbleserver.domain.member.Member;
 
 import java.time.LocalDateTime;
 import java.util.List;
@@ -10,6 +12,7 @@ import java.util.List;
 public record MeetingResponseDto(
         Long meetingId,
         Long clubId,
+        MeetingMemberInfoResult ownerInfo,
         String title,
 
         @JsonFormat(pattern = "yyyy-MM-dd'T'HH:mm")
@@ -26,6 +29,7 @@ public record MeetingResponseDto(
         return new MeetingResponseDto(
                 meeting.getId(),
                 meeting.getClub().getId(),
+                toMeetingMemberInfo(meeting.getOwner()),
                 meeting.getTitle(),
                 meeting.getSchedule().getDatetime(),
                 meeting.getLocation(),
@@ -41,5 +45,13 @@ public record MeetingResponseDto(
         return meetings.stream()
                 .map(MeetingResponseDto::toDto)
                 .toList();
+    }
+
+    private static MeetingMemberInfoResult toMeetingMemberInfo(Member owner) {
+        return new MeetingMemberInfoResult(
+                owner.getId(),
+                owner.getName(),
+                owner.getProfileImage().getUrl()
+        );
     }
 }
