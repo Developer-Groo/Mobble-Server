3cd4229c9bf2df2d1e7f7910d907f5d4bf821f92
taejunUM <utj0411@naver.com>
2025-12-09 22:08:36 +0900

    refactor: 기존 다가올 meeting 조회 메서드 제거

--- src/main/java/com/mobble/mobbleserver/infrastructure/persistence/meeting/JpaMeetingRepository.java
+++ src/main/java/com/mobble/mobbleserver/infrastructure/persistence/meeting/JpaMeetingRepository.java
@@ -3,14 +3,11 @@ package com.mobble.mobbleserver.infrastructure.persistence.meeting;
 import com.mobble.mobbleserver.domain.meeting.Meeting;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.time.LocalDateTime;
 import java.util.List;
 
-public interface JpaMeetingRepository extends JpaRepository<Meeting, Long> {
+public interface JpaMeetingRepository extends JpaRepository<Meeting, Long>, MeetingQueryDslRepository {
 
     List<Meeting> findByClubIdOrderBySchedule_DatetimeAsc(Long clubId);
-
-    List<Meeting> findByClubIdAndSchedule_DatetimeGreaterThanEqualOrderBySchedule_DatetimeAsc(Long clubId, LocalDateTime dateTime);
 
     List<Long> findMainImageIdsByClubId(Long clubId);
 }
