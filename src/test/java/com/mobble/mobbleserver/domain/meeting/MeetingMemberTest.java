935ae3a53dbc7d916fa98e39a776d02507f9655a
taejunUM <utj0411@naver.com>
2025-12-09 17:59:13 +0900

    test: MeetingMemberTest 작성

---
+++ src/test/java/com/mobble/mobbleserver/domain/meeting/MeetingMemberTest.java
@@ -0,0 +1,62 @@
+package com.mobble.mobbleserver.domain.meeting;
+
+import com.mobble.mobbleserver.domain.clubMember.ClubMember;
+import com.mobble.mobbleserver.domain.member.Member;
+import com.mobble.mobbleserver.support.fixture.meeting.MeetingTestFixture;
+import com.mobble.mobbleserver.support.fixture.member.MemberTestFixture;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.mockito.Mockito.mock;
+
+class MeetingMemberTest {
+
+    private final ClubMember mockClubMember = mock(ClubMember.class);
+
+    @Nested
+    class Create {
+
+        @Test
+        void success_create() {
+            Meeting meeting = MeetingTestFixture.createDefaultMeeting(mockClubMember);
+            Member member = MemberTestFixture.createDefaultMember();
+
+            MeetingMember meetingMember = MeetingMember.createMeetingMember(meeting, member);
+
+            assertThat(meetingMember.getMeeting()).isEqualTo(meeting);
+            assertThat(meetingMember.getMember()).isEqualTo(member);
+        }
+
+        @Test
+        void success_fail_when_meeting_null() {
+            Member member = MemberTestFixture.createDefaultMember();
+
+            assertThatThrownBy(() -> MeetingMember.createMeetingMember(null, member))
+                    .isInstanceOf(NullPointerException.class)
+                    .hasMessage("meeting must not be null");
+        }
+
+        @Test
+        void success_fail_when_member_null() {
+            Meeting meeting = MeetingTestFixture.createDefaultMeeting(mockClubMember);
+
+            assertThatThrownBy(() -> MeetingMember.createMeetingMember(meeting, null))
+                    .isInstanceOf(NullPointerException.class)
+                    .hasMessage("member must not be null");
+        }
+    }
+
+    @Test
+    void success_detach() {
+        Meeting meeting = MeetingTestFixture.createDefaultMeeting(mockClubMember);
+        Member member = MemberTestFixture.createDefaultMember();
+
+        MeetingMember meetingMember = MeetingMember.createMeetingMember(meeting, member);
+
+        meetingMember.detach();
+
+        assertThat(meetingMember.getMeeting()).isNull();
+    }
+}
