1e05092f1d78d1b21f54ded5a5c3edc1c64be72d
Groo <woohyeon3382@naver.com>
2025-12-10 21:28:59 +0900

    chore: 주석 제거

--- src/main/java/com/mobble/mobbleserver/application/meetingMember/service/MeetingMemberModifyService.java
+++ src/main/java/com/mobble/mobbleserver/application/meetingMember/service/MeetingMemberModifyService.java
@@ -22,7 +22,6 @@ public class MeetingMemberModifyService implements AttendMeetingPort {
 
     @Override
     public void toggleAttend(Long meetingId, Long memberId) {
-        // Todo: 검증 조건 추가 필요
         Meeting meeting = assertMeetingByMeetingId(meetingId);
         Member member = assertMemberByMemberId(memberId);
 
