da8e3b4d017c55b5915753ac5b807cdcce58fb4c
Groo <woohyeon3382@naver.com>
2025-12-10 21:32:39 +0900

    refactor: DirectChatRoom port 패키지 변경 및 네이밍 변경

--- src/main/java/com/mobble/mobbleserver/application/chat/room/port/provided/command/direct/DirectChatRoomCreatePort.java
+++ src/main/java/com/mobble/mobbleserver/application/chat/room/port/provided/command/direct/DirectChatRoomCreatePort.java
@@ -1,9 +1,9 @@
-package com.mobble.mobbleserver.application.chat.room.port.provided.direct;
+package com.mobble.mobbleserver.application.chat.room.port.provided.command.direct;
 
 import com.mobble.mobbleserver.infrastructure.web.chat.room.direct.dto.request.DirectChatRoomCreateRequestDto;
 import com.mobble.mobbleserver.infrastructure.web.chat.room.direct.dto.response.DirectChatRoomPreviewResponseDto;
 
 public interface DirectChatRoomCreatePort {
 
-    DirectChatRoomPreviewResponseDto createDirectChatRoom(DirectChatRoomCreateRequestDto dto, Long memberId);
+    DirectChatRoomPreviewResponseDto create(DirectChatRoomCreateRequestDto dto, Long memberId);
 }
