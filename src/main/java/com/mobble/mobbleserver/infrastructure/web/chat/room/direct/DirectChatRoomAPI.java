e4aeb286f3d6ab0aec25122824bdfa0a7c930702
Groo <woohyeon3382@naver.com>
2025-12-10 21:36:44 +0900

    refactor: 네이밍 변경 및 주석 제거

--- src/main/java/com/mobble/mobbleserver/infrastructure/web/chat/room/direct/DirectChatRoomAPI.java
+++ src/main/java/com/mobble/mobbleserver/infrastructure/web/chat/room/direct/DirectChatRoomAPI.java
@@ -1,10 +1,10 @@
 package com.mobble.mobbleserver.infrastructure.web.chat.room.direct;
 
-import com.mobble.mobbleserver.application.chat.room.port.provided.common.ChatRoomExitPort;
-import com.mobble.mobbleserver.application.chat.room.port.provided.direct.DirectChatRoomCreatePort;
-import com.mobble.mobbleserver.application.chat.room.port.provided.direct.DirectChatRoomQueryPort;
+import com.mobble.mobbleserver.application.chat.room.port.provided.command.participant.ChatRoomExitPort;
+import com.mobble.mobbleserver.application.chat.room.port.provided.command.direct.DirectChatRoomCreatePort;
 import com.mobble.mobbleserver.infrastructure.web.chat.room.direct.dto.request.DirectChatRoomCreateRequestDto;
 import com.mobble.mobbleserver.infrastructure.web.chat.room.direct.dto.response.DirectChatRoomPreviewResponseDto;
+import jakarta.validation.Valid;
 import jakarta.validation.constraints.Positive;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
@@ -13,8 +13,6 @@ import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.List;
-
 @Validated
 @RestController
 @RequiredArgsConstructor
@@ -22,28 +20,19 @@ import java.util.List;
 public class DirectChatRoomAPI {
 
     private final DirectChatRoomCreatePort directChatRoomCreatePort;
-    private final DirectChatRoomQueryPort directChatRoomQueryPort;
     private final ChatRoomExitPort chatRoomExitPort;
 
-    @PostMapping("/chat-rooms/direct")
-    public ResponseEntity<DirectChatRoomPreviewResponseDto> createDirectChatRoom(
-            @RequestBody DirectChatRoomCreateRequestDto dto, // Todo: Dto 유지 고민 (receiverId 값 1개 존재)
+    @PostMapping("/chat/rooms/direct")
+    public ResponseEntity<DirectChatRoomPreviewResponseDto> create(
+            @RequestBody @Valid DirectChatRoomCreateRequestDto dto,
             @AuthenticationPrincipal(expression = "memberId") Long memberId
     ) {
         return ResponseEntity.status(HttpStatus.CREATED)
-                .body(directChatRoomCreatePort.createDirectChatRoom(dto, memberId));
+                .body(directChatRoomCreatePort.create(dto, memberId));
     }
 
-    @GetMapping("/chat-rooms/direct")
-    public ResponseEntity<List<DirectChatRoomPreviewResponseDto>> getDirectChatRooms(
-            @AuthenticationPrincipal(expression = "memberId") Long memberId
-    ) {
-        return ResponseEntity.status(HttpStatus.OK)
-                .body(directChatRoomQueryPort.getDirectChatRoomsPreview(memberId));
-    }
-
-    @DeleteMapping("/chat-rooms/{chat-room-id}/direct/leave")
-    public ResponseEntity<Void> leaveDirectChatRoom(
+    @DeleteMapping("/chat/rooms/{chat-room-id}/direct/leave")
+    public ResponseEntity<Void> leave(
             @PathVariable("chat-room-id") @Positive Long chatRoomId,
             @AuthenticationPrincipal(expression = "memberId") Long memberId
     ) {
