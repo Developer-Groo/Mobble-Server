e3cecee9880c66bf9d73624d206af77e41ca01e1
Groo <woohyeon3382@naver.com>
2025-12-11 17:58:55 +0900

    feat: findMessageByMessageId port 시그니처 정의

--- src/main/java/com/mobble/mobbleserver/application/chat/message/port/required/MessageReadPort.java
+++ src/main/java/com/mobble/mobbleserver/application/chat/message/port/required/MessageReadPort.java
@@ -5,8 +5,11 @@ import com.mobble.mobbleserver.domain.chat.message.ChatMessage;
 import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Map;
+import java.util.Optional;
 
 public interface MessageReadPort {
+
+    Optional<ChatMessage> findMessageByMessageId(Long messageId);
 
     List<ChatMessage> findMessagesFrom(Long roomId, LocalDateTime startDate, Long lastMessageId, LocalDateTime lastCreatedAt);
 
