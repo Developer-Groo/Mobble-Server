4b362eb9612576d64fd5f8972c8286b2507d8d23
Groo <woohyeon3382@naver.com>
2025-12-11 16:24:45 +0900

    refactor: port 메서드 시그니처 변경
    - web 계층 의존성 제거

--- src/main/java/com/mobble/mobbleserver/application/chat/room/port/provided/command/direct/DirectChatRoomCreatePort.java
+++ src/main/java/com/mobble/mobbleserver/application/chat/room/port/provided/command/direct/DirectChatRoomCreatePort.java
@@ -1,9 +1,8 @@
 package com.mobble.mobbleserver.application.chat.room.port.provided.command.direct;
 
-import com.mobble.mobbleserver.infrastructure.web.chat.room.direct.dto.request.DirectChatRoomCreateRequestDto;
-import com.mobble.mobbleserver.infrastructure.web.chat.room.direct.dto.response.DirectChatRoomPreviewResponseDto;
+import com.mobble.mobbleserver.application.chat.room.result.DirectChatRoomPreviewResult;
 
 public interface DirectChatRoomCreatePort {
 
-    DirectChatRoomPreviewResponseDto create(DirectChatRoomCreateRequestDto dto, Long memberId);
+    DirectChatRoomPreviewResult create(Long receiverId, Long memberId);
 }
