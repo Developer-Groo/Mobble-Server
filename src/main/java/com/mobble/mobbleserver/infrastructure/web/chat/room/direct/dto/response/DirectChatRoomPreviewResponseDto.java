3ff4e8a1d1c34890ba3b87cbff7427a0c276ce74
Groo <woohyeon3382@naver.com>
2025-12-11 16:25:59 +0900

    refactor: dto 필요없는 필드 제거 및 생성 메서드 파라미터 변경

--- src/main/java/com/mobble/mobbleserver/infrastructure/web/chat/room/direct/dto/response/DirectChatRoomPreviewResponseDto.java
+++ src/main/java/com/mobble/mobbleserver/infrastructure/web/chat/room/direct/dto/response/DirectChatRoomPreviewResponseDto.java
@@ -1,5 +1,6 @@
 package com.mobble.mobbleserver.infrastructure.web.chat.room.direct.dto.response;
 
+import com.mobble.mobbleserver.application.chat.room.result.DirectChatRoomPreviewResult;
 import com.mobble.mobbleserver.domain.chat.message.ChatMessage;
 import com.mobble.mobbleserver.domain.chat.room.ChatRoom;
 import com.mobble.mobbleserver.domain.member.Member;
@@ -8,29 +9,17 @@ import java.time.LocalDateTime;
 
 public record DirectChatRoomPreviewResponseDto(
         Long chatRoomId,
+        Long receiverId,
         String receiverName,
-        String receiverProfileImageUrl,
-        String lastMessage,
-        LocalDateTime lastMessageTime,
-        int unreadCount,
-        Long lastReadMessageId
+        String receiverProfileImageUrl
 ) {
 
-    public static DirectChatRoomPreviewResponseDto toDto(
-            ChatRoom chatRoom,
-            Member receiver,
-            ChatMessage lastMessage,
-            int unreadCount,
-            Long lastReadMessageId
-    ) {
+    public static DirectChatRoomPreviewResponseDto toDto(DirectChatRoomPreviewResult result) {
         return new DirectChatRoomPreviewResponseDto(
-                chatRoom.getId(),
-                receiver.getName(),
-                receiver.getProfileImage().getUrl(),
-                lastMessage != null ? lastMessage.getContent() : "",
-                lastMessage != null ? lastMessage.getCreatedAt() : null,
-                unreadCount,
-                lastReadMessageId
+                result.chatRoomId(),
+                result.receiverId(),
+                result.receiverName(),
+                result.receiverProfileImageUrl()
         );
     }
 }
