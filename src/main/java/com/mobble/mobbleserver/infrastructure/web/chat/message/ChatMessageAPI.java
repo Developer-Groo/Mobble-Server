e288e90c5719a764c987838df8e6233d69ab5e5b
Groo <woohyeon3382@naver.com>
2025-12-12 22:21:02 +0900

    refactor: 사용하지 않는 파라미터 제거 및 변경된 port 적용

--- src/main/java/com/mobble/mobbleserver/infrastructure/web/chat/message/ChatMessageAPI.java
+++ src/main/java/com/mobble/mobbleserver/infrastructure/web/chat/message/ChatMessageAPI.java
@@ -1,6 +1,7 @@
 package com.mobble.mobbleserver.infrastructure.web.chat.message;
 
 import com.mobble.mobbleserver.application.chat.message.port.provided.MessageQueryPort;
+import com.mobble.mobbleserver.application.chat.message.result.ChatMessageSliceResult;
 import com.mobble.mobbleserver.infrastructure.web.chat.message.dto.response.ChatMessageResponseDto;
 import jakarta.validation.constraints.Positive;
 import lombok.RequiredArgsConstructor;
@@ -11,7 +12,6 @@ import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
 import java.time.LocalDateTime;
-import java.util.List;
 
 @Validated
 @RestController
@@ -22,15 +22,15 @@ public class ChatMessageAPI {
     private final MessageQueryPort messageQueryPort;
 
     @GetMapping("/chat-rooms/{chat-room-id}/messages")
-    public ResponseEntity<List<ChatMessageResponseDto>> list(
+    public ResponseEntity<ChatMessageResponseDto> getMessages(
             @PathVariable(name = "chat-room-id") @Positive Long chatRoomId,
-            @RequestParam(required = false) Long cursor,
-            @RequestParam(required = false) LocalDateTime lastCreatedAt,
-            @RequestParam(defaultValue = "prev") String direction,
-            @RequestParam(defaultValue = "50") int limit,
+            @RequestParam(required = false) Long cursorId,
+            @RequestParam(required = false) LocalDateTime cursorCreatedAt,
             @AuthenticationPrincipal(expression = "memberId") Long memberId
     ) {
+        ChatMessageSliceResult result = messageQueryPort.getMessages(chatRoomId, memberId, cursorId, cursorCreatedAt);
+
         return ResponseEntity.status(HttpStatus.OK)
-                .body(messageQueryPort.list(chatRoomId, memberId, cursor, lastCreatedAt, direction, limit));
+                .body(ChatMessageResponseDto.toDto(result));
     }
 }
