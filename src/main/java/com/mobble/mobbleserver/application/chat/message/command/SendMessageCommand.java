7ed82302d798c6eba88b74437d735a5971da6354
Groo <woohyeon3382@naver.com>
2025-12-19 11:38:09 +0900

    feat: mentionedMemberIds 필드 추가

--- src/main/java/com/mobble/mobbleserver/application/chat/message/command/SendMessageCommand.java
+++ src/main/java/com/mobble/mobbleserver/application/chat/message/command/SendMessageCommand.java
@@ -2,24 +2,29 @@ package com.mobble.mobbleserver.application.chat.message.command;
 
 import com.mobble.mobbleserver.domain.chat.message.MessageType;
 
+import java.util.List;
+
 public record SendMessageCommand(
         Long chatRoomId,
         Long senderId,
         String content,
-        MessageType type
-) {
+        MessageType type,
+        List<Long> mentionedMemberIds
+ ) {
 
     public static SendMessageCommand create(
             Long chatRoomId,
             Long senderId,
             String content,
-            MessageType type
+            MessageType type,
+            List<Long> mentionedMemberIds
     ) {
         return new SendMessageCommand(
                 chatRoomId,
                 senderId,
                 content,
-                type
+                type,
+                mentionedMemberIds
         );
     }
 }
